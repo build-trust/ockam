b502380604ef477c3bf083a34b21532dbe3f574e
Mrinal Wadhwa <mrinal@ockam.io>
2019-09-17 09:29:58 -0700

    fix(implementations/c): multiple declarations of of vault struct

--- implementations/c/include/ockam/vault.h
+++ implementations/c/include/ockam/vault.h
@@ -2,8 +2,6 @@
 #define OCKAM_VAULT_H
 
 typedef struct ockam_vault_t *ockam_vault_t;
-typedef struct ockam_vault_t *ockam_vault_t;
-
 
 extern ockam_vault_t ockam_vault_init(void);
 extern int ockam_vault_random(ockam_vault_t vault);
