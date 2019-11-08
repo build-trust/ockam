db08186ed9792953329a7dfb671f579c4ba7a63d
Mrinal Wadhwa <mrinal@ockam.io>
2019-11-08 09:38:22 -0800

    fix(implementations/elixir): remove c implementation dependency

--- implementations/elixir/src/ockam.c
+++ implementations/elixir/src/ockam.c
@@ -1,9 +1,10 @@
 #include "erl_nif.h"
-#include "ockam.h"
+/* #include "ockam.h" */
 
 static ERL_NIF_TERM
 random(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]) {
-  int r = rand();
+  /* int r = rand(); */
+  int r = 50;
   return enif_make_int(env, r);
 }
 
