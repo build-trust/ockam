aab6cb07a833c7848f5bd4e732bbe44feb0e4b37
Paul Schoenfelder <paulschoenfelder@fastmail.com>
2020-01-28 16:20:00 -0500

    build: tweaks to erlang/elixir build
    - Update to Erlang 22.2.3 and Elixir 1.10.0
    - Configure Erlang build more precisely to avoid unused components being
    built, this should help in build time
    - Install Hex and Rebar by default

--- tools/builder/debian/provision/elixir.sh
+++ tools/builder/debian/provision/elixir.sh
@@ -1,9 +1,14 @@
 #!/bin/sh -eux
 
-download elixirsrc "https://github.com/elixir-lang/elixir/archive/v1.9.0.tar.gz" \
-  "dbf4cb66634e22d60fe4aa162946c992257f700c7db123212e7e29d1c0b0c487"
+cd /tmp
+
+download elixirsrc "https://github.com/elixir-lang/elixir/archive/v1.10.0.tar.gz" \
+  "6f0d35acfcbede5ef7dced3e37f016fd122c2779000ca9dcaf92975b220737b7"
 
 cd elixirsrc
-make install clean
+make && make install
+mix local.hex --force
+mix local.rebar --force
+
 cd /tmp
 rm -rf elixirsrc
--- tools/builder/debian/provision/erlang.sh
+++ tools/builder/debian/provision/erlang.sh
@@ -1,15 +1,40 @@
 #!/bin/sh -eux
 
+cd /tmp
+
 download otpsrc \
-  "https://github.com/erlang/otp/archive/OTP-22.0.7.tar.gz" \
-  "04c090b55ec4a01778e7e1a5b7fdf54012548ca72737965b7aa8c4d7878c92bc"
+  "https://github.com/erlang/otp/archive/OTP-22.2.3.tar.gz" \
+  "8470fff519d9ffa5defba4e42c3c1e64cd86905313040246d4a6e35799a9e614"
 
 cd otpsrc
-top="$(pwd)"
-export ERL_TOP="$top"
+export ERL_TOP="$(pwd)"
 ./otp_build autoconf
-./configure --build="$(dpkg-architecture --query DEB_BUILD_GNU_TYPE)"
+./configure \
+    --build="$(dpkg-architecture --query DEB_BUILD_GNU_TYPE)" \
+    --without-javac \
+    --without-wx \
+    --without-debugger \
+    --without-observer \
+    --without-jinterface \
+    --without-cosEvent\
+    --without-cosEventDomain \
+    --without-cosFileTransfer \
+    --without-cosNotification \
+    --without-cosProperty \
+    --without-cosTime \
+    --without-cosTransactions \
+    --without-et \
+    --without-gs \
+    --without-ic \
+    --without-megaco \
+    --without-orber \
+    --without-percept \
+    --without-typer \
+    --enable-threads \
+    --enable-shared-zlib \
+    --enable-ssl=dynamic-ssl-lib
 make "-j$(nproc)"
 make install
+
 cd /tmp
 rm -rf otpsrc
