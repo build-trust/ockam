df50b6ed84ef933e329236cffd04c4941856dbf1
Mrinal Wadhwa <mrinal@ockam.io>
2020-05-27 12:01:24 -0700

    feat: bump version to 0.7.0

--- implementations/elixir/mix.exs
+++ implementations/elixir/mix.exs
@@ -4,7 +4,7 @@ defmodule Ockam.MixProject do
   def project do
     [
       app: :ockam,
-      version: "0.6.0",
+      version: "0.7.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(Mix.env()),
