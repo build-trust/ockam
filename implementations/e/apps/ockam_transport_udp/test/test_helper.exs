ae8c7d2eae2343dd62cc88e38429a93835975fdc
Mrinal Wadhwa <mrinal@ockam.io>
2020-08-28 15:40:32 -0700

    refactor(elixir): add test for ockam_transport_udp start

--- implementations/e/apps/ockam_transport_udp/test/test_helper.exs
+++ implementations/e/apps/ockam_transport_udp/test/test_helper.exs
@@ -1,3 +1,4 @@
 Application.ensure_all_started(:ockam)
+Application.ensure_all_started(:ockam_transport_udp)
 
 ExUnit.start(capture_log: true, trace: true)
--- implementations/e/apps/ockam_transport_udp/test/udp_test.exs
+++ implementations/e/apps/ockam_transport_udp/test/udp_test.exs
@@ -1,4 +1,17 @@
 defmodule Ockam.Transport.UDP.Tests do
   use ExUnit.Case, async: true
   doctest Ockam
+
+  describe "Ockam.Transport.UDP.start/2" do
+    test "Ockam.Transport.UDP.Server is started" do
+      Ockam.Transport.UDP.Server |> find_child |> Process.alive?() |> assert
+    end
+  end
+
+  def find_child(name) do
+    {_, pid, _, _} =
+      Ockam.Transport.UDP |> Supervisor.which_children() |> Enum.find(fn {t, _, _, _} -> t == name end)
+
+    pid
+  end
 end
