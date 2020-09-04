ed026dbb3b03b8f1a81b64e436a84da39e45e0c3
Mrinal Wadhwa <mrinal@ockam.io>
2020-09-04 08:54:10 -0700

    docs(elixir): improve documentation of router guards

--- implementations/e/applications/ockam/lib/ockam/router/gaurds.ex
+++ implementations/e/applications/ockam/lib/ockam/router/gaurds.ex
@@ -1,18 +1,24 @@
 defmodule Ockam.Router.Guards do
   @moduledoc """
-  Defines an elixir macro suitable for use in guard expressions to validate some
-  types defined in Ockam.Router.
-
-  See `Ockam.Router`.
+  Defines elixir macros suitable for use in guard expressions to validate
+  some types used by Ockam.Router.
   """
 
   @doc """
-  Returns `true` if `term` is a valid `t:Ockam.Router.address_type/0`;
+  Returns `true` if `term` is a valid `t:Ockam.Router.Address.type/0`;
   otherwise returns `false`.
 
   Allowed in guard tests. Inlined by the compiler.
   """
+  @doc guard: true
   defguard is_address_type(term) when is_integer(term) and term >= 0 and term <= 65_535
 
-  defguard is_message_handler(fun) when is_function(fun)
+  @doc """
+  Returns `true` if `term` is a valid `t:Ockam.Router.Message.t/0`;
+  otherwise returns `false`.
+
+  Allowed in guard tests. Inlined by the compiler.
+  """
+  @doc guard: true
+  defguard is_message_handler(term) when is_function(term)
 end
