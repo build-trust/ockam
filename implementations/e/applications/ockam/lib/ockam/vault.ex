966b766aeae575553e5d56b73dd1ab3b050aa636
Mrinal Wadhwa <mrinal@ockam.io>
2020-09-15 00:31:04 -0700

    refactor(elixir): add vault behaviour

---
+++ implementations/e/applications/ockam/lib/ockam/vault.ex
@@ -0,0 +1,18 @@
+defmodule Ockam.Vault do
+  @moduledoc false
+
+  @type t :: any()
+  @type digest :: <<_::32>>
+
+  @doc """
+  Computes the SHA-256 digest of the input.
+  """
+  @callback sha256(vault :: t, input :: binary()) :: {:ok, digest :: digest()} | {:error, reason :: any()}
+
+  @doc """
+  Dispatches to an im .
+  """
+  @spec sha256(vault :: t, input :: binary()) :: {:ok, digest :: digest()} | {:error, reason :: any()}
+  def sha256(vault, input), do: vault.sha256(input)
+
+end
