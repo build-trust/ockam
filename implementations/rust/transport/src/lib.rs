081c8a529f69f5b5ee8f37a78050d1426f3f4bab
Michael Lodder <mike@ockam.io>
2020-10-13 12:27:57 -0600

    feat(rust): another merge conflict fix
    Signed-off-by: Michael Lodder <mike@ockam.io>

--- implementations/rust/transport/src/lib.rs
+++ implementations/rust/transport/src/lib.rs
@@ -93,6 +93,17 @@ pub mod transport {
                     // println!("{:?}", b);
                     match Message::decode(&buff[0..s]) {
                         Ok((mut m, _unused)) => {
+                            // println!("Received message type: {:?}\nOnward route:",
+                            // m.message_type); m.onward_route.
+                            // print_route(); println!("Return route:");
+                            // m.return_route.print_route();
+                            // match m.message_type {
+                            //     MessageType::Payload => {
+                            //         println!("Payload: {}",
+                            // std::str::from_utf8(&m.message_body).unwrap());
+                            //     }
+                            //     _ => {}
+                            // }
                             if !m.onward_route.addresses.is_empty()
                                 && m.onward_route.addresses[0].a_type == AddressType::Udp
                             {
