0c78614f57ca74feb2d0e830ad406de47cfae41d
Mrinal Wadhwa <mrinal@ockam.io>
2020-10-24 22:39:13 -0700

    feat(elixir): define the error module

--- implementations/elixir/ockam/ockam/lib/ockam/error.ex
+++ implementations/elixir/ockam/ockam/lib/ockam/error.ex
@@ -3,12 +3,26 @@ defmodule Ockam.Error do
 
   defmacro __using__(_options) do
     quote do
-      @type t :: %__MODULE__{reason: any(), module: atom}
+      @type t :: %__MODULE__{reason: any()}
 
-      defexception [:reason, :module]
+      defexception [:reason, :metadata]
 
-      def message(%__MODULE__{module: module} = error) do
-        module.format_error(error)
+      def message(error), do: format_error(error)
+
+      def format_error(%__MODULE__{metadata: %{module: module, stacktrace: stacktrace}} = error) do
+        module.format_error(error) <> "\nStacktrace:\n" <> Exception.format_stacktrace(stacktrace)
+      end
+
+      defmacro new(reason) do
+        error_module = __MODULE__
+        caller_module = __CALLER__.module
+
+        quote do
+          {:current_stacktrace, [_ | stacktrace]} = Process.info(self(), :current_stacktrace)
+
+          metadata = %{stacktrace: stacktrace, module: unquote(caller_module)}
+          %unquote(error_module){reason: unquote(reason), metadata: metadata}
+        end
       end
     end
   end
