2265aa4768d6e0eda3f86f6614908c912e179412
Mrinal Wadhwa <mrinal@ockam.io>
2020-11-06 11:39:30 -0800

    feat(elixir): define secure channel listener

---
+++ implementations/elixir/ockam/ockam/lib/ockam/secure_channel/listener.ex
@@ -0,0 +1,49 @@
+defmodule Ockam.SecureChannel.Listener do
+  @moduledoc false
+
+  use Ockam.Worker
+
+  alias Ockam.Message
+  alias Ockam.SecureChannel.Channel
+
+  @doc false
+  @impl true
+  def setup(options, state) do
+    with {:ok, vault} <- get_from_options(:vault, options),
+         {:ok, identity_keypair} <- get_from_options(:identity_keypair, options) do
+      state = Map.put(state, :vault, vault)
+      state = Map.put(state, :identity_keypair, identity_keypair)
+      {:ok, state}
+    end
+  end
+
+  @doc false
+  @impl true
+  def handle_message(message, state) do
+    create_channel(message, state)
+  end
+
+  defp create_channel(message, state) do
+    channel_options =
+      [role: :responder]
+      |> Keyword.put(:vault, state.vault)
+      |> Keyword.put(:identity_keypair, state.identity_keypair)
+
+    with {:ok, channel_options} <- update_routes(message, channel_options),
+         {:ok, _address} <- Channel.create(channel_options) do
+      {:ok, state}
+    end
+  end
+
+  defp update_routes(message, channel_options) do
+    onward_route = Message.onward_route(message)
+    return_route = Message.return_route(message)
+    payload = Message.payload(message)
+
+    {_address, onward_route} = List.pop_at(onward_route, length(onward_route) - 1)
+    message = %{onward_route: onward_route, return_route: return_route, payload: payload}
+
+    channel_options = Keyword.put(channel_options, :initiating_message, message)
+    {:ok, channel_options}
+  end
+end
