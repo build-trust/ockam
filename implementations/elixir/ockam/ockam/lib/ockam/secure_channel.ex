c2cbe3413a4e9e0a0eaf554fe01f219465ce0499
Mrinal Wadhwa <mrinal@ockam.io>
2020-11-06 11:42:21 -0800

    feat(elixir): add secure channel application interface

--- implementations/elixir/ockam/ockam/lib/ockam/secure_channel.ex
+++ implementations/elixir/ockam/ockam/lib/ockam/secure_channel.ex
@@ -1,3 +1,31 @@
 defmodule Ockam.SecureChannel do
-  @moduledoc false
+  @moduledoc """
+  A secure channel provides end-to-end secure and private communication that is
+  safe against eavesdropping, tampering, and forgery of messages en-route.
+  """
+
+  alias Ockam.SecureChannel.Channel
+  alias Ockam.SecureChannel.Listener
+
+  @doc """
+  Create a channel listener that will create new channel responders every
+  time it receives a message.
+  """
+  defdelegate create_listener(options), to: Listener, as: :create
+
+  @doc """
+  Create a secure channel.
+  """
+  defdelegate create(options), to: Channel
+
+  @doc """
+  Send a message through a secure channel.
+  """
+  defdelegate send(channel, message), to: Channel
+
+  @doc """
+  Returns a map of information about the peer responder or initiator of a
+  channel.
+  """
+  defdelegate peer(channel), to: Channel
 end
---
+++ implementations/elixir/ockam/ockam/test/secure_channel_test.exs
@@ -0,0 +1,28 @@
+defmodule Ockam.SecureChannel.Tests do
+  use ExUnit.Case, async: true
+  doctest Ockam.SecureChannel
+
+  alias Ockam.SecureChannel
+  alias Ockam.Vault
+  alias Ockam.Vault.Software, as: SoftwareVault
+
+  test "secure channel works" do
+    secret_attributes = %{type: :curve25519, persistence: :ephemeral, purpose: :key_agreement}
+
+    {:ok, responder_vault} = SoftwareVault.init()
+    {:ok, responder_identity} = Vault.secret_generate(responder_vault, secret_attributes)
+
+    {:ok, listener} =
+      SecureChannel.create_listener(vault: responder_vault, identity_keypair: responder_identity)
+
+    {:ok, initiator_vault} = SoftwareVault.init()
+    {:ok, initiator_identity} = Vault.secret_generate(initiator_vault, secret_attributes)
+
+    {:ok, _c} =
+      SecureChannel.create(
+        route: [listener],
+        vault: initiator_vault,
+        identity_keypair: initiator_identity
+      )
+  end
+end
