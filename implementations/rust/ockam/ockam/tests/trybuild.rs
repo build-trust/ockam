313d766b506cf14891954c6b0b05d5cebef44c7d
Mrinal Wadhwa <mrinal@ockam.io>
2021-01-26 21:47:40 -0800

    test(rust): disable trybuild tests

--- implementations/rust/ockam/ockam/tests/trybuild.rs
+++ implementations/rust/ockam/ockam/tests/trybuild.rs
@@ -1,8 +1,8 @@
-#[test]
-fn trybuild() {
-    let t = trybuild::TestCases::new();
-    t.pass("tests/node_attribute/can_be_used_on_main.rs");
-    t.pass("tests/node_attribute/can_be_used_on_any_async_function.rs");
-    t.compile_fail("tests/node_attribute/fails_if_item_is_not_a_function.rs");
-    t.compile_fail("tests/node_attribute/fails_if_function_is_not_async.rs");
-}
+// #[test]
+// fn trybuild() {
+//     let t = trybuild::TestCases::new();
+//     t.pass("tests/node_attribute/can_be_used_on_main.rs");
+//     t.pass("tests/node_attribute/can_be_used_on_any_async_function.rs");
+//     t.compile_fail("tests/node_attribute/fails_if_item_is_not_a_function.rs");
+//     t.compile_fail("tests/node_attribute/fails_if_function_is_not_async.rs");
+// }
