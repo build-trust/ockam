75f7f5b095946c8eaca8497d4a2ad939b1fbbe69
Oleksandr Deundiak <olek@ockam.io>
2021-02-03 20:56:57 +0200

    feat(rust): add key_id computation to key_id vault

--- implementations/rust/ockam/ockam_vault/src/kid_impl.rs
+++ implementations/rust/ockam/ockam_vault/src/kid_impl.rs
@@ -1,6 +1,6 @@
 use crate::software_vault::SoftwareVault;
 use crate::VaultError;
-use ockam_vault_core::{KidVault, Secret};
+use ockam_vault_core::{HashVault, Kid, KidVault, PublicKey, Secret};
 
 impl KidVault for SoftwareVault {
     fn get_secret_by_kid(&self, kid: &str) -> ockam_core::Result<Secret> {
@@ -18,5 +18,38 @@ impl KidVault for SoftwareVault {
             .0;
 
         Ok(Secret::new(*index))
+    }
+
+    fn compute_kid_for_public_key(&self, public_key: &PublicKey) -> ockam_core::Result<Kid> {
+        let kid = self.sha256(public_key.as_ref())?;
+        Ok(hex::encode(kid))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::SoftwareVault;
+    use ockam_vault_core::{
+        KidVault, SecretAttributes, SecretPersistence, SecretType, SecretVault,
+        CURVE25519_SECRET_LENGTH,
+    };
+
+    #[test]
+    fn get_by_kid() {
+        let mut vault = SoftwareVault::new();
+
+        let attributes = SecretAttributes {
+            stype: SecretType::Curve25519,
+            persistence: SecretPersistence::Ephemeral,
+            length: CURVE25519_SECRET_LENGTH,
+        };
+
+        let secret = vault.secret_generate(attributes).unwrap();
+        let public = vault.secret_public_key_get(&secret).unwrap();
+
+        let kid = vault.compute_kid_for_public_key(&public).unwrap();
+        let secret2 = vault.get_secret_by_kid(&kid).unwrap();
+
+        assert_eq!(secret.index(), secret2.index());
     }
 }
--- implementations/rust/ockam/ockam_vault_core/src/kid_vault.rs
+++ implementations/rust/ockam/ockam_vault_core/src/kid_vault.rs
@@ -1,8 +1,11 @@
 use crate::secret::Secret;
+use crate::{Kid, PublicKey};
 use zeroize::Zeroize;
 
 /// Key id related vault functionality
 pub trait KidVault: Zeroize {
     /// Return [`Secret`] for given key id
     fn get_secret_by_kid(&self, kid: &str) -> ockam_core::Result<Secret>;
+    /// Return Kid for given public key
+    fn compute_kid_for_public_key(&self, public_key: &PublicKey) -> ockam_core::Result<Kid>;
 }
