cdf343c5b3b5bb6b347697208a08807bbdb7f704
robinbudd <robin@ockam.io>
2021-02-03 15:08:57 -0800

    refactor(rust): tcp_transport - rename "new()" to "create"

--- implementations/rust/ockam/ockam_transport_tcp/src/traits.rs
+++ implementations/rust/ockam/ockam_transport_tcp/src/traits.rs
@@ -15,7 +15,6 @@ pub trait Connection {
 
     /// Receives a message.
     async fn receive(&mut self, message: &mut [u8]) -> Result<usize, String>;
-    //    fn close(&mut self);
 }
 
 /// The `Listerner` trait represents transport connection listeners.
