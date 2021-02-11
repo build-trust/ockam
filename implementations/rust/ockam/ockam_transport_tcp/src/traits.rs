4d447bb4b624687874e0289e9197ea3dfe2efdf2
robinbudd <robin@ockam.io>
2021-02-11 06:30:56 -0800

    chore(rust): resolve minor merge conflicts in ockam_transport_tcp

--- implementations/rust/ockam/ockam_transport_tcp/src/traits.rs
+++ implementations/rust/ockam/ockam_transport_tcp/src/traits.rs
@@ -1,25 +1,22 @@
 extern crate alloc;
-
 use crate::error::Error;
-use alloc::sync::Arc;
 use async_trait::async_trait;
-use tokio::sync::Mutex;
 
 /// The `Connection` trait represents transport connections.
 #[async_trait]
 pub trait Connection {
     /// Establishes the transport connection.
-    async fn connect(&mut self) -> Result<(), String>;
+    async fn connect(&mut self) -> Result<(), Error>;
 
     /// Sends a message.
-    async fn send(&mut self, message: &[u8]) -> Result<usize, String>;
+    async fn send(&mut self, message: &[u8]) -> Result<usize, Error>;
 
     /// Receives a message.
-    async fn receive(&mut self, message: &mut [u8]) -> Result<usize, String>;
+    async fn receive(&mut self, message: &mut [u8]) -> Result<usize, Error>;
 }
 
 /// The `Listerner` trait represents transport connection listeners.
 #[async_trait]
 pub trait Listener {
-    async fn accept(&mut self) -> Result<Arc<Mutex<dyn Connection + Send>>, Error>;
+    async fn accept(&mut self) -> Result<Box<dyn Connection + Send>, Error>;
 }
