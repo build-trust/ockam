a938902e1719955e20273ad9aed87b8a9786cffd
Oleksandr Deundiak <olek@ockam.io>
2021-02-18 21:08:48 +0200

    feat(rust): rename signer_vault -> signer in ockam_vault_core

--- implementations/rust/ockam/ockam_vault_core/src/signer.rs
+++ implementations/rust/ockam/ockam_vault_core/src/signer.rs
@@ -1,8 +1,8 @@
 use crate::secret::Secret;
 use zeroize::Zeroize;
 
-/// Signing vault functionality
-pub trait SignerVault: Zeroize {
+/// Signing functionality
+pub trait Signer: Zeroize {
     /// Generate a signature  for given data using given secret key
     fn sign(&mut self, secret_key: &Secret, data: &[u8]) -> ockam_core::Result<[u8; 64]>;
 }
