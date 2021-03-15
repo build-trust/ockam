30779f540bc8e8b9dcf2e9fa7c1ec09cc0b1c78a
Katharina Fey <kookie@spacekookie.de>
2021-03-15 14:33:07 +0100

    feat(rust): re-implement tcp message sending and receiving

--- implementations/rust/ockam/ockam_transport_tcp/src/init.rs
+++ implementations/rust/ockam/ockam_transport_tcp/src/init.rs
@@ -1,11 +1,15 @@
 use crate::{TcpRecvWorker, TcpSendWorker};
 use ockam::{Address, Context, Result};
-use std::net::SocketAddr;
+use std::{
+    net::SocketAddr,
+    sync::{atomic::AtomicBool, Arc},
+};
 use tokio::net::TcpStream;
 
 pub struct WorkerPair {
     tx_addr: Address,
     rx_addr: Address,
+    run: Arc<AtomicBool>,
 }
 
 impl WorkerPair {
@@ -20,11 +24,12 @@ impl WorkerPair {
         Self {
             tx_addr: format!("{}_tx", addr).into(),
             rx_addr: format!("{}_rx", addr).into(),
+            run: Arc::new(AtomicBool::new(true)),
         }
     }
 }
 
-/// Start a new pair of Tcp connection workers
+/// Start a new pair of TCP connection workers
 ///
 /// One worker handles outgoing messages, while another handles
 /// incoming messages.  The local worker address is chosen based on
@@ -38,16 +43,28 @@ where
     // TODO: make i/o errors into ockam_error
     let stream = TcpStream::connect(peer.clone()).await.unwrap();
 
+    let WorkerPair {
+        rx_addr,
+        tx_addr,
+        run,
+    } = WorkerPair::from_peer(&peer);
+
     // Create two workers based on the split TCP I/O streams
     let (rx, tx) = stream.into_split();
     let sender = TcpSendWorker { tx };
-    let receiver = TcpRecvWorker { rx };
+    let receiver = TcpRecvWorker {
+        rx,
+        run: Arc::clone(&run),
+    };
 
     // Derive local worker addresses, and start them
-    let WorkerPair { rx_addr, tx_addr } = WorkerPair::from_peer(&peer);
     ctx.start_worker(tx_addr.clone(), sender).await?;
     ctx.start_worker(rx_addr.clone(), receiver).await?;
 
     // Return a handle to the worker pair
-    Ok(WorkerPair { rx_addr, tx_addr })
+    Ok(WorkerPair {
+        rx_addr,
+        tx_addr,
+        run,
+    })
 }
--- implementations/rust/ockam/ockam_transport_tcp/src/sender.rs
+++ implementations/rust/ockam/ockam_transport_tcp/src/sender.rs
@@ -1,8 +1,36 @@
+use crate::TcpError;
 use ockam::{async_worker, Context, Result, Routed, TransportMessage, Worker};
-use tokio::net::tcp::OwnedWriteHalf;
+use tokio::{io::AsyncWriteExt, net::tcp::OwnedWriteHalf};
 
+/// A TCP sending message worker
+///
+/// Create this worker type by calling
+/// [`start_tcp_worker`](crate::start_tcp_worker)!
+///
+/// This half of the worker is created when spawning a new connection
+/// worker pair, and listens for messages from the node message system
+/// to dispatch to a remote peer.
 pub struct TcpSendWorker {
     pub(crate) tx: OwnedWriteHalf,
+}
+
+fn prepare_message(msg: TransportMessage) -> Result<Vec<u8>> {
+    let mut msg_buf = serde_bare::to_vec(&msg).map_err(|_| TcpError::SendBadMessage)?;
+
+    // Create a buffer that includes the message length in big endian
+    let mut len = (msg_buf.len() as u16).to_be_bytes().to_vec();
+
+    // Fun fact: reversing a vector in place, appending the length,
+    // and then reversing it again is faster for large message sizes
+    // than adding the large chunk of data.
+    //
+    // https://play.rust-lang.org/?version=stable&mode=release&edition=2018&gist=8669a640004ac85c7be38b19e3e73dcb
+    msg_buf.reverse();
+    len.reverse();
+    msg_buf.append(&mut len);
+    msg_buf.reverse();
+
+    Ok(msg_buf)
 }
 
 #[async_worker]
@@ -12,7 +40,18 @@ impl Worker for TcpSendWorker {
 
     // TcpSendWorker will receive messages from the TcpRouter to send
     // across the TcpStream to our friend
-    async fn handle_message(&mut self, _: &mut Context, _: Routed<TransportMessage>) -> Result<()> {
-        Ok(())
+    async fn handle_message(
+        &mut self,
+        _: &mut Context,
+        msg: Routed<TransportMessage>,
+    ) -> Result<()> {
+        // Create a message buffer with pre-pended length
+        let msg = prepare_message(msg.take())?;
+
+        match self.tx.write(msg.as_slice()).await {
+            Ok(_) => Ok(()),
+            // TODO: match different error types here!
+            Err(_) => Err(TcpError::SendBadMessage.into()),
+        }
     }
 }
