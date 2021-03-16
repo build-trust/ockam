4e65c4023a95614a6112f12684a821a1394eae89
Katharina Fey <kookie@spacekookie.de>
2021-03-16 17:51:40 +0100

    feat(rust): add tcp routing example on new architecture

---
+++ implementations/rust/examples/tcp/examples/network_echo_server.rs
@@ -0,0 +1,56 @@
+//! This example is part of `network_echo`
+//!
+//! You need to start this binary first, before letting the
+//! `network_echo_client` connect to it.
+
+#[macro_use]
+extern crate tracing;
+
+use ockam::{async_worker, Context, Result, Routed, Worker};
+use ockam_transport_tcp::TcpRouter;
+use std::net::SocketAddr;
+
+struct Responder;
+
+#[async_worker]
+impl Worker for Responder {
+    type Context = Context;
+    type Message = String;
+
+    async fn handle_message(&mut self, ctx: &mut Context, msg: Routed<String>) -> Result<()> {
+        info!("Responder: {}", msg);
+        ctx.send_message(msg.reply(), msg.take()).await?;
+        Ok(())
+    }
+}
+
+fn get_bind_addr() -> SocketAddr {
+    std::env::args()
+        .skip(1)
+        .take(1)
+        .next()
+        .unwrap_or(format!("127.0.0.1:10222"))
+        .parse()
+        .ok()
+        .unwrap_or_else(|| {
+            error!("Failed to parse socket address!");
+            eprintln!("Usage: network_echo_server <ip>:<port>");
+            std::process::exit(1);
+        })
+}
+
+#[ockam::node]
+async fn main(mut ctx: Context) -> Result<()> {
+    // Get either the default socket address, or a user-input
+    let bind_addr = get_bind_addr();
+    debug!("Binding to: {}", bind_addr);
+
+    // Create a new _binding_ TcpRouter
+    let _r = TcpRouter::bind(&ctx, bind_addr).await?;
+
+    // Create the responder worker
+    ctx.start_worker("simple.responder", Responder).await?;
+
+    // The server never shuts down
+    Ok(())
+}
