21ce12329051f4689de68005045bd58fd2986842
Katharina Fey <kookie@spacekookie.de>
2021-03-26 15:10:49 +0100

    chore(rust): handle message payloads without inner length

--- implementations/rust/ockam/ockam_node/src/relay.rs
+++ implementations/rust/ockam/ockam_node/src/relay.rs
@@ -95,10 +95,32 @@ where
         }
     }
 
+    fn try_decode(mut v: Vec<u8>) -> Result<M> {
+        M::decode(&v).or_else(|_| {
+            trace!("Parsing payload without inner length...");
+
+            // This condition means we _may_ be dealing with a payload
+            // sent by a non-Rust implementation.  In this case we
+            // prepend the length of the mesage to the vector and try
+            // again.  I know it's bad, but as long as we don't have
+            // properly specified payload encoding this is what will
+            // have to do.
+            let mut new_v = vec![v.len() as u8]; // FIXME: does not handle message sizes over 255
+            trace!("New message length: {:?}", new_v);
+
+            new_v.append(&mut v);
+            M::decode(&new_v)
+        })
+    }
+
     /// Convenience function to handle an incoming direct message
     #[inline]
     fn handle_direct(&mut self, msg: TransportMessage) -> Result<(M, Route)> {
-        M::decode(&msg.payload)
+        let TransportMessage {
+            payload, return_, ..
+        } = msg;
+
+        Self::try_decode(payload)
             .map_err(|e| {
                 error!(
                     "Failed to decode message payload for worker {}",
@@ -106,7 +128,7 @@ where
                 );
                 e.into()
             })
-            .map(|m| (m, msg.return_.clone()))
+            .map(|m| (m, return_.clone()))
     }
 
     #[inline]
