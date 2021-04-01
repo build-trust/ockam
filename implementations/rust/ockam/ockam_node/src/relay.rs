b0d4923295f07c12e3b459ae9236a04bd2572f3d
Katharina Fey <kookie@spacekookie.de>
2021-04-01 12:59:42 +0300

    feat(rust): use special message parsing for context receive

--- implementations/rust/ockam/ockam_node/src/relay.rs
+++ implementations/rust/ockam/ockam_node/src/relay.rs
@@ -15,7 +15,7 @@
 //! The `Relay` is then responsible for turning the message back into
 //! a type and notifying the companion actor.
 
-use crate::{Context, Mailbox};
+use crate::{parser, Context, Mailbox};
 use ockam_core::{
     Address, Message, Result, Route, Routed, RouterMessage, TransportMessage, Worker,
 };
@@ -95,24 +95,6 @@ where
         }
     }
 
-    fn try_decode(mut v: Vec<u8>) -> Result<M> {
-        M::decode(&v).or_else(|_| {
-            trace!("Parsing payload without inner length...");
-
-            // This condition means we _may_ be dealing with a payload
-            // sent by a non-Rust implementation.  In this case we
-            // prepend the length of the mesage to the vector and try
-            // again.  I know it's bad, but as long as we don't have
-            // properly specified payload encoding this is what will
-            // have to do.
-            let mut new_v = vec![v.len() as u8]; // FIXME: does not handle message sizes over 255
-            trace!("New message length: {:?}", new_v);
-
-            new_v.append(&mut v);
-            M::decode(&new_v)
-        })
-    }
-
     /// Convenience function to handle an incoming direct message
     #[inline]
     fn handle_direct(&mut self, msg: TransportMessage) -> Result<(M, Route)> {
@@ -120,7 +102,7 @@ where
             payload, return_, ..
         } = msg;
 
-        Self::try_decode(payload)
+        parser::message::<M>(payload)
             .map_err(|e| {
                 error!(
                     "Failed to decode message payload for worker {}",
