bd0b12cfd26cb66212ee94db844490636419ce3e
Oleksandr Deundiak <olek@ockam.io>
2021-04-02 17:16:59 +0300

    chore(rust): add step 5 guide example

---
+++ implementations/rust/ockam/ockam_examples/examples/step-5-server.rs
@@ -0,0 +1,49 @@
+use ockam::{async_worker, Context, Result, Route, Routed, Worker};
+use ockam_transport_tcp::{self as tcp, TcpRouter};
+use std::net::SocketAddr;
+
+struct EchoService;
+
+const HUB_ADDRESS: &str = "127.0.0.1:4000";
+
+#[async_worker]
+impl Worker for EchoService {
+    type Message = String;
+    type Context = Context;
+
+    async fn initialize(&mut self, ctx: &mut Self::Context) -> Result<()> {
+        // Send a "register" event to the Hub. The hub will reply with a forwarding address.
+        ctx.send_message(
+            Route::new()
+                .append_t(1, HUB_ADDRESS)
+                .append("forwarding_service"),
+            "register".to_string(),
+        )
+        .await
+    }
+
+    async fn handle_message(&mut self, ctx: &mut Context, msg: Routed<String>) -> Result<()> {
+        if &msg.as_str() == &"register" {
+            let address = msg.reply().recipient().to_string();
+            println!(
+                "echo_service: My address on the hub is {}",
+                address.strip_prefix("0:").unwrap()
+            );
+            Ok(())
+        } else {
+            println!("echo_service: {}", msg);
+            ctx.send_message(msg.reply(), msg.take()).await
+        }
+    }
+}
+
+#[ockam::node]
+async fn main(ctx: Context) -> Result<()> {
+    let router = TcpRouter::register(&ctx).await?;
+    let hub_connection =
+        tcp::start_tcp_worker(&ctx, HUB_ADDRESS.parse::<SocketAddr>().unwrap()).await?;
+
+    router.register(&hub_connection).await?;
+
+    ctx.start_worker("echo_service", EchoService).await
+}
