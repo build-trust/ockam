34f4df4c69dd96174226f64140116ab5f1865caa
Jared Stanbrough <jared@ockam.io>
2021-04-07 11:37:37 -0700

    feat: script to print all crate versions

---
+++ tools/scripts/crate-versions.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+if [ -z "$OCKAM_ROOT" ]
+then
+  echo "Please set the OCKAM_ROOT environment variable to the ockam repository root directory."
+  exit 0
+fi
+
+pushd $OCKAM_ROOT/implementations/rust/ockam/ >/dev/null
+echo "crate,version"
+for d in *
+do 
+  echo -n "$d,"
+  perl -ne '/^version = "([^"]+)"/ and print "$1\n"' < $d/Cargo.toml 
+done
+popd >/dev/null
