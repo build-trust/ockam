5bfe93651d4c656b472fbc77bd35e74c5eaab672
Mrinal Wadhwa <mrinal@ockam.io>
2021-04-13 08:28:18 -0700

    docs: use address instead of primary address

--- examples/rust/get_started/src/hop.rs
+++ examples/rust/get_started/src/hop.rs
@@ -10,11 +10,11 @@ impl Worker for Hop {
     /// This handle function takes any incoming message and forwards
     /// it to the next hop in it's route
     async fn handle_message(&mut self, ctx: &mut Context, msg: Routed<Any>) -> Result<()> {
-        println!("Address: {}, Received: {}", ctx.primary_address(), msg);
+        println!("Address: {}, Received: {}", ctx.address(), msg);
 
         let mut msg = msg.into_transport_message();
         msg.onward_route.step()?;
-        msg.return_route.modify().prepend(ctx.primary_address());
+        msg.return_route.modify().prepend(ctx.address());
         ctx.forward(msg).await
     }
 }
