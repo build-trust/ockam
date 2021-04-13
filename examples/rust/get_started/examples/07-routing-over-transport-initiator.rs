c1584f062f9f210d14bb47348fdd12a7ad30b9d6
Mrinal Wadhwa <mrinal@ockam.io>
2021-04-13 08:35:13 -0700

    docs: add routing over tcp example

---
+++ examples/rust/get_started/examples/07-routing-over-transport-initiator.rs
@@ -0,0 +1,21 @@
+use ockam::{Context, Result, Route};
+use ockam_transport_tcp::{TcpTransport, TCP};
+
+#[ockam::node]
+async fn main(mut ctx: Context) -> Result<()> {
+    TcpTransport::create(&ctx, "127.0.0.1:4000").await?;
+
+    // Send a message to the echoer worker, on a different node, over a tcp transport
+    ctx.send(
+        Route::new()
+            .append_t(TCP, "127.0.0.1:4000")
+            .append("echoer"),
+        "Hello Ockam!".to_string()
+    ).await?;
+
+    // Wait to receive a reply and print it.
+    let reply = ctx.receive::<String>().await?;
+    println!("Initiator Received: {}", reply); // should print "Hello Ockam!"
+
+    ctx.stop().await
+}
