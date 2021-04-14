b3b446522e7c5f8598ea5aa4d7bb5bde23094561
Mrinal Wadhwa <mrinal@ockam.io>
2021-04-14 15:41:35 -0700

    docs: improve new worker step it rust guide and examples

--- examples/rust/get_started/examples/02-worker.rs
+++ examples/rust/get_started/examples/02-worker.rs
@@ -3,10 +3,10 @@ use ockam_get_started::Echoer;
 
 #[ockam::node]
 async fn main(mut ctx: Context) -> Result<()> {
-    // Start an echoer worker.
+    // Start an Echoer worker at address "echoer"
     ctx.start_worker("echoer", Echoer).await?;
 
-    // Send a message to the echoer worker.
+    // Send a message to the "echoer" worker.
     ctx.send("echoer", "Hello Ockam!".to_string()).await?;
 
     // Wait to receive a reply and print it.
