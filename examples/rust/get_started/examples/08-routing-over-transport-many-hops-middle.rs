450dcf1c0830a09c5580c69041a977f989ee19a5
Katharina Fey <kookie@spacekookie.de>
2021-04-15 10:46:02 +0200

    fix(rust): update multi-hop tcp example to updated transport api

--- examples/rust/get_started/examples/08-routing-over-transport-many-hops-middle.rs
+++ examples/rust/get_started/examples/08-routing-over-transport-many-hops-middle.rs
@@ -3,8 +3,8 @@ use ockam_transport_tcp::TcpTransport;
 
 #[ockam::node]
 async fn main(ctx: Context) -> Result<()> {
-    TcpTransport::create_listener(&ctx, "127.0.0.1:4000").await?;
-    TcpTransport::create(&ctx, "127.0.0.1:6000").await?;
+    let tcp = TcpTransport::create_listener(&ctx, "127.0.0.1:4000").await?;
+    tcp.connect("127.0.0.1:6000").await?;
 
     // This node never shuts down.
     Ok(())
