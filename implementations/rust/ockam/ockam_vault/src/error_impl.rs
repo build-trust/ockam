b187fc11547de8d0c6fccb9d563f5337bf36501d
Oleksandr Deundiak <olek@ockam.io>
2021-04-20 18:11:35 +0300

    feat(rust): make error_domain trait function use self

--- implementations/rust/ockam/ockam_vault/src/error_impl.rs
+++ implementations/rust/ockam/ockam_vault/src/error_impl.rs
@@ -1,8 +1,14 @@
 use crate::{SoftwareVault, VaultError};
 use ockam_vault_core::ErrorVault;
 
-impl ErrorVault for SoftwareVault {
-    fn error_domain() -> &'static str {
+impl SoftwareVault {
+    pub fn error_domain_static() -> &'static str {
         VaultError::DOMAIN_NAME
+    }
+}
+
+impl ErrorVault for SoftwareVault {
+    fn error_domain(&self) -> &'static str {
+        Self::error_domain_static()
     }
 }
--- implementations/rust/ockam/ockam_vault_core/src/error_vault.rs
+++ implementations/rust/ockam/ockam_vault_core/src/error_vault.rs
@@ -1,5 +1,5 @@
 use zeroize::Zeroize;
 
 pub trait ErrorVault: Zeroize {
-    fn error_domain() -> &'static str;
+    fn error_domain(&self) -> &'static str;
 }
