90c0234a65b933af1edd6a21347aee324c45f89a
Oleksandr Deundiak <olek@ockam.io>
2021-04-22 01:17:57 +0300

    feat(rust): update channel example

--- implementations/rust/ockam/ockam_examples/example_projects/channel/src/bin/client.rs
+++ implementations/rust/ockam/ockam_examples/example_projects/channel/src/bin/client.rs
@@ -1,19 +1,17 @@
 use channel_examples::client_worker::Client;
-use ockam::{Result, Vault};
-use ockam_transport_tcp::TcpTransport;
-use ockam_vault::SoftwareVault;
+use ockam::{Result, TcpTransport, Vault};
 
 #[ockam::node]
 async fn main(ctx: ockam::Context) -> Result<()> {
-    let vault_address = Vault::create(&ctx, SoftwareVault::default()).await?;
+    let vault_address = Vault::create(&ctx).await?;
 
-    let hub_addr = "104.42.24.183:4000";
+    let hub_addr = "40.78.99.34:4000";
 
     // Create and register a connection worker pair
     let tcp = TcpTransport::create(&ctx).await?;
     tcp.connect(hub_addr).await?;
 
-    let client = Client::new(hub_addr, "6841596d".to_string(), vault_address);
+    let client = Client::new(hub_addr, "a6a7be76".to_string(), vault_address);
 
     ctx.start_worker("echo_client", client).await?;
 
