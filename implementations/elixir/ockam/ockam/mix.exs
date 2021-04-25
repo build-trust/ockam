133853bd73ee780e0762cb6bd1f2a23472abafe9
Gonzalo Gabriel Jiménez Fuentes <iam@mendrugory.com>
2021-04-25 09:04:54 +0200

    feat(elixir): Token Lease Manager Prototype
    Prototype to have a flexible Token lease manager which could work with diferent Cloud APIs.
    
    Removing non-used dependency

--- implementations/elixir/ockam/ockam/mix.exs
+++ implementations/elixir/ockam/ockam/mix.exs
@@ -47,7 +47,6 @@ defmodule Ockam.MixProject do
     [
       {:bare, "~> 0.1.1"},
       {:gen_state_machine, "~> 3.0"},
-      {:httpoison, "~> 1.8"},
       {:ockam_vault_software, path: "../ockam_vault_software", optional: true},
       {:telemetry, "~> 0.4.2", optional: true},
       {:ranch, "~> 2.0", optional: true},
