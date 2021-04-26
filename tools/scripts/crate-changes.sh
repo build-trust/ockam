b6bdb504f6c5d3b04af365b0b1da49473a1f4e76
Jared Stanbrough <jared@ockam.io>
2021-04-26 11:00:34 -0700

    feat(rust): ockam_core v0.11.1

--- tools/scripts/crate-changes.sh
+++ tools/scripts/crate-changes.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-if [ -z "$OCKAM_ROOT" ]
+if [ -z "$OCKAM_HOME" ]
 then
   echo "Please set the OCKAM_ROOT environment variable to the ockam repository root directory."
   exit 0
 fi
 
-pushd $OCKAM_ROOT/implementations/rust/ockam/ >/dev/null
+pushd $OCKAM_HOME/implementations/rust/ockam/ >/dev/null
 for d in *
 do
   echo -n "git log ${d}_v"
