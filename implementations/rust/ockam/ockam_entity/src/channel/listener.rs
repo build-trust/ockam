f2606e8e94297dbaef9052a5d55af6168c02f0bb
Jared Stanbrough <jared@ockam.io>
2021-05-13 11:27:06 -0700

    fix(rust): fix ctx var rename in shutdown

--- implementations/rust/ockam/ockam_entity/src/channel/listener.rs
+++ implementations/rust/ockam/ockam_entity/src/channel/listener.rs
@@ -43,7 +43,7 @@ impl<V: ProfileVault> Worker for ProfileChannelListener<V> {
         Ok(())
     }
 
-    async fn shutdown(&mut self, _ctx: &mut Self::Context) -> Result<()> {
+    async fn shutdown(&mut self, ctx: &mut Self::Context) -> Result<()> {
         ctx.stop_worker(self.listener_address.take().unwrap()).await
     }
 
