d815e0ca2b5f0effa223e0ee69dfa24377e4dc1b
Wojciech Wais <wojciech.wais@gmail.com>
2021-05-30 23:41:32 +0000

    chore(rust): add secure channel tunneling tests

--- implementations/rust/ockam/ockam_entity/src/channel.rs
+++ implementations/rust/ockam/ockam_entity/src/channel.rs
@@ -91,9 +91,11 @@ mod test {
     use crate::{Entity, SecureChannels};
     use ockam_core::{route, Message};
     use ockam_vault_sync_core::Vault;
+    use serial_test::serial;
 
     #[test]
-    fn disable_test_channel() {
+    #[serial]
+    fn test_channel() {
         let (mut ctx, mut executor) = ockam_node::start_node();
         executor
             .execute(async move {
@@ -139,6 +141,181 @@ mod test {
                 assert_eq!(local_info.their_profile_id(), &bob.identifier().unwrap());
 
                 assert_eq!("Hello, Alice!", msg.body());
+
+                ctx.stop().await.unwrap();
+            })
+            .unwrap();
+    }
+
+    #[test]
+    #[serial]
+    fn test_tunneled_secure_channel_works() {
+        let (mut ctx, mut executor) = ockam_node::start_node();
+        executor
+            .execute(async move {
+                let vault = Vault::create(&ctx).unwrap();
+
+                let mut alice = Entity::create(&ctx, &vault).unwrap();
+                let mut bob = Entity::create(&ctx, &vault).unwrap();
+
+                let alice_trust_policy = IdentifierTrustPolicy::new(bob.identifier().unwrap());
+                let bob_trust_policy = IdentifierTrustPolicy::new(alice.identifier().unwrap());
+
+                bob.create_secure_channel_listener("bob_listener", bob_trust_policy.clone())
+                    .unwrap();
+
+                let alice_channel = alice
+                    .create_secure_channel(route!["bob_listener"], alice_trust_policy.clone())
+                    .unwrap();
+
+                bob.create_secure_channel_listener("bob_another_listener", bob_trust_policy)
+                    .unwrap();
+
+                let alice_another_channel = alice
+                    .create_secure_channel(
+                        route![alice_channel, "bob_another_listener"],
+                        alice_trust_policy,
+                    )
+                    .unwrap();
+
+                ctx.send(
+                    route![alice_another_channel, ctx.address()],
+                    "Hello, Bob!".to_string(),
+                )
+                .await
+                .unwrap();
+                let msg = ctx.receive::<String>().await.unwrap().take();
+                let return_route = msg.return_route();
+                assert_eq!("Hello, Bob!", msg.body());
+
+                ctx.send(return_route, "Hello, Alice!".to_string())
+                    .await
+                    .unwrap();
+                assert_eq!(
+                    "Hello, Alice!",
+                    ctx.receive::<String>().await.unwrap().take().body()
+                );
+
+                ctx.stop().await.unwrap();
+            })
+            .unwrap();
+    }
+
+    #[test]
+    #[serial]
+    fn test_double_tunneled_secure_channel_works() {
+        let (mut ctx, mut executor) = ockam_node::start_node();
+        executor
+            .execute(async move {
+                let vault = Vault::create(&ctx).unwrap();
+
+                let mut alice = Entity::create(&ctx, &vault).unwrap();
+                let mut bob = Entity::create(&ctx, &vault).unwrap();
+
+                let alice_trust_policy = IdentifierTrustPolicy::new(bob.identifier().unwrap());
+                let bob_trust_policy = IdentifierTrustPolicy::new(alice.identifier().unwrap());
+
+                bob.create_secure_channel_listener("bob_listener", bob_trust_policy.clone())
+                    .unwrap();
+
+                let alice_channel = alice
+                    .create_secure_channel(route!["bob_listener"], alice_trust_policy.clone())
+                    .unwrap();
+
+                bob.create_secure_channel_listener(
+                    "bob_another_listener",
+                    bob_trust_policy.clone(),
+                )
+                .unwrap();
+
+                let alice_another_channel = alice
+                    .create_secure_channel(
+                        route![alice_channel, "bob_another_listener"],
+                        alice_trust_policy.clone(),
+                    )
+                    .unwrap();
+
+                bob.create_secure_channel_listener("bob_yet_another_listener", bob_trust_policy)
+                    .unwrap();
+
+                let alice_yet_another_channel = alice
+                    .create_secure_channel(
+                        route![alice_another_channel, "bob_yet_another_listener"],
+                        alice_trust_policy,
+                    )
+                    .unwrap();
+
+                ctx.send(
+                    route![alice_yet_another_channel, ctx.address()],
+                    "Hello, Bob!".to_string(),
+                )
+                .await
+                .unwrap();
+                let msg = ctx.receive::<String>().await.unwrap().take();
+                let return_route = msg.return_route();
+                assert_eq!("Hello, Bob!", msg.body());
+
+                ctx.send(return_route, "Hello, Alice!".to_string())
+                    .await
+                    .unwrap();
+                assert_eq!(
+                    "Hello, Alice!",
+                    ctx.receive::<String>().await.unwrap().take().body()
+                );
+
+                ctx.stop().await.unwrap();
+            })
+            .unwrap();
+    }
+
+    #[test]
+    #[serial]
+    fn test_many_times_tunneled_secure_channel_works() {
+        let (mut ctx, mut executor) = ockam_node::start_node();
+        executor
+            .execute(async move {
+                let vault = Vault::create(&ctx).unwrap();
+
+                let mut alice = Entity::create(&ctx, &vault).unwrap();
+                let mut bob = Entity::create(&ctx, &vault).unwrap();
+
+                let alice_trust_policy = IdentifierTrustPolicy::new(bob.identifier().unwrap());
+                let bob_trust_policy = IdentifierTrustPolicy::new(alice.identifier().unwrap());
+
+                let n = rand::random::<u8>() % 5 + 4;
+                let mut channels = vec![];
+                for i in 0..n {
+                    bob.create_secure_channel_listener(i.to_string(), bob_trust_policy.clone())
+                        .unwrap();
+                    let channel_route: Route;
+                    if i > 0 {
+                        channel_route = route![channels.pop().unwrap(), i.to_string()];
+                    } else {
+                        channel_route = route![i.to_string()];
+                    }
+                    let alice_channel = alice
+                        .create_secure_channel(channel_route, alice_trust_policy.clone())
+                        .unwrap();
+                    channels.push(alice_channel);
+                }
+
+                ctx.send(
+                    route![channels.pop().unwrap(), ctx.address()],
+                    "Hello, Bob!".to_string(),
+                )
+                .await
+                .unwrap();
+                let msg = ctx.receive::<String>().await.unwrap().take();
+                let return_route = msg.return_route();
+                assert_eq!("Hello, Bob!", msg.body());
+
+                ctx.send(return_route, "Hello, Alice!".to_string())
+                    .await
+                    .unwrap();
+                assert_eq!(
+                    "Hello, Alice!",
+                    ctx.receive::<String>().await.unwrap().take().body()
+                );
 
                 ctx.stop().await.unwrap();
             })
