69a2d3929ce70c7888569285bdc7b9548b5ec740
Oleksandr Deundiak <olek@ockam.io>
2021-07-15 16:34:18 +0300

    chore(rust): update credentials example

---
+++ implementations/rust/ockam/ockam_examples/example_projects/credentials/src/bin/bob.rs
@@ -0,0 +1,61 @@
+use lib::{
+    read_line, DOOR_LISTENER_ADDRESS, DOOR_TCP_ADDRESS, DOOR_WORKER_ADDRESS, OFFICE_ISSUER_ADDRESS,
+    OFFICE_LISTENER_ADDRESS, OFFICE_TCP_ADDRESS,
+};
+use ockam::{
+    credential_attribute_values, credential_type, reveal_attributes, route, Context,
+    CredentialProtocol, Entity, EntityIdentifier, IdentifierTrustPolicy, Identity, Result,
+    SecureChannels, TcpTransport, Vault, TCP,
+};
+use std::convert::TryFrom;
+
+#[ockam::node]
+async fn main(ctx: Context) -> Result<()> {
+    let vault = Vault::create(&ctx)?;
+    let mut entity = Entity::create(&ctx, &vault)?;
+    println!("Bob id: {}", entity.identifier()?);
+
+    println!("Enter Office id: ");
+    let office_id = read_line();
+    let office_id = EntityIdentifier::try_from(office_id.as_str())?;
+
+    println!("Enter Door id: ");
+    let door_id = read_line();
+    let door_id = EntityIdentifier::try_from(door_id.as_str())?;
+
+    let tcp = TcpTransport::create(&ctx).await?;
+    tcp.connect(OFFICE_TCP_ADDRESS).await?;
+
+    let office_channel = entity.create_secure_channel(
+        route![(TCP, OFFICE_TCP_ADDRESS), OFFICE_LISTENER_ADDRESS],
+        IdentifierTrustPolicy::new(office_id.clone()),
+    )?;
+
+    let credential = entity.acquire_credential(
+        route![office_channel, OFFICE_ISSUER_ADDRESS],
+        &office_id,
+        credential_type!["TYPE_ID"; "door_id", (Number, "can_open_door")],
+        credential_attribute_values![door_id.clone().to_string(), 1],
+    )?;
+
+    println!("Bob got credential!");
+
+    tcp.connect(DOOR_TCP_ADDRESS).await?;
+
+    let door_channel = entity.create_secure_channel(
+        route![(TCP, DOOR_TCP_ADDRESS), DOOR_LISTENER_ADDRESS],
+        IdentifierTrustPolicy::new(door_id.clone()),
+    )?;
+
+    entity.present_credential(
+        route![door_channel, DOOR_WORKER_ADDRESS],
+        credential,
+        reveal_attributes!["door_id", "can_open_door"],
+    )?;
+
+    println!("Bob presented credential!");
+
+    // TODO: Send actual payload
+
+    Ok(())
+}
---
+++ implementations/rust/ockam/ockam_examples/example_projects/credentials/src/bin/office.rs
@@ -0,0 +1,35 @@
+use lib::{read_line, OFFICE_ISSUER_ADDRESS, OFFICE_LISTENER_ADDRESS, OFFICE_TCP_ADDRESS};
+use ockam::{
+    credential_type, Context, CredentialProtocol, Entity, EntityIdentifier, IdentifierTrustPolicy,
+    Identity, NoOpTrustPolicy, Profile, Result, SecureChannels, TcpTransport, Vault,
+};
+use std::convert::TryFrom;
+
+#[ockam::node]
+async fn main(ctx: Context) -> Result<()> {
+    let tcp = TcpTransport::create(&ctx).await?;
+    tcp.listen(OFFICE_TCP_ADDRESS).await?;
+
+    // The Office is an Issuer of credentials. In this case, permission to open doors.
+    let vault = Vault::create(&ctx)?;
+    let mut entity = Entity::create(&ctx, &vault)?; // TODO: add options to setup entity from creation
+                                                    // Rename to create_credential_issuance_key
+    entity.create_key(/* Move to Entity? */ Profile::CREDENTIALS_ISSUE)?;
+
+    println!("Office id: {}", entity.identifier()?);
+
+    entity.create_secure_channel_listener(OFFICE_LISTENER_ADDRESS, NoOpTrustPolicy)?;
+
+    println!("Enter Bob id: ");
+    let bob_id = read_line();
+    let bob_id = EntityIdentifier::try_from(bob_id.as_str())?;
+
+    entity.create_credential_issuance_listener(
+        OFFICE_ISSUER_ADDRESS,
+        credential_type!["TYPE_ID"; "door_id", (Number, "can_open_door")],
+        IdentifierTrustPolicy::new(bob_id.clone()),
+        // TODO: TrustPolicy doesn't have access to enough data about the requested credential to make a decision
+    )?;
+
+    Ok(())
+}
