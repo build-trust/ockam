9b1cf55adb848e90ecf05ad0b075db848da52f4b
Antoine van Gelder <antoine@ockam.io>
2021-07-22 22:00:56 +0200

    fix: apply failing lints for stream guide content

--- examples/rust/get_started/examples/14-stream-over-cloud-node-initiator.rs
+++ examples/rust/get_started/examples/14-stream-over-cloud-node-initiator.rs
@@ -11,21 +11,21 @@ async fn main(mut ctx: Context) -> Result<()> {
 
     // Create the stream client
     let (tx, _rx) = Stream::new(&ctx)?
-	.stream_service("stream")
-	.index_service("stream_index")
-	.client_id("stream-over-cloud-node-initiator")
-	.with_interval(Duration::from_millis(100))
-	.connect(
-	    route![(TCP, "127.0.0.1:4000")],
-	    // Stream name from THIS node to the OTHER node
-	    "test-a-b",
-	    // Stream name from the OTHER node to THIS node
-	    "test-b-a",
-	)
-	.await?;
+        .stream_service("stream")
+        .index_service("stream_index")
+        .client_id("stream-over-cloud-node-initiator")
+        .with_interval(Duration::from_millis(100))
+        .connect(
+            route![(TCP, "127.0.0.1:4000")],
+            // Stream name from THIS node to the OTHER node
+            "test-a-b",
+            // Stream name from the OTHER node to THIS node
+            "test-b-a",
+        )
+        .await?;
 
     ctx.send(tx.to_route().append("echoer"), "Hello World!".to_string())
-	.await?;
+        .await?;
 
     let reply = ctx.receive_block::<String>().await?;
     println!("Reply via stream: {}", reply);
