9ba17b5d581f331b777d8cc72d55d24e219c05b5
Daniil Fedotov <daniil@ockam.io>
2021-07-30 15:49:25 -0400

    docs: create project for kafka example docker image

---
+++ examples/rust/ockam_kafka/examples/example.rs
@@ -0,0 +1,28 @@
+// This node routes a message, to a worker on a cloud node, over the tcp transport.
+
+use ockam::{route, Context, Result, TcpTransport, TCP};
+
+#[ockam::node]
+async fn main(mut ctx: Context) -> Result<()> {
+    // Create a cloud node by going to https://hub.ockam.network
+    let cloud_node_tcp_address = "1.node.ockam.network:4000";
+
+    // Initialize the TCP Transport.
+    let _tcp = TcpTransport::create(&ctx).await?;
+
+    // Send a message to the `echo_service` worker on your cloud node.
+    ctx.send(
+        // route to the echo_service worker on your cloud node
+        route![(TCP, cloud_node_tcp_address), "echo_service"],
+        // the message you want echo-ed back
+        "Hello Ockam!".to_string(),
+    )
+    .await?;
+
+    // Wait to receive the echo and print it.
+    let msg = ctx.receive::<String>().await?;
+    println!("App Received: '{}'", msg); // should print "Hello Ockam!"
+
+    // Stop the node.
+    ctx.stop().await
+}
