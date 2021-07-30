b12d5f75faa373013974c65854691c840cd9609f
Jared Stanbrough <jared@ockam.io>
2021-07-30 12:56:45 -0700

    ci: utility for replacing example source in markdown

---
+++ tools/guide/example_blocks/src/main.rs
@@ -0,0 +1,51 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader, Read};
+
+fn print_file(file: &str) {
+    let examples = std::env::var("EXAMPLES_DIR")
+        .expect("set EXAMPLES_DIR environment variable to source location");
+    let file = format!("{}/{}", examples, file);
+
+    let err = format!("Can't find example source {}", file);
+    let mut file = File::open(file).expect(err.as_str());
+
+    let mut content = String::new();
+    file.read_to_string(&mut content).expect("short read");
+    println!("{}", content);
+}
+
+fn main() {
+    let file: String = std::env::args()
+        .skip(1)
+        .next()
+        .expect("missing file argument");
+
+    let file = File::open(file).expect("unable to open file");
+    let file = BufReader::new(file);
+
+    let mut in_example = false;
+
+    for line in file.lines() {
+        match line {
+            Ok(line) => {
+                let example_start = line.starts_with("// examples/");
+                let block_end = line.starts_with("```");
+
+                if in_example {
+                    if block_end {
+                        in_example = false;
+                        println!("{}", line);
+                    }
+                } else {
+                    println!("{}", line);
+                    if example_start {
+                        in_example = true;
+                        let example_name = line.split("/").last().expect("no example filename");
+                        print_file(example_name);
+                    }
+                }
+            }
+            _ => break,
+        }
+    }
+}
