bee7ec35e53b6fed6e38ad0e25b72522a4e4d256
Jared Stanbrough <jared@ockam.io>
2021-08-23 12:23:45 -0700

    feat(rust): initial publish of ockam_node_nostd

--- implementations/rust/ockam/ockam_node_no_std/src/lib.rs
+++ implementations/rust/ockam/ockam_node_no_std/src/lib.rs
@@ -1,7 +1,9 @@
 #![cfg_attr(not(feature = "std"), no_std)]
+//! `no_std` implementation of Ockam Node
 
 use core::future::Future;
 
+/// Block on the execution of the given future.
 pub fn block_on<T>(future: impl Future<Output = T> + 'static + Send) -> T
 where
     T: Send + 'static,
