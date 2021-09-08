30850dc745c559e6751d9fbd7a3929f2f322852e
Oleksandr Deundiak <olek@ockam.io>
2021-09-08 23:55:48 +0300

    chore(rust): update inlet-outlet examples

--- examples/rust/secure_remote_access/examples/03-outlet.rs
+++ examples/rust/secure_remote_access/examples/03-outlet.rs
@@ -1,4 +1,4 @@
-use ockam::{route, Context, Result, TcpTransport, TCP};
+use ockam::{Context, Result, TcpTransport};
 use ockam::{Entity, SecureChannels, TrustEveryonePolicy, Vault};
 
 #[ockam::node]
@@ -6,18 +6,15 @@ async fn main(ctx: Context) -> Result<()> {
     // Initialize the TCP Transport.
     let tcp = TcpTransport::create(&ctx).await?;
 
-    // Create a Vault to store our cryptographic keys and an Entity to represent this Node.
-    // Then initiate a handshake with the secure channel listener on the node that has the
-    // TCP Transport Inlet.
-    //
-    // For this example, we know that the Inlet node is listening for Ockam Routing Messages
-    // over TCP at "127.0.0.1:4000" and its secure channel listener is
-    // at address: "secure_channel_listener_service".
+    // Create:
+    //   1. A Vault to store our cryptographic keys
+    //   2. An Entity to represent this Node
+    //   3. A Secure Channel Listener at Worker address - secure_channel_listener_service
+    //      that will wait for requests to start an Authenticated Key Exchange.
 
     let vault = Vault::create(&ctx)?;
     let mut e = Entity::create(&ctx, &vault)?;
-    let r = route![(TCP, "127.0.0.1:4000"), "secure_channel_listener_service"];
-    let channel = e.create_secure_channel(r, TrustEveryonePolicy)?;
+    e.create_secure_channel_listener("secure_channel_listener_service", TrustEveryonePolicy)?;
 
     // Expect first command line argument to be the TCP address of a target TCP server.
     // For example: 127.0.0.1:5000
@@ -38,14 +35,8 @@ async fn main(ctx: Context) -> Result<()> {
     let outlet_target = std::env::args().nth(1).expect("no outlet target given");
     tcp.create_outlet("outlet", outlet_target).await?;
 
-    // Send an Ockam Routing Message, via the secure channel, to the node that is
-    // running a TCP Transport Inlet.
-    //
-    // For this example we know that the main function of that node is waiting for a
-    // message from us. The Ockam Worker address of the main function is "app".
-
-    let r = route![channel, "app"];
-    ctx.send(r, "outlet".to_string()).await?;
+    // Create a TCP listener to receive Ockam Routing Messages from other ockam nodes.
+    tcp.listen("127.0.0.1:4000").await?;
 
     // We won't call ctx.stop() here,
     // so this program will keep running until you interrupt it with Ctrl-C.
