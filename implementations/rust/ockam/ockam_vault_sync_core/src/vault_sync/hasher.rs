0d5f9b68fa5706d209ac60d8c5aabb8accaa9451
Martin Smith <mcodesmith@gmail.com>
2021-09-27 17:59:04 -0700

    chore(rust): convert methods to use `call()` for request and responses

--- implementations/rust/ockam/ockam_vault_sync_core/src/vault_sync/hasher.rs
+++ implementations/rust/ockam/ockam_vault_sync_core/src/vault_sync/hasher.rs
@@ -1,44 +1,42 @@
-use ockam_core::{Result, ResultMessage};
+use ockam_core::Result;
 use ockam_vault_core::{Hasher, Secret, SecretAttributes, SmallBuffer};
 
 use crate::{VaultRequestMessage, VaultResponseMessage, VaultSync, VaultSyncCoreError};
 
-
 impl Hasher for VaultSync {
     fn sha256(&mut self, data: &[u8]) -> Result<[u8; 32]> {
-        let resp = self.handle
-            .call::<VaultRequestMessage, ResultMessage<VaultResponseMessage>>(
-                VaultRequestMessage::Sha256 { data: data.into() }
-            )?.into();
+        let resp = self.call(VaultRequestMessage::Sha256 {
+            data: data.into()
+        })?;
 
-        if let Ok(VaultResponseMessage::Sha256(s)) = resp {
+        if let VaultResponseMessage::Sha256(s) = resp {
             Ok(s)
         } else {
             Err(VaultSyncCoreError::InvalidResponseType.into())
         }
-}
-
-fn hkdf_sha256(
-    &mut self,
-    salt: &Secret,
-    info: &[u8],
-    ikm: Option<&Secret>,
-    output_attributes: SmallBuffer<SecretAttributes>,
-) -> Result<SmallBuffer<Secret>> {
-    let resp: ResultMessage<VaultResponseMessage> = self.handle
-        .call(VaultRequestMessage::HkdfSha256 {
-            salt: salt.clone(),
-            info: info.into(),
-            ikm: ikm.cloned(),
-            output_attributes,
-        })?;
-
-    if let Ok(VaultResponseMessage::HkdfSha256(s)) = resp.into() {
-        Ok(s)
-    } else {
-        Err(VaultSyncCoreError::InvalidResponseType.into())
     }
-}
+
+    fn hkdf_sha256(
+        &mut self,
+        salt: &Secret,
+        info: &[u8],
+        ikm: Option<&Secret>,
+        output_attributes: SmallBuffer<SecretAttributes>,
+    ) -> Result<SmallBuffer<Secret>> {
+        let resp = self.call(
+                VaultRequestMessage::HkdfSha256 {
+                    salt: salt.clone(),
+                    info: info.into(),
+                    ikm: ikm.cloned(),
+                    output_attributes,
+                })?;
+
+        if let VaultResponseMessage::HkdfSha256(s) = resp {
+            Ok(s)
+        } else {
+            Err(VaultSyncCoreError::InvalidResponseType.into())
+        }
+    }
 }
 
 #[cfg(test)]
--- implementations/rust/ockam/ockam_vault_sync_core/src/vault_sync/signer.rs
+++ implementations/rust/ockam/ockam_vault_sync_core/src/vault_sync/signer.rs
@@ -1,31 +1,27 @@
-use crate::{VaultRequestMessage, VaultResponseMessage, VaultSync, VaultSyncCoreError};
 use ockam_core::Result;
-use ockam_node::block_future;
 use ockam_vault_core::{Secret, Signature, Signer};
+
+use crate::{VaultRequestMessage, VaultResponseMessage, VaultSync, VaultSyncCoreError};
 
 impl Signer for VaultSync {
     fn sign(&mut self, secret_key: &Secret, data: &[u8]) -> Result<Signature> {
-        block_future(&self.ctx.runtime(), async move {
-            self.send_message(VaultRequestMessage::Sign {
-                secret_key: secret_key.clone(),
-                data: data.into(),
-            })
-            .await?;
+        let resp = self.call(VaultRequestMessage::Sign {
+            secret_key: secret_key.clone(),
+            data: data.into(),
+        })?;
 
-            let resp = self.receive_message().await?;
-
-            if let VaultResponseMessage::Sign(s) = resp {
-                Ok(s)
-            } else {
-                Err(VaultSyncCoreError::InvalidResponseType.into())
-            }
-        })
+        if let VaultResponseMessage::Sign(s) = resp {
+            Ok(s)
+        } else {
+            Err(VaultSyncCoreError::InvalidResponseType.into())
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
     use ockam_vault::SoftwareVault;
+
     use ockam_vault_test_attribute::*;
 
     fn new_vault() -> SoftwareVault {
