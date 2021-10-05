f7f99e6e6fd7181f39906a4766d283db7b0b27cc
Martin Smith <mcodesmith@gmail.com>
2021-10-04 19:06:16 -0700

    fix(rust): continuity fix on imports/use and remove of double import

--- implementations/rust/ockam/ockam_vault_sync_core/src/vault_sync.rs
+++ implementations/rust/ockam/ockam_vault_sync_core/src/vault_sync.rs
@@ -1,10 +1,8 @@
 use crate::{Vault, VaultRequestMessage, VaultResponseMessage, VaultTrait};
 #[cfg(not(feature = "std"))]
 use ockam_core::compat::rand::random;
-use ockam_core::{Address, Result, ResultMessage, Route};
+use ockam_core::{async_trait::async_trait, Address, AsyncTryClone, Result, ResultMessage, Route};
 use ockam_node::{block_future, Context, Handle};
-#[cfg(feature = "std")]
-use rand::random;
 use tracing::debug;
 use zeroize::Zeroize;
 
