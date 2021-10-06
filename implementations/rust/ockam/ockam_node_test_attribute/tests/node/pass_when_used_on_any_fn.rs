9d78ac80ef83db934efcd8cd0de79008f710bff5
Adrian Benavides <adri.benavides312@gmail.com>
2021-10-06 11:43:18 +0200

    chore: minor changes

--- implementations/rust/ockam/ockam_node_test_attribute/tests/node/pass_when_used_on_any_fn.rs
+++ implementations/rust/ockam/ockam_node_test_attribute/tests/node/pass_when_used_on_any_fn.rs
@@ -1,4 +1,4 @@
 #[ockam_node_test_attribute::node]
-async fn foo(mut c: ockam::Context) {
+async fn main(mut c: ockam::Context) {
     c.stop().await.unwrap();
 }
--- implementations/rust/ockam/ockam_node_test_attribute/tests/trybuild.rs
+++ implementations/rust/ockam/ockam_node_test_attribute/tests/trybuild.rs
@@ -3,5 +3,7 @@ fn trybuild() {
     let t = trybuild::TestCases::new();
     t.pass("tests/node/pass*.rs");
     t.compile_fail("tests/node/fail*.rs");
+    // The node_test tests check that a specific compile error occurs: "the main function doesn't exist",
+    // which means that the macro was compiled without errors but it can't be used outside of a test context.
     t.compile_fail("tests/node_test/*.rs");
 }
