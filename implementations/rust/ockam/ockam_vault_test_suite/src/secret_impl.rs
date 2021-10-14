2759674ccdb43a55d6e56e5e37a69a5fee215625
Oleksandr Deundiak <olek@ockam.io>
2021-10-14 17:47:41 +0300

    test(rust): make vault tests async

--- implementations/rust/ockam/ockam_vault_test_suite/src/secret_impl.rs
+++ implementations/rust/ockam/ockam_vault_test_suite/src/secret_impl.rs
@@ -4,45 +4,45 @@ use ockam_vault_core::{
     CURVE25519_SECRET_LENGTH,
 };
 
-pub fn new_public_keys(vault: &mut impl SecretVault) {
+pub async fn new_public_keys(vault: &mut impl SecretVault) {
     let attributes = SecretAttributes::new(
         SecretType::Curve25519,
         SecretPersistence::Ephemeral,
         CURVE25519_SECRET_LENGTH,
     );
 
-    let res = vault.secret_generate(attributes);
+    let res = vault.secret_generate(attributes).await;
     assert!(res.is_ok());
     let p256_ctx_1 = res.unwrap();
 
-    let res = vault.secret_public_key_get(&p256_ctx_1);
+    let res = vault.secret_public_key_get(&p256_ctx_1).await;
     assert!(res.is_ok());
     let pk_1 = res.unwrap();
     assert_eq!(pk_1.as_ref().len(), CURVE25519_PUBLIC_LENGTH);
 
-    let res = vault.secret_generate(attributes);
+    let res = vault.secret_generate(attributes).await;
     assert!(res.is_ok());
     let c25519_ctx_1 = res.unwrap();
-    let res = vault.secret_public_key_get(&c25519_ctx_1);
+    let res = vault.secret_public_key_get(&c25519_ctx_1).await;
     assert!(res.is_ok());
     let pk_1 = res.unwrap();
     assert_eq!(pk_1.as_ref().len(), CURVE25519_PUBLIC_LENGTH);
 }
 
-pub fn new_secret_keys(vault: &mut impl SecretVault) {
+pub async fn new_secret_keys(vault: &mut impl SecretVault) {
     let types = [(SecretType::Curve25519, 32), (SecretType::Buffer, 24)];
     for (t, s) in &types {
         let attributes = SecretAttributes::new(*t, SecretPersistence::Ephemeral, *s);
-        let res = vault.secret_generate(attributes);
+        let res = vault.secret_generate(attributes).await;
         assert!(res.is_ok());
         let sk_ctx = res.unwrap();
-        let sk = vault.secret_export(&sk_ctx).unwrap();
+        let sk = vault.secret_export(&sk_ctx).await.unwrap();
         assert_eq!(sk.as_ref().len(), *s);
-        vault.secret_destroy(sk_ctx).unwrap();
+        vault.secret_destroy(sk_ctx).await.unwrap();
     }
 }
 
-pub fn secret_import_export(vault: &mut impl SecretVault) {
+pub async fn secret_import_export(vault: &mut impl SecretVault) {
     let attributes = SecretAttributes::new(
         SecretType::Curve25519,
         SecretPersistence::Ephemeral,
@@ -53,11 +53,12 @@ pub fn secret_import_export(vault: &mut impl SecretVault) {
 
     let secret = vault
         .secret_import(decode(secret_str).unwrap().as_slice(), attributes)
+        .await
         .unwrap();
 
     assert_eq!(secret.index(), 1);
     assert_eq!(
-        encode(vault.secret_export(&secret).unwrap().as_ref()),
+        encode(vault.secret_export(&secret).await.unwrap().as_ref()),
         secret_str
     );
 
@@ -65,28 +66,35 @@ pub fn secret_import_export(vault: &mut impl SecretVault) {
     let secret_str = "5f791cc52297f62c7b8829b15f828acbdb3c613371d21aa1";
     let secret = vault
         .secret_import(decode(secret_str).unwrap().as_slice(), attributes)
+        .await
         .unwrap();
 
     assert_eq!(secret.index(), 2);
 
     assert_eq!(
-        encode(vault.secret_export(&secret).unwrap().as_ref()),
+        encode(vault.secret_export(&secret).await.unwrap().as_ref()),
         secret_str
     );
 }
 
-pub fn secret_attributes_get(vault: &mut impl SecretVault) {
+pub async fn secret_attributes_get(vault: &mut impl SecretVault) {
     let attributes = SecretAttributes::new(
         SecretType::Curve25519,
         SecretPersistence::Ephemeral,
         CURVE25519_SECRET_LENGTH,
     );
 
-    let secret = vault.secret_generate(attributes).unwrap();
-    assert_eq!(vault.secret_attributes_get(&secret).unwrap(), attributes);
+    let secret = vault.secret_generate(attributes).await.unwrap();
+    assert_eq!(
+        vault.secret_attributes_get(&secret).await.unwrap(),
+        attributes
+    );
 
     let attributes = SecretAttributes::new(SecretType::Buffer, SecretPersistence::Ephemeral, 24);
 
-    let secret = vault.secret_generate(attributes).unwrap();
-    assert_eq!(vault.secret_attributes_get(&secret).unwrap(), attributes);
+    let secret = vault.secret_generate(attributes).await.unwrap();
+    assert_eq!(
+        vault.secret_attributes_get(&secret).await.unwrap(),
+        attributes
+    );
 }
--- implementations/rust/ockam/ockam_vault_test_suite/src/signer_impl.rs
+++ implementations/rust/ockam/ockam_vault_test_suite/src/signer_impl.rs
@@ -3,18 +3,22 @@ use ockam_vault_core::{
     CURVE25519_SECRET_LENGTH,
 };
 
-pub fn sign(vault: &mut (impl Signer + Verifier + SecretVault)) {
+pub async fn sign(vault: &mut (impl Signer + Verifier + SecretVault)) {
     let secret = vault
         .secret_generate(SecretAttributes::new(
             SecretType::Curve25519,
             SecretPersistence::Ephemeral,
             CURVE25519_SECRET_LENGTH,
         ))
+        .await
         .unwrap();
-    let res = vault.sign(&secret, b"hello world!");
+    let res = vault.sign(&secret, b"hello world!").await;
     assert!(res.is_ok());
-    let pubkey = vault.secret_public_key_get(&secret).unwrap();
+    let pubkey = vault.secret_public_key_get(&secret).await.unwrap();
     let signature = res.unwrap();
-    let res = vault.verify(&signature, &pubkey, b"hello world!").unwrap();
+    let res = vault
+        .verify(&signature, &pubkey, b"hello world!")
+        .await
+        .unwrap();
     assert!(res);
 }
