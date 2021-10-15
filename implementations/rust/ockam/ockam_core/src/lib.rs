85c4436ae354dc77338fba84fadd187a631a9da2
Oleksandr Deundiak <olek@ockam.io>
2021-10-15 05:32:48 +0300

    feat(rust): add generic async_try_clone implementation for structs with clone

--- implementations/rust/ockam/ockam_core/src/lib.rs
+++ implementations/rust/ockam/ockam_core/src/lib.rs
@@ -94,4 +94,14 @@ pub mod traits {
         /// Try cloning a object and return an `Err` in case of failure.
         async fn async_try_clone(&self) -> Result<Self>;
     }
+
+    #[async_trait]
+    impl<D> AsyncTryClone for D
+    where
+        D: Clone + Sync,
+    {
+        async fn async_try_clone(&self) -> Result<Self> {
+            Ok(self.clone())
+        }
+    }
 }
