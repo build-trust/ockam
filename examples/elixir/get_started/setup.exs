a7e24396fbf8381e595bc8a4f4bb27e38354cb47
Daniil Fedotov <daniil@ockam.io>
2021-10-19 17:02:38 -0400

    feat(elixir): remote forwarder worker for elixir

--- examples/elixir/get_started/setup.exs
+++ examples/elixir/get_started/setup.exs
@@ -2,7 +2,7 @@ repo = "https://github.com/ockam-network/ockam.git"
 
 Mix.install([
   {:ockam,
-    git: repo, branch: "hairyhum/elixir-forwarding-example", sparse: "implementations/elixir/ockam/ockam"},
+    git: repo, branch: "hairyhum/elixir-remote-forwarder", sparse: "implementations/elixir/ockam/ockam"},
   {:ockam_vault_software, override: true,
     git: repo, branch: "develop", sparse: "implementations/elixir/ockam/ockam_vault_software"},
   {:ranch, "~> 1.8"}
