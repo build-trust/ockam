a924888f33ce1e4fb679ca4c9091ade7104a28fd
Oleksandr Deundiak <olek@ockam.io>
2021-10-26 16:46:59 +0300

    chore(rust): fix clippy warning for ockam

--- implementations/rust/ockam/ockam/src/stream/producer.rs
+++ implementations/rust/ockam/ockam/src/stream/producer.rs
@@ -67,7 +67,7 @@ async fn handle_response(
             );
             Ok(())
         }
-        _ => Err(OckamError::NoSuchProtocol)?,
+        _ => Err(OckamError::NoSuchProtocol.into()),
     }
 }
 
