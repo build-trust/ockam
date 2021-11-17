00bc7f27b33d6ff283be074464e99b887d9cc335
Adrián Benavides <adri.benavides312@gmail.com>
2021-11-17 07:18:13 +0100

    chore: move macros test to ockam crate to avoid deps cycles

--- implementations/rust/ockam/ockam/tests/main.rs
+++ implementations/rust/ockam/ockam/tests/main.rs
@@ -1,4 +1,20 @@
 #[test]
+fn async_try_clone() {
+    let t = trybuild::TestCases::new();
+    t.pass("tests/async_try_clone/test.rs");
+}
+
+#[test]
+fn node() {
+    #[cfg(not(feature = "no_main"))]
+    {
+        let t = trybuild::TestCases::new();
+        t.compile_fail("tests/node/fail*.rs");
+        t.pass("tests/node/pass*.rs");
+    };
+}
+
+#[test]
 fn node_test() {
     let t = trybuild::TestCases::new();
     t.compile_fail("tests/node_test/fail*.rs");
