361692bb5a180db5b9b451f6da482c24eed76dcb
Oleksandr Deundiak <olek@ockam.io>
2021-11-17 02:26:23 +0200

    chore(rust): add ssh auth example

---
+++ implementations/rust/ockam/ockam_examples/example_projects/ssh_credentials/src/bin/alice.rs
@@ -0,0 +1,55 @@
+use credentials_example::{BOB_LISTENER_ADDRESS, BOB_TCP_ADDRESS, ECHOER};
+use ockam::{
+    route, Context, Entity, Identity, Result, SoftwareVault, TcpTransport, TrustEveryonePolicy,
+    VaultSync, TCP,
+};
+use ockam_core::vault::{SecretAttributes, SecretPersistence, SecretType, SecretVault};
+use std::{env, fs};
+
+#[ockam::node]
+async fn main(mut ctx: Context) -> Result<()> {
+    let _tcp = TcpTransport::create(&ctx).await?;
+
+    let mut vault = VaultSync::create(&ctx, SoftwareVault::default()).await?;
+    let vault_address = vault.address();
+    let mut alice = Entity::create(&ctx, &vault_address).await?;
+
+    let secret_key_path = env::var("SECRET_KEY_PATH").unwrap();
+    let secret_key = fs::read_to_string(secret_key_path).unwrap();
+
+    let secret_key = ssh_key::PrivateKey::from_openssh(&secret_key)
+        .unwrap()
+        .key_data
+        .ed25519()
+        .unwrap()
+        .private
+        .clone();
+
+    let secret_key = vault
+        .secret_import(
+            secret_key.as_ref(),
+            SecretAttributes::new(SecretType::Ed25519, SecretPersistence::Ephemeral, 32),
+        )
+        .await?;
+
+    alice.add_key("SSH".into(), &secret_key).await?;
+
+    let channel = alice
+        .create_secure_channel(
+            route![(TCP, BOB_TCP_ADDRESS), BOB_LISTENER_ADDRESS],
+            TrustEveryonePolicy,
+        )
+        .await?;
+
+    ctx.send(
+        route![channel, ECHOER],
+        "Hello, Bob! I'm Alice from github".to_string(),
+    )
+    .await?;
+    let msg = ctx.receive::<String>().await?.take().body();
+    println!("Echo back: {}", &msg);
+
+    ctx.stop().await?;
+
+    Ok(())
+}
---
+++ implementations/rust/ockam/ockam_examples/example_projects/ssh_credentials/src/bin/bob.rs
@@ -0,0 +1,58 @@
+use credentials_example::{BOB_LISTENER_ADDRESS, BOB_TCP_ADDRESS, ECHOER};
+use ockam::{
+    Context, Entity, EntityAccessControlBuilder, Result, Routed, SoftwareVault, TcpTransport,
+    TrustPublicKeyPolicy, VaultSync, Worker,
+};
+use ockam_core::vault::{PublicKey, SecretType};
+use ockam_core::AsyncTryClone;
+use std::{env, fs};
+
+pub struct Echoer;
+
+#[ockam::worker]
+impl Worker for Echoer {
+    type Context = Context;
+    type Message = String;
+
+    async fn handle_message(&mut self, ctx: &mut Context, msg: Routed<String>) -> Result<()> {
+        println!("Address: {}, Received: {}", ctx.address(), msg);
+
+        // Echo the message body back on its return_route.
+        ctx.send(msg.return_route(), msg.body()).await
+    }
+}
+
+#[ockam::node]
+async fn main(ctx: Context) -> Result<()> {
+    let vault = VaultSync::create(&ctx, SoftwareVault::default()).await?;
+    let vault_address = vault.address();
+
+    let access_control = EntityAccessControlBuilder::new_with_any_id();
+    ctx.start_worker_with_access_control(ECHOER, Echoer, access_control)
+        .await?;
+
+    let mut bob = Entity::create(&ctx, &vault_address).await?;
+
+    let public_key_path = env::var("PUBLIC_KEY_PATH").unwrap();
+    let public_key = fs::read_to_string(public_key_path).unwrap();
+
+    let public_key = ssh_key::PublicKey::from_openssh(&public_key)
+        .unwrap()
+        .key_data
+        .ed25519()
+        .unwrap()
+        .clone();
+
+    let public_key = PublicKey::new(public_key.as_ref().to_vec(), SecretType::Ed25519);
+
+    let trust_policy =
+        TrustPublicKeyPolicy::new(public_key, "SSH", bob.async_try_clone().await.unwrap());
+
+    bob.create_secure_channel_listener(BOB_LISTENER_ADDRESS, trust_policy)
+        .await?;
+
+    let tcp = TcpTransport::create(&ctx).await?;
+    tcp.listen(BOB_TCP_ADDRESS).await?;
+
+    Ok(())
+}
