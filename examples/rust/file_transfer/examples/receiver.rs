3c426b6552246dfa9167ae84cabd95552216fea4
dvermd <315743+dvermd@users.noreply.github.com>
2021-11-23 23:18:42 +0100

    docs(rust): file transfer example

---
+++ examples/rust/file_transfer/examples/receiver.rs
@@ -0,0 +1,118 @@
+// examples/receiver.rs
+
+use file_transfer::FileData;
+use ockam::{Context, Entity, Result, TrustEveryonePolicy, Vault};
+use ockam::{RemoteForwarder, Routed, TcpTransport, Worker, TCP};
+
+use std::str;
+
+use tokio::fs::OpenOptions;
+use tokio::io::AsyncWriteExt;
+
+#[derive(Default)]
+struct FileReception {
+    name: String,
+    size: usize,
+    written_size: usize,
+    file: Option<tokio::fs::File>,
+}
+
+#[ockam::worker]
+impl Worker for FileReception {
+    type Context = Context;
+    type Message = FileData;
+
+    async fn handle_message(&mut self, ctx: &mut Context, msg: Routed<Self::Message>) -> Result<()> {
+        match msg.as_body() {
+            FileData::Description(desc) => {
+                self.name = desc.name.clone();
+                self.size = desc.size;
+                self.file = Some(
+                    OpenOptions::new()
+                        .create(true)
+                        .write(true)
+                        .open(&self.name)
+                        .await
+                        .map_err(|e| ockam::Error::new(0, e.to_string()))?,
+                )
+            }
+            FileData::Data(data) => {
+                if self.written_size + data.len() > self.size {
+                    return Err(ockam::Error::new(
+                        0,
+                        format!(
+                            "Received too many bytes already read: {}, received: {}, final size: {}",
+                            self.written_size,
+                            data.len(),
+                            self.size
+                        ),
+                    ));
+                }
+                if let Some(file) = &mut self.file {
+                    match file
+                        .write(
+                            str::from_utf8(data)
+                                .map_err(|e| ockam::Error::new(0, e.to_string()))?
+                                .as_bytes(),
+                        )
+                        .await
+                    {
+                        Ok(n) => {
+                            self.written_size += n;
+                            if self.written_size == self.size {
+                                ctx.stop().await?;
+                            }
+                        }
+                        Err(e) => {
+                            return Err(ockam::Error::new(0, e.to_string()));
+                        }
+                    }
+                } else {
+                    return Err(ockam::Error::new(0, "File not opened"));
+                }
+            }
+            FileData::Quit => ctx.stop().await?,
+        }
+
+        Ok(())
+    }
+}
+
+#[ockam::node]
+async fn main(ctx: Context) -> Result<()> {
+    // Initialize the TCP Transport.
+    TcpTransport::create(&ctx).await?;
+
+    // Create a Vault to safely store secret keys for Receiver.
+    let vault = Vault::create(&ctx).await?;
+
+    // Create an Entity to represent Receiver.
+    let mut receiver = Entity::create(&ctx, &vault).await?;
+
+    // Create a secure channel listener for Receiver that will wait for requests to
+    // initiate an Authenticated Key Exchange.
+    receiver
+        .create_secure_channel_listener("listener", TrustEveryonePolicy)
+        .await?;
+
+    // The computer that is running this program is likely within a private network and
+    // not accessible over the internet.
+    //
+    // To allow Sender and others to initiate an end-to-end secure channel with this program
+    // we connect with 1.node.ockam.network:4000 as a TCP client and ask the forwarding
+    // service on that node to create a forwarder for us.
+    //
+    // All messages that arrive at that forwarding address will be sent to this program
+    // using the TCP connection we created as a client.
+    let node_in_hub = (TCP, "1.node.ockam.network:4000");
+    let forwarder = RemoteForwarder::create(&ctx, node_in_hub, "listener").await?;
+    println!("\n[✓] RemoteForwarder was created on the node at: 1.node.ockam.network:4000");
+    println!("Forwarding address for Receiver is:");
+    println!("{}", forwarder.remote_address());
+
+    // Start a worker, of type FileReception, at address "receiver".
+    ctx.start_worker("receiver", FileReception::default()).await?;
+
+    // We won't call ctx.stop() here, this program will quit when the file will be entirely received
+    Ok(())
+}
---
+++ examples/rust/file_transfer/examples/sender.rs
@@ -0,0 +1,92 @@
+// examples/sender.rs
+
+use file_transfer::{FileData, FileDescription};
+use ockam::{route, Context, Entity, TrustEveryonePolicy, Vault};
+use ockam::{TcpTransport, TCP};
+
+use std::path::PathBuf;
+
+use anyhow::Result;
+use structopt::StructOpt;
+use tokio::fs::File;
+use tokio::io::AsyncReadExt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "sender", about = "An example of file transfer implemented with ockam.")]
+struct Opt {
+    /// Input file
+    #[structopt(parse(from_os_str))]
+    input: PathBuf,
+
+    /// Forwarding address
+    #[structopt(short, long)]
+    address: String,
+
+    /// Sending chunk
+    #[structopt(short, long, default_value = "8192")]
+    chunk_size: usize,
+}
+
+#[ockam::node]
+async fn main(ctx: Context) -> Result<()> {
+    let opt = Opt::from_args();
+
+    // Initialize the TCP Transport.
+    TcpTransport::create(&ctx).await?;
+
+    // Create a Vault to safely store secret keys for Sender.
+    let vault = Vault::create(&ctx).await?;
+
+    // Create an Entity to represent Sender.
+    let mut sender = Entity::create(&ctx, &vault).await?;
+
+    // This program expects that the receiver has setup a forwarding address,
+    // for his secure channel listener, on the Ockam node at 1.node.ockam.network:4000.
+    //
+    // Read this forwarding address for Receiver's secure channel listener from command line argument.
+    let forwarding_address = opt.address.trim();
+
+    // Combine the tcp address of the node and the forwarding_address to get a route
+    // to Receiver's secure channel listener.
+    let route_to_receiver_listener = route![(TCP, "1.node.ockam.network:4000"), forwarding_address];
+
+    // As Sender, connect to the Receiver's secure channel listener, and perform an
+    // Authenticated Key Exchange to establish an encrypted secure channel with Receiver.
+    let channel = sender
+        .create_secure_channel(route_to_receiver_listener, TrustEveryonePolicy)
+        .await?;
+
+    println!("\n[✓] End-to-end encrypted secure channel was established.\n");
+
+    // Open file, send name and size to Receiver then send chunk of files.
+    let mut file = File::open(&opt.input).await?;
+    let metadata = file.metadata().await?;
+    if !metadata.is_file() {
+        anyhow::bail!("Can only transfer a file")
+    }
+
+    // Can safely unwrap the first time because we're sure we have a file because we opened it above
+    //     and `file_name` returns None when the path ends with `..` which can't be a file
+    // Can safely unwrap the second time because the path came from a String from the command line and thus should be a valid UTF8
+    let filename = opt.input.file_name().unwrap().to_str().unwrap().to_owned();
+    let descr = FileData::Description(FileDescription {
+        name: filename,
+        size: metadata.len() as usize,
+    });
+
+    ctx.send(route![channel.clone(), "receiver"], descr).await?;
+
+    let mut buffer = vec![0u8; opt.chunk_size];
+    loop {
+        if let Ok(count) = file.read(&mut buffer).await {
+            if count == 0 {
+                break;
+            }
+            let data = FileData::Data(buffer[..count].to_vec());
+            ctx.send(route![channel.clone(), "receiver"], data).await?;
+        }
+    }
+
+    // We won't call ctx.stop() here, this program will run until you stop it with Ctrl-C
+    Ok(())
+}
