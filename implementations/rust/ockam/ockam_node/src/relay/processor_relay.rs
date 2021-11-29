e46ad1bfa359cad13e835d0d14b42b1f4d7375db
Oleksandr Deundiak <olek@ockam.io>
2021-11-29 21:07:14 +0200

    chore(rust): reduce processor log level

--- implementations/rust/ockam/ockam_node/src/relay/processor_relay.rs
+++ implementations/rust/ockam/ockam_node/src/relay/processor_relay.rs
@@ -56,7 +56,7 @@ where
             // Then select over the two futures
             tokio::select! {
                 _ = shutdown_signal => {
-                    info!("Shutting down processor {}", ctx_addr);
+                    debug!("Shutting down processor {}", ctx_addr);
                 },
                 _ = run_loop => {}
             };
