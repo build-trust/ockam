f6721ea3355b9a535d1025251adae87d6a9f395b
Michael Uti <utimichael9@gmail.com>
2021-12-14 17:42:26 +0100

    chore: inform user to review generated changelog
    Inform script user to review generated changelog and commit before calling
    crate bump script

--- tools/scripts/release/changelog.sh
+++ tools/scripts/release/changelog.sh
@@ -8,3 +8,5 @@ source tools/scripts/release/crates-to-publish.sh
 for crate in ${updated_crates[@]}; do
     git cliff --unreleased --commit-path implementations/rust/ockam/$crate --prepend implementations/rust/ockam/$crate/CHANGELOG.md
 done
+
+echo "Changelog has been generated. Please review and commit."
