b2f3cdcde495dd5f4056a89bb0469d50a11a039c
Rishabh Chauhan <binareshu96@gmail.com>
2022-01-01 16:33:55 +0530

    docs: add windows instruction to hello ockam example

--- implementations/rust/ockam/ockam_macros/src/node_attribute/args.rs
+++ implementations/rust/ockam/ockam_macros/src/node_attribute/args.rs
@@ -1,11 +1,18 @@
-pub(crate) fn node(args: syn::AttributeArgs) -> Result<Args, syn::Error> {
+use syn::{AttributeArgs, Meta, NestedMeta};
+
+pub(crate) fn node(args: AttributeArgs) -> Result<Args, syn::Error> {
     match args.first() {
-        None => Ok(Args {}),
+        None => Ok(Args { no_main: false }),
+        Some(NestedMeta::Meta(Meta::Path(x))) if x.is_ident("no_main") => {
+            Ok(Args { no_main: true })
+        }
         Some(arg) => Err(syn::Error::new_spanned(
             arg,
-            "This macro doesn't accept any argument",
+            "`ockam::node` does not support this syntax",
         )),
     }
 }
 
-pub(crate) struct Args {}
+pub(crate) struct Args {
+    pub(crate) no_main: bool,
+}
