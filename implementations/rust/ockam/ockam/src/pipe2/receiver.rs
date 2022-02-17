e609f446be86200d4fb8d3bac691946fb3f684cb
Katharina Fey <kookie@spacekookie.de>
2022-02-17 17:04:11 +0100

    feat(rust): very basic pipe example with new api

--- implementations/rust/ockam/ockam/src/pipe2/receiver.rs
+++ implementations/rust/ockam/ockam/src/pipe2/receiver.rs
@@ -1,1 +1,32 @@
+use crate::{Context, OckamMessage, WorkerSystem};
+use ockam_core::{Address, Any, LocalMessage, Result, Routed, TransportMessage, Worker};
 
+pub struct PipeReceiver {
+    system: WorkerSystem<Context, OckamMessage>,
+    int_addr: Address,
+}
+
+#[crate::worker]
+impl Worker for PipeReceiver {
+    type Context = Context;
+    type Message = OckamMessage;
+
+    async fn initialize(&mut self, ctx: &mut Context) -> Result<()> {
+        ctx.set_cluster(crate::pipe2::CLUSTER_NAME).await?;
+        Ok(())
+    }
+
+    async fn handle_message(&mut self, ctx: &mut Context, msg: Routed<OckamMessage>) -> Result<()> {
+        let inner: TransportMessage = msg.body().data()?;
+        ctx.forward(LocalMessage::new(inner, vec![])).await
+    }
+}
+
+impl PipeReceiver {
+    pub fn new(int_addr: Address) -> Self {
+        Self {
+            system: WorkerSystem::default(),
+            int_addr,
+        }
+    }
+}
