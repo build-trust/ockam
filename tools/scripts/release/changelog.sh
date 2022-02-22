1b67c272e8e52feb62868da788e5bd472fc60575
Michael Uti <utimichael9@gmail.com>
2022-02-23 00:20:26 +0100

    ci: revert changelog script to use v0.4.2

--- tools/scripts/release/changelog.sh
+++ tools/scripts/release/changelog.sh
@@ -6,7 +6,7 @@
 source tools/scripts/release/crates-to-publish.sh
 
 for crate in ${updated_crates[@]}; do
-    git cliff --unreleased --include-path implementations/rust/ockam/$crate/* --prepend implementations/rust/ockam/$crate/CHANGELOG.md
+    git cliff --unreleased --commit-path implementations/rust/ockam/$crate --prepend implementations/rust/ockam/$crate/CHANGELOG.md
 done
 
 echo "Changelog has been generated. Please review and commit."
