563d76bf93da623cfea72f186f7ed33a2de9ed50
Katharina Fey <kookie@spacekookie.de>
2022-02-22 17:02:27 +0100

    chore(rust): add full composition pipe tests

--- implementations/rust/ockam/ockam/src/pipe2/tests.rs
+++ implementations/rust/ockam/ockam/src/pipe2/tests.rs
@@ -136,3 +136,62 @@ async fn fixed_ordering_pipe(ctx: &mut Context) -> Result<()> {
     assert_eq!(msg, *msg2);
     ctx.stop().await
 }
+
+#[crate::test]
+async fn fixed_delivery_and_ordering_pipe(ctx: &mut Context) -> Result<()> {
+    let rx_addr = Address::random(0);
+
+    // Start a static receiver
+    let rx = PipeBuilder::fixed()
+        .receive(rx_addr.clone())
+        .delivery_ack()
+        .enforce_ordering()
+        .build(ctx)
+        .await?;
+    info!("Created receiver pipe: {}", rx.addr());
+
+    // Connect to a static receiver
+    let sender = PipeBuilder::fixed()
+        .connect(vec![rx_addr])
+        .delivery_ack()
+        .enforce_ordering()
+        .build(ctx)
+        .await?;
+
+    info!("Created sender pipe: {}", sender.addr());
+
+    let msg = String::from("Hello through the pipe");
+    ctx.send(vec![sender.addr(), "app".into()], msg.clone())
+        .await?;
+
+    let msg2 = ctx.receive::<String>().await?;
+    assert_eq!(msg, *msg2);
+    ctx.stop().await
+}
+
+#[crate::test]
+async fn dynamic_delivery_and_ordering_pipe(ctx: &mut Context) -> Result<()> {
+    let listener = PipeBuilder::dynamic()
+        .receive("my-pipe-listener")
+        .delivery_ack()
+        .enforce_ordering()
+        .build(ctx)
+        .await?;
+
+    // Point the sender to the listener which will spawn a receiver
+    let sender = PipeBuilder::dynamic()
+        .connect(listener.addr())
+        .delivery_ack()
+        .enforce_ordering()
+        .build(ctx)
+        .await?;
+
+    let msg = String::from("Hello through the pipe");
+    ctx.send(vec![sender.addr(), "app".into()], msg.clone())
+        .await?;
+
+    let msg2 = ctx.receive::<String>().await?;
+    assert_eq!(msg, *msg2);
+
+    ctx.stop().await
+}
