92675cc41f9bbf6a3db4aa8e3b2efd146bacb8de
Thom Chiovoloni <chiovolonit@gmail.com>
2022-03-03 07:06:34 -0800

    feat(rust): initial support for vault file storage

--- implementations/rust/ockam/ockam_vault_sync_core/src/vault.rs
+++ implementations/rust/ockam/ockam_vault_sync_core/src/vault.rs
@@ -9,4 +9,12 @@ impl Vault {
     pub fn create() -> VaultMutex<ockam_vault::SoftwareVault> {
         VaultMutex::create(ockam_vault::SoftwareVault::new())
     }
+    #[cfg(feature = "software_vault_storage")]
+    pub async fn from_path(
+        p: impl AsRef<std::path::Path>,
+    ) -> Result<VaultMutex<ockam_vault::SoftwareVault>, ockam_core::Error> {
+        Ok(VaultMutex::create(
+            ockam_vault::SoftwareVault::from_path(p).await?,
+        ))
+    }
 }
