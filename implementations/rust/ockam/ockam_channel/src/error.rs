feea7508cbaf4bd667be165e318551d671c091ee
Katharina Fey <kookie@spacekookie.de>
2022-03-31 15:01:15 +0200

    refactor(rust): errors: fix ockam_channel

--- implementations/rust/ockam/ockam_channel/src/error.rs
+++ implementations/rust/ockam/ockam_channel/src/error.rs
@@ -1,34 +1,44 @@
-use ockam_core::Error;
+use ockam_core::{
+    error::{
+        code::{ErrorCode, Kind, Origin},
+        Error2,
+    },
+    thiserror,
+};
 
 /// Types of errors that may occur constructing a secure channel.
-#[derive(Debug, Clone, Copy)]
+#[derive(Clone, Debug, thiserror::Error)]
 pub enum SecureChannelError {
     /// The key exchange process failed.
+    #[error("the key exchange process failed.")]
     KeyExchange = 1,
     /// Internal state is invalid.
+    #[error("internal state is invalid.")]
     InvalidInternalState,
     /// Expected nonce was invalid.
+    #[error("expected nonce was invalid.")]
     InvalidNonce,
     /// Key exchange process did not complete.
+    #[error("key exchange process did not complete.")]
     KeyExchangeNotComplete,
     /// Invalid response received from the Hub.
+    #[error("invalid response received from the Hub.")]
     InvalidHubResponse,
     /// Invalid LocalInfo type
+    #[error("invalid LocalInfo type")]
     InvalidLocalInfoType,
 }
+#[allow(clippy::from_over_into)]
+impl Into<Error2> for SecureChannelError {
+    fn into(self) -> Error2 {
+        use SecureChannelError::*;
+        let kind = match self {
+            KeyExchange | KeyExchangeNotComplete => Kind::Protocol,
+            InvalidInternalState | InvalidNonce | InvalidHubResponse | InvalidLocalInfoType => {
+                Kind::Invalid
+            }
+        };
 
-impl SecureChannelError {
-    /// Integer code associated with the error domain.
-    pub const DOMAIN_CODE: u32 = 16_000;
-    /// Error domain
-    pub const DOMAIN_NAME: &'static str = "OCKAM_SECURE_CHANNEL";
-}
-
-impl From<SecureChannelError> for Error {
-    fn from(e: SecureChannelError) -> Error {
-        Error::new(
-            SecureChannelError::DOMAIN_CODE + (e as u32),
-            ockam_core::compat::format!("{}::{:?}", module_path!(), e),
-        )
+        Error2::new(ErrorCode::new(Origin::Channel, kind), self)
     }
 }
