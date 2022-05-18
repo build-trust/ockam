5b8a7b0055520b0286e9a6808ba53858e59d8c1f
Oleksandr Deundiak <olek@ockam.io>
2022-05-18 19:36:36 +0300

    feat(rust): split worker into `Encryptor` and `Decryptor` in ockam_channel crate

--- implementations/rust/ockam/ockam_channel/src/secure_channel_listener.rs
+++ implementations/rust/ockam/ockam_channel/src/secure_channel_listener.rs
@@ -1,4 +1,4 @@
-use crate::{SecureChannelNewKeyExchanger, SecureChannelVault, SecureChannelWorker};
+use crate::{SecureChannelDecryptor, SecureChannelNewKeyExchanger, SecureChannelVault};
 use ockam_core::async_trait;
 use ockam_core::compat::rand::random;
 use ockam_core::compat::{boxed::Box, vec::Vec};
@@ -30,7 +30,7 @@ impl<V: SecureChannelVault, N: SecureChannelNewKeyExchanger> SecureChannelListen
 #[derive(Serialize, Deserialize, Clone, PartialEq, Debug, Message)]
 pub struct CreateResponderChannelMessage {
     payload: Vec<u8>,
-    completed_callback_address: Option<Address>,
+    custom_payload: Option<Vec<u8>>,
 }
 
 impl CreateResponderChannelMessage {
@@ -39,17 +39,17 @@ impl CreateResponderChannelMessage {
         &self.payload
     }
     /// Callback Address
-    pub fn completed_callback_address(&self) -> &Option<Address> {
-        &self.completed_callback_address
+    pub fn custom_payload(&self) -> &Option<Vec<u8>> {
+        &self.custom_payload
     }
 }
 
 impl CreateResponderChannelMessage {
     /// Create message using payload and callback_address
-    pub fn new(payload: Vec<u8>, completed_callback_address: Option<Address>) -> Self {
+    pub fn new(payload: Vec<u8>, custom_payload: Option<Vec<u8>>) -> Self {
         CreateResponderChannelMessage {
             payload,
-            completed_callback_address,
+            custom_payload,
         }
     }
 }
@@ -66,36 +66,25 @@ impl<V: SecureChannelVault, N: SecureChannelNewKeyExchanger> Worker
         ctx: &mut Self::Context,
         msg: Routed<Self::Message>,
     ) -> Result<()> {
-        let reply = msg.return_route().clone();
+        let return_route = msg.return_route().clone();
         let msg = msg.body();
 
         let address_remote: Address = random();
-        let address_local: Address = random();
 
         debug!(
-            "Starting SecureChannel responder at local: {}, remote: {}",
-            &address_local, &address_remote
+            "Starting SecureChannel responder at remote: {}",
+            &address_remote
         );
 
-        let responder = self.new_key_exchanger.responder().await?;
+        let key_exchanger = self.new_key_exchanger.responder().await?;
         let vault = self.vault.async_try_clone().await?;
-        let channel = SecureChannelWorker::new(
-            false,
-            reply.clone(),
-            address_remote.clone(),
-            address_local.clone(),
-            msg.completed_callback_address().clone(),
-            None,
-            responder,
-            vault,
-        )
-        .await?;
+        let decryptor = SecureChannelDecryptor::new_responder(key_exchanger, None, vault).await?;
 
-        ctx.start_worker(vec![address_remote.clone(), address_local], channel)
+        ctx.start_worker(vec![address_remote.clone()], decryptor)
             .await?;
 
         // We want this message's return route lead to the remote channel worker, not listener
-        let msg = TransportMessage::v1(address_remote, reply, msg.payload().encode()?);
+        let msg = TransportMessage::v1(address_remote, return_route, msg.payload().encode()?);
 
         ctx.forward(LocalMessage::new(msg, Vec::new())).await?;
 
