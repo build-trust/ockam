78f5f43d22fdeee93b7dd1fcd375f7b210634d6e
Mrinal Wadhwa <mrinal@ockam.io>
2022-05-25 23:31:30 -0700

    feat(rust): add node subcommand

---
+++ implementations/rust/ockam/ockam_command/src/node/start.rs
@@ -0,0 +1,50 @@
+use clap::Args;
+
+use crate::util::embedded_node;
+use ockam::{Context, Result, Routed, TcpTransport, Worker};
+
+struct Status {
+    node_name: String,
+}
+
+#[ockam::worker]
+impl Worker for Status {
+    type Message = String;
+    type Context = Context;
+
+    async fn handle_message(&mut self, ctx: &mut Context, msg: Routed<String>) -> Result<()> {
+        println!(
+            "\n[✓] Node: {}, Address: {}, Received: {}",
+            self.node_name,
+            ctx.address(),
+            msg
+        );
+        Ok(())
+    }
+}
+
+#[derive(Clone, Debug, Args)]
+pub struct StartCommand {
+    /// Name of the node.
+    #[clap(default_value_t = String::from("default"))]
+    pub node_name: String,
+}
+
+impl StartCommand {
+    pub fn run(command: StartCommand) {
+        embedded_node(setup, command)
+    }
+}
+
+async fn setup(ctx: Context, c: StartCommand) -> Result<()> {
+    let tcp = TcpTransport::create(&ctx).await?;
+    tcp.listen("127.0.0.1:62526").await?;
+
+    ctx.start_worker(
+        "status",
+        Status {
+            node_name: c.node_name,
+        },
+    )
+    .await
+}
