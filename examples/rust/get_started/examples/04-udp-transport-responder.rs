dcc0574f63753fe00385727209b4aeb55fb8aea3
Yuan Lyu <lyuyuan92@gmail.com>
2022-05-28 22:43:26 -0400

    docs(rust): add udp transport get-started examples

---
+++ examples/rust/get_started/examples/04-udp-transport-responder.rs
@@ -0,0 +1,21 @@
+// This node starts an udp listener and an echoer worker.
+// It then runs forever waiting for messages.
+
+use hello_ockam::Echoer;
+use ockam::{Context, Result};
+use ockam_transport_udp::UdpTransport;
+
+#[ockam::node]
+async fn main(ctx: Context) -> Result<()> {
+    // Initialize the UDP Transport.
+    let udp = UdpTransport::create(&ctx).await?;
+
+    // Create a UDP listener and wait for incoming datagrams.
+    udp.listen("127.0.0.1:4000").await?;
+
+    // Create an echoer worker
+    ctx.start_worker("echoer", Echoer).await?;
+
+    // Don't call ctx.stop() here so this node runs forever.
+    Ok(())
+}
