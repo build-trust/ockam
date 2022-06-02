01811dc056dc7e15b38c6f3048fd61e1cdb04ac4
Katharina Fey <kookie@spacekookie.de>
2022-06-02 18:30:32 +0200

    feat(rust): utility function to purge all nodes

---
+++ implementations/rust/ockam/ockam_command/src/node/purge.rs
@@ -0,0 +1,25 @@
+//! A simple command to purge existing configuration
+
+use crate::config::OckamConfig;
+use clap::Args;
+
+#[derive(Clone, Debug, Args)]
+pub struct PurgeCommand {
+    /// Should nodes be terminated with SIGKILL instead of SIGTERM
+    #[clap(display_order = 900, long, short)]
+    sigkill: bool,
+}
+
+impl PurgeCommand {
+    pub fn run(cfg: &mut OckamConfig, command: PurgeCommand) {
+        let nodes: Vec<_> = cfg
+            .get_nodes()
+            .iter()
+            .map(|(name, _)| name.clone())
+            .collect();
+
+        for node_name in nodes {
+            crate::node::delete::delete_node(cfg, &node_name, command.sigkill);
+        }
+    }
+}
