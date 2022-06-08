5d79969902f297a8fc060e35e313f5829cd9344d
Oleksandr Deundiak <olek@ockam.io>
2022-06-08 16:20:45 +0300

    feat(rust): replace `Identity`-related structs with opaque binaries in secure channel

--- implementations/rust/ockam/ockam_identity/src/channel/messages.rs
+++ implementations/rust/ockam/ockam_identity/src/channel/messages.rs
@@ -1,11 +1,10 @@
-use crate::Contact;
 use ockam_core::compat::vec::Vec;
 use ockam_core::Message;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Message)]
 pub(crate) enum IdentityChannelMessage {
-    Request { contact: Contact, proof: Vec<u8> },
-    Response { contact: Contact, proof: Vec<u8> },
+    Request { contact: Vec<u8>, proof: Vec<u8> },
+    Response { contact: Vec<u8>, proof: Vec<u8> },
     Confirm,
 }
