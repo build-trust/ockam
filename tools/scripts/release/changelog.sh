2f8fcf46386b8d2a7a493c2174f919b78208d518
metaclips <utimichael9@gmail.com>
2022-06-14 23:12:03 +0100

    ci: add release version in changelog

--- tools/scripts/release/changelog.sh
+++ tools/scripts/release/changelog.sh
@@ -12,6 +12,13 @@ for crate in $( echo "$updated_crates" ); do
     echo "Generating changelog for $crate with tag $last_git_tag"
     with_commit_msg="feat: updated dependencies"
     git-cliff $last_git_tag.. --with-commit $with_commit_msg --include-path implementations/rust/ockam/$crate/**/*.rs --prepend implementations/rust/ockam/$crate/CHANGELOG.md
+
+    # Replace ## unreleased text to bumped version
+    version=$(eval "tomlq package.version -f implementations/rust/ockam/$crate/Cargo.toml")
+
+    search="## unreleased"
+    replace="## $version - $(date +'%Y-%m-%d')"
+    sed -i -e "s/$search/$replace/" implementations/rust/ockam/$crate/CHANGELOG.md
 done
 
 echo "Changelog has been generated. Please review and commit."
