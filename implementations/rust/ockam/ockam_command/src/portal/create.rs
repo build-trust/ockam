88598fe7097562d199cf1c7b17dc2d61eff25f09
Katharina Fey <kookie@spacekookie.de>
2022-06-22 13:58:42 +0200

    refactor(rust): add sub-commands for portal and transport creation
    This commit partially undoes the previous work towards addon
    commands.  The main issue is with creating meaningful but dynamic help
    pages if we leave clap's structure of parsing parameters to pass to
    plugins (even if internal).
    
    Comments and preparations are left in the code to use to upgrade this
    system in the future.

--- implementations/rust/ockam/ockam_command/src/portal/create.rs
+++ implementations/rust/ockam/ockam_command/src/portal/create.rs
@@ -1,7 +1,5 @@
-use crate::util::{api, connect_to, stop_node, OckamConfig};
-use clap::Args;
-use ockam::{Context, Route, TCP};
-use ockam_api::{nodes::types::TransportStatus, Status};
+use crate::util::OckamConfig;
+use clap::{Args, Subcommand};
 
 #[derive(Clone, Debug, Args)]
 pub struct CreateCommand {
@@ -9,25 +7,33 @@ pub struct CreateCommand {
     #[clap(short, long)]
     pub api_node: Option<String>,
 
-    /// Create an inlet portal
-    #[clap(short, long, conflicts_with("outlet"))]
-    pub inlet: bool,
-
-    /// Create an outlet portal
-    #[clap(short, long, conflicts_with("inlet"))]
-    pub outlet: bool,
-
-    /// Transport connection or bind address
-    pub address: String,
+    /// Select a creation variant
+    #[clap(subcommand)]
+    pub create_subcommand: CreateTypeCommand,
 
     /// Give this portal endpoint a name.  If none is provided a
     /// random one will be generated.
+    #[clap(short, long)]
     pub alias: Option<String>,
+}
+
+#[derive(Clone, Debug, Subcommand)]
+pub enum CreateTypeCommand {
+    /// Create a TCP portal inlet
+    TcpInlet {
+        /// Portal inlet bind address
+        bind: String,
+    },
+    /// Create a TCP portal outlet
+    TcpOutlet {
+        /// Portal outlet connection address
+        address: String,
+    },
 }
 
 impl CreateCommand {
     pub fn run(cfg: &mut OckamConfig, command: CreateCommand) {
-        let port = match cfg.select_node(&command.api_node) {
+        let _port = match cfg.select_node(&command.api_node) {
             Some(cfg) => cfg.port,
             None => {
                 eprintln!("No such node available.  Run `ockam node list` to list available nodes");
