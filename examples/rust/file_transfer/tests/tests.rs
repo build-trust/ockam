bcdb2bb80eea55c25db51b8b0c76f5b466a6f282
Neil Singh <neil2468@users.noreply.github.com>
2022-06-24 12:25:29 +0100

    chore(rust): remove restriction that file data must be valid utf8

---
+++ examples/rust/file_transfer/tests/tests.rs
@@ -0,0 +1,95 @@
+use example_test_helper::{CmdBuilder, Error};
+use file_diff::diff;
+use rand::Rng;
+use std::fs::{remove_file, OpenOptions};
+use std::io::{BufWriter, Write};
+use std::path;
+use std::sync::atomic::{AtomicU32, Ordering::Relaxed};
+
+// These are all in bytes
+const SMALL_CHUNK_SIZE: u32 = 32;
+const LARGE_CHUNK_SIZE: u32 = 50 * 1024;
+const TINY_FILE_SIZE: u32 = 100;
+const MEDIUM_FILE_SIZE: u32 = 100 * 1024;
+
+#[test]
+fn tiny_file_transfer() -> Result<(), Error> {
+    do_file_transfer(TINY_FILE_SIZE, None)
+}
+
+#[test]
+fn tiny_file_transfer_small_chunks() -> Result<(), Error> {
+    do_file_transfer(TINY_FILE_SIZE, Some(SMALL_CHUNK_SIZE))
+}
+
+#[test]
+fn medium_file_transfer() -> Result<(), Error> {
+    do_file_transfer(MEDIUM_FILE_SIZE, None)
+}
+
+#[test]
+fn medium_file_transfer_small_chunks() -> Result<(), Error> {
+    do_file_transfer(MEDIUM_FILE_SIZE, Some(SMALL_CHUNK_SIZE))
+}
+
+#[test]
+fn medium_file_transfer_large_chunks() -> Result<(), Error> {
+    do_file_transfer(MEDIUM_FILE_SIZE, Some(LARGE_CHUNK_SIZE))
+}
+
+// Use an atomic to allow tests to have unique IDs when run in parallel
+static TMP_ID: AtomicU32 = AtomicU32::new(0);
+
+fn do_file_transfer(file_size: u32, chunk_size: Option<u32>) -> Result<(), Error> {
+    // Spawn receiver, wait for & grab dynamic forwarding address
+    let receiver = CmdBuilder::new("cargo run --example receiver").spawn()?;
+    let fwd_address = receiver.match_stdout(r"(?m)^FWD_(\w+)$")?.swap_remove(0).unwrap();
+    println!("Forwarding address: {fwd_address}");
+
+    // Create temporary binary file to transfer
+    // Use unique filenames to stop tests clashing when run in parallel
+    let filename = format!(
+        "{:x}_{}_{}_test.bin",
+        TMP_ID.fetch_add(1, Relaxed),
+        file_size,
+        chunk_size.unwrap_or(0)
+    );
+    let source_path = format!("tests{}{}", path::MAIN_SEPARATOR, filename);
+    let target_path = filename;
+    // Scope file and its writer so they are dropped before we continue
+    {
+        let f = OpenOptions::new()
+            .write(true)
+            .create(true)
+            .truncate(true)
+            .open(&source_path)?;
+
+        let mut writer = BufWriter::new(f);
+        let mut rng = rand::thread_rng();
+        for _ in 0..file_size {
+            writer.write(&[rng.gen::<u8>()])?;
+        }
+        writer.flush()?;
+    }
+
+    // Spawn sender
+    let mut cmd_line = format!("cargo run --example sender {source_path} --address {fwd_address}");
+    if let Some(val) = chunk_size {
+        cmd_line.push_str(&format!(" --chunk-size {val}"));
+    }
+    let sender = CmdBuilder::new(&cmd_line).spawn()?;
+    sender.match_stdout(r"(?i)End-to-end encrypted secure channel was established")?;
+
+    // Wait for receiver to complete
+    let (exitcode, _) = receiver.wait()?;
+    assert_eq!(Some(0), exitcode);
+
+    // Check for a successful test
+    assert!(diff(&target_path, &source_path));
+
+    // Cleanup
+    remove_file(source_path)?;
+    remove_file(target_path)?;
+
+    Ok(())
+}
