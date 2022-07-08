a3fc62fd81442cfce6e1e25397db94c0ef63ca29
Neil Singh <neil2468@users.noreply.github.com>
2022-07-08 18:06:58 +0100

    chore(rust): improve output of check_documentation.sh for humans

--- tools/docs/verify_md.sh
+++ tools/docs/verify_md.sh
@@ -5,6 +5,10 @@ TMP=$(mktemp)
 example_blocks $FILE >$TMP
 cmp -s $FILE $TMP
 if [[ $? -ne 0 ]]; then
-  echo "$FILE examples are not up to date."
+  echo "$FILE examples are not up to date. See diff below."
+  echo "===================="
+  diff -c $FILE $TMP
+  echo "===================="
+  echo
   exit 1
 fi
