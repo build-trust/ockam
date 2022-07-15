2618b68e082100ed46460a921f79634ee5579fb4
Toralf Wittner <tw@dtex.org>
2022-07-15 14:08:08 +0200

    feat(rust): remove ability to set arbitrary attributes
    From the command-line one can get and delete attributes, but setting
    attriattributes is handled by services, e.g. the authority.

--- implementations/rust/ockam/ockam_api/tests/auth_smoke.rs
+++ implementations/rust/ockam/ockam_api/tests/auth_smoke.rs
@@ -1,37 +1,18 @@
+use ockam::authenticated_storage::{AuthenticatedStorage, InMemoryStorage};
 use ockam_api::auth;
-use ockam_api::auth::types::Attributes;
 use ockam_core::Result;
 use ockam_node::Context;
 
 #[ockam_macros::test]
 async fn auth_smoke(ctx: &mut Context) -> Result<()> {
-    #[cfg(not(feature = "lmdb"))]
-    {
-        let s = ockam::authenticated_storage::InMemoryStorage::new();
-        ctx.start_worker("auth", auth::Server::new(s)).await?;
-    }
-
-    #[cfg(feature = "lmdb")]
-    let tempfile = tempfile::NamedTempFile::new().map_err(|e| {
-        use ockam_core::errcode::{Kind, Origin};
-        ockam_core::Error::new(Origin::Other, Kind::Io, e)
-    })?;
-
-    #[cfg(feature = "lmdb")]
-    {
-        let s = ockam_api::lmdb::LmdbStorage::new(tempfile.path()).await?;
-        ctx.start_worker("auth", auth::Server::new(s)).await?;
-    }
+    let s = InMemoryStorage::new();
+    ctx.start_worker("auth", auth::Server::new(s.clone()))
+        .await?;
 
     let mut client = auth::Client::new("auth".into(), ctx).await?;
 
-    let mut attrs = Attributes::new();
-    attrs
-        .put("a", &b"hello"[..])
-        .put("b", &b"world"[..])
-        .put("c", &b"42"[..]);
-
-    client.set("foo", &attrs).await?;
+    s.set("foo", "a".to_string(), b"hello".to_vec()).await?;
+    s.set("foo", "b".to_string(), b"world".to_vec()).await?;
 
     assert_eq!(Some(&b"hello"[..]), client.get("foo", "a").await?);
     assert_eq!(Some(&b"world"[..]), client.get("foo", "b").await?);
@@ -39,6 +20,5 @@ async fn auth_smoke(ctx: &mut Context) -> Result<()> {
     client.del("foo", "a").await?;
     assert_eq!(None, client.get("foo", "a").await?);
 
-    ctx.stop().await?;
-    Ok(())
+    ctx.stop().await
 }
