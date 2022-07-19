f13ac82ec175686deb396ec55267cafa4a5e1b31
Adrian Benavides <adri.benavides312@gmail.com>
2022-07-19 11:39:53 +0200

    fix(rust): cloud and node arguments set as global
    Common arguments of cloud commands (basically `NodeOpts` and `CloudOpts`) where not set as global,
    and the user had to write those arguments before the subcommand:
    `ockam space <ADDRESS> list ...`
    
    Now, `NodeOpts` and `CloudOpts` have the `global` attribute, so that subcommands can use them
    as their own arguments:
    `ockam space list <ADDRESS> ...`

--- implementations/rust/ockam/ockam_command/tests/cmd_space.rs
+++ implementations/rust/ockam/ockam_command/tests/cmd_space.rs
@@ -3,28 +3,32 @@ use std::process::Command;
 
 #[test]
 fn valid_arguments() -> Result<(), Box<dyn std::error::Error>> {
-    let prefix_args = [
-        "--test-argument-parser",
-        "space",
-        "/dnsaddr/localhost/tcp/4000",
-        "-a",
-        "node-name",
-    ];
+    let prefix_args = ["--test-argument-parser", "space"];
+    let common_args = ["/dnsaddr/localhost/tcp/4000", "-a", "node-name"];
 
     let mut cmd = Command::cargo_bin("ockam")?;
-    cmd.args(&prefix_args).arg("create").arg("space-name");
+    cmd.args(&prefix_args)
+        .arg("create")
+        .arg("space-name")
+        .args(common_args);
     cmd.assert().success();
 
     let mut cmd = Command::cargo_bin("ockam")?;
-    cmd.args(&prefix_args).arg("list");
+    cmd.args(&prefix_args).arg("list").args(common_args);
     cmd.assert().success();
 
     let mut cmd = Command::cargo_bin("ockam")?;
-    cmd.args(&prefix_args).arg("show").arg("space-id");
+    cmd.args(&prefix_args)
+        .arg("show")
+        .arg("space-id")
+        .args(common_args);
     cmd.assert().success();
 
     let mut cmd = Command::cargo_bin("ockam")?;
-    cmd.args(&prefix_args).arg("delete").arg("space-id");
+    cmd.args(&prefix_args)
+        .arg("delete")
+        .arg("space-id")
+        .args(common_args);
     cmd.assert().success();
 
     Ok(())
