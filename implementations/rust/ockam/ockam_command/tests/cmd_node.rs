0df0d9658b44b772d24e7ac3ea95313f61c15c21
justinvzyl <justinvzyl@users.noreply.github.com>
2022-07-29 23:59:17 -0700

    test(rust): add argument tests for `node show` and `node delete`

---
+++ implementations/rust/ockam/ockam_command/tests/cmd_node.rs
@@ -0,0 +1,23 @@
+use assert_cmd::prelude::*;
+use std::process::Command;
+
+#[test]
+fn valid_arguments() -> Result<(), Box<dyn std::error::Error>> {
+    // show node success
+    let mut cmd = Command::cargo_bin("ockam")?;
+    cmd.arg("--test-argument-parser")
+        .arg("node")
+        .arg("show")
+        .arg("node-name");
+    cmd.assert().success();
+
+    // delete node success
+    let mut cmd = Command::cargo_bin("ockam")?;
+    cmd.arg("--test-argument-parser")
+        .arg("node")
+        .arg("delete")
+        .arg("node-name");
+    cmd.assert().success();
+
+    Ok(())
+}
