e0343ca570a0a876d518106e39a3216133bc9ef6
metaclips <utimichael9@gmail.com>
2022-08-01 10:11:10 +0100

    ci: pin ockam package action sha

--- tools/scripts/release/acceptance_tests.sh
+++ tools/scripts/release/acceptance_tests.sh
@@ -27,11 +27,11 @@ fi
 function test_published_crates_io_release() {
   set -e
   ockam_version=${RELEASE_VERSION}
-  gh workflow run test_published_package.yml --ref docker -R $owner/artifacts -F ockam_version=$ockam_version
+  gh workflow run acceptance_test.yml --ref docker -R $owner/artifacts -F ockam_version=$ockam_version
 
   # Wait for workflow run
   sleep 10
-  run_id=$(gh run list --workflow=test_published_package.yml -b docker -u $GITHUB_USERNAME -L 1 -R $owner/artifacts  --json databaseId | jq -r .[0].databaseId)
+  run_id=$(gh run list --workflow=acceptance_test.yml -b docker -u $GITHUB_USERNAME -L 1 -R $owner/artifacts  --json databaseId | jq -r .[0].databaseId)
   gh run watch $run_id --exit-status -R $owner/artifacts
 }
 
