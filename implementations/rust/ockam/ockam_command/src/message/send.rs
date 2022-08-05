f3850c68c051a8097eb24d2542e0c6300c8a7213
leonzchang <bellerophon00530@gmail.com>
2022-08-05 18:27:16 +0800

    refactor(rust): encapsulate string parsing behavior in the struct
    Signed-off-by: 張立 <bellerophon00530@gmail.com>

--- implementations/rust/ockam/ockam_command/src/message/send.rs
+++ implementations/rust/ockam/ockam_command/src/message/send.rs
@@ -6,10 +6,10 @@ use ockam::{Context, TcpTransport};
 use ockam_multiaddr::MultiAddr;
 
 use crate::util::embedded_node;
-
 #[derive(Clone, Debug, Args)]
 pub struct SendCommand {
-    addr: String,
+    #[clap(flatten)]
+    addr: HyphenatedMultiAddr,
     message: String,
 }
 
@@ -19,38 +19,57 @@ impl SendCommand {
     }
 }
 
+#[derive(Clone, Debug, Args)]
+struct HyphenatedMultiAddr {
+    addr: String,
+}
+
+impl HyphenatedMultiAddr {
+    #[allow(dead_code)]
+    fn new(addr: &str) -> Self {
+        HyphenatedMultiAddr {
+            addr: addr.to_string(),
+        }
+    }
+
+    fn multiaddr(&self) -> Result<MultiAddr, anyhow::Error> {
+        let addr = match &self.addr {
+            addr if addr.contains("/-") => {
+                let mut buffer = String::new();
+                io::stdin().read_line(&mut buffer)?;
+                let args_from_stdin = buffer
+                    .trim()
+                    .split('/')
+                    .filter(|&s| !s.is_empty())
+                    .fold("".to_owned(), |acc, s| format!("{acc}/{s}"));
+
+                addr.replace("/-", &args_from_stdin)
+            }
+            addr if addr.contains("-/") => {
+                let mut buffer = String::new();
+                io::stdin().read_line(&mut buffer)?;
+
+                let args_from_stdin = buffer
+                    .trim()
+                    .split('/')
+                    .filter(|&s| !s.is_empty())
+                    .fold("/".to_owned(), |acc, s| format!("{acc}{s}/"));
+
+                addr.replace("-/", &args_from_stdin)
+            }
+            _ => self.addr.to_owned(),
+        };
+
+        let multi_addr = MultiAddr::from_str(&addr)?;
+        Ok(multi_addr)
+    }
+}
+
 async fn send_message(mut ctx: Context, cmd: SendCommand) -> anyhow::Result<()> {
     let _tcp = TcpTransport::create(&ctx).await?;
 
-    let addr = match cmd.addr {
-        addr if addr.contains("/-") => {
-            let mut buffer = String::new();
-            io::stdin().read_line(&mut buffer)?;
-            let args_from_stdin = buffer
-                .trim()
-                .split('/')
-                .filter(|&s| !s.is_empty())
-                .fold("".to_owned(), |acc, s| format!("{acc}/{s}"));
-
-            addr.replace("/-", &args_from_stdin)
-        }
-        addr if addr.contains("-/") => {
-            let mut buffer = String::new();
-            io::stdin().read_line(&mut buffer)?;
-
-            let args_from_stdin = buffer
-                .trim()
-                .split('/')
-                .filter(|&s| !s.is_empty())
-                .fold("/".to_owned(), |acc, s| format!("{acc}{s}/"));
-
-            addr.replace("-/", &args_from_stdin)
-        }
-        _ => cmd.addr,
-    };
-
-    let addr = MultiAddr::from_str(&addr)?;
-    if let Some(route) = ockam_api::multiaddr_to_route(&addr) {
+    let multi_addr = cmd.addr.multiaddr()?;
+    if let Some(route) = ockam_api::multiaddr_to_route(&multi_addr) {
         ctx.send(route, cmd.message).await?;
         let message = ctx.receive::<String>().await?;
         println!("{}", message);
