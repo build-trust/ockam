e6459c258ccbe29e7fa99aea3f68a0c80b67f072
Pro465 <namitapro0987@gmail.com>
2022-08-05 15:57:42 +0530

    chose(rust): try to fix errors

--- implementations/rust/ockam/ockam_command/src/lib.rs
+++ implementations/rust/ockam/ockam_command/src/lib.rs
@@ -242,9 +242,9 @@ pub enum OckamSubcommand {
 }
 
 fn replace_hyphen_with_stdin(s: String) -> String {
-    if a.contains("/-") {
+    if s.contains("/-") {
         let mut buffer = String::new();
-        io::stdin()
+        std::io::stdin()
             .read_line(&mut buffer)
             .expect("could not read from standard input");
         let args_from_stdin = buffer
@@ -256,7 +256,7 @@ fn replace_hyphen_with_stdin(s: String) -> String {
         s.replace("/-", &args_from_stdin)
     } else if s.contains("-/") {
         let mut buffer = String::new();
-        io::stdin()
+        std::io::stdin()
             .read_line(&mut buffer)
             .expect("could not read from standard input");
 
