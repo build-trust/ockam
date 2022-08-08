74025043f3e013194230fd9391e203b3827c35d5
Tim Chan <tim@chan.net>
2022-08-07 17:12:27 -0700

    feat(rust): long_version should display git hash

---
+++ implementations/rust/ockam/ockam_command/build.rs
@@ -0,0 +1,14 @@
+use std::process::Command;
+
+fn hash() {
+    let output = Command::new("git")
+        .args(&["rev-parse", "HEAD"])
+        .output()
+        .unwrap();
+    let git_hash = String::from_utf8(output.stdout).unwrap();
+    println!("cargo:rustc-env=GIT_HASH={}", git_hash);
+}
+
+fn main() {
+    hash();
+}
