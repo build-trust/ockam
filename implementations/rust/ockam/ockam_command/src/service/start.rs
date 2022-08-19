de44e05602deb069c3268b66e45f0c80a21fa46a
Toralf Wittner <tw@dtex.org>
2022-08-19 17:04:48 +0200

    feat(rust): add launch configuration to start services
    Example:
    
    ```json
    {
      "startup_services": {
        "vault": {},
        "identity": {
          "address": "identity_service"
        },
        "verifier": {
          "address": "verify"
          "disabled": true
        },
        "authenticator": {
          "enrollers": "/tmp/enrollers.json",
          "project": "p42"
        }
      }
    }
    ```

--- implementations/rust/ockam/ockam_command/src/service/start.rs
+++ implementations/rust/ockam/ockam_command/src/service/start.rs
@@ -1,22 +1,22 @@
-use std::path::PathBuf;
-
 use crate::node::NodeOpts;
 use crate::util::{api, connect_to, exitcode, stop_node};
 use crate::CommandGlobalOpts;
-use anyhow::{anyhow, Context};
+use anyhow::{anyhow, Context as _, Result};
 use clap::{Args, Subcommand};
 use minicbor::Decoder;
+use ockam::Context;
 use ockam_api::error::ApiError;
 use ockam_api::nodes::models::services::{StartAuthenticatorRequest, StartVerifierService};
 use ockam_api::nodes::NODEMANAGER_ADDR;
 use ockam_core::api::{Error, Method, Request, Response, Status};
 use ockam_core::Route;
+use std::path::PathBuf;
 use tracing::debug;
 
 #[derive(Clone, Debug, Args)]
 pub struct StartCommand {
     #[clap(flatten)]
-    node_opts: NodeOpts,
+    pub node_opts: NodeOpts,
 
     #[clap(subcommand)]
     pub create_subcommand: StartSubCommand,
@@ -25,13 +25,16 @@ pub struct StartCommand {
 #[derive(Clone, Debug, Subcommand)]
 pub enum StartSubCommand {
     Vault {
-        addr: Option<String>,
+        #[clap(default_value = "vault_service")]
+        addr: String,
     },
     Identity {
-        addr: Option<String>,
+        #[clap(default_value = "identity_service")]
+        addr: String,
     },
     Authenticated {
-        addr: Option<String>,
+        #[clap(default_value = "authenticated")]
+        addr: String,
     },
     Verifier {
         #[clap(long, default_value = "verifier")]
@@ -50,7 +53,7 @@ pub enum StartSubCommand {
 }
 
 impl StartCommand {
-    pub fn run(opts: CommandGlobalOpts, command: StartCommand) -> anyhow::Result<()> {
+    pub fn run(opts: CommandGlobalOpts, command: StartCommand) -> Result<()> {
         let cfg = opts.config;
         let port = match cfg.select_node(&command.node_opts.api_node) {
             Some(cfg) => cfg.port,
@@ -61,14 +64,38 @@ impl StartCommand {
         };
 
         match command.create_subcommand {
-            StartSubCommand::Vault { .. } => connect_to(port, command, start_vault_service),
-            StartSubCommand::Identity { .. } => connect_to(port, command, start_identity_service),
-            StartSubCommand::Authenticated { .. } => {
-                connect_to(port, command, start_authenticated_service)
+            StartSubCommand::Vault { .. } => connect_to(port, command, |mut ctx, cmd, rte| async {
+                let a = start_vault_service(&mut ctx, cmd, rte).await;
+                let b = stop_node(ctx).await;
+                a.and(b)
+            }),
+            StartSubCommand::Identity { .. } => {
+                connect_to(port, command, |mut ctx, cmd, rte| async {
+                    let a = start_identity_service(&mut ctx, cmd, rte).await;
+                    let b = stop_node(ctx).await;
+                    a.and(b)
+                })
             }
-            StartSubCommand::Verifier { .. } => connect_to(port, command, start_verifier_service),
+            StartSubCommand::Authenticated { .. } => {
+                connect_to(port, command, |mut ctx, cmd, rte| async {
+                    let a = start_authenticated_service(&mut ctx, cmd, rte).await;
+                    let b = stop_node(ctx).await;
+                    a.and(b)
+                })
+            }
+            StartSubCommand::Verifier { .. } => {
+                connect_to(port, command, |mut ctx, cmd, rte| async {
+                    let a = start_verifier_service(&mut ctx, cmd, rte).await;
+                    let b = stop_node(ctx).await;
+                    a.and(b)
+                })
+            }
             StartSubCommand::Authenticator { .. } => {
-                connect_to(port, command, start_authenticator_service)
+                connect_to(port, command, |mut ctx, cmd, rte| async {
+                    let a = start_authenticator_service(&mut ctx, cmd, rte).await;
+                    let b = stop_node(ctx).await;
+                    a.and(b)
+                })
             }
         }
 
@@ -77,16 +104,14 @@ impl StartCommand {
 }
 
 pub async fn start_vault_service(
-    ctx: ockam::Context,
+    ctx: &mut Context,
     cmd: StartCommand,
     mut base_route: Route,
-) -> anyhow::Result<()> {
+) -> Result<()> {
     let addr = match cmd.create_subcommand {
         StartSubCommand::Vault { addr, .. } => addr,
         _ => return Err(ApiError::generic("Internal logic error").into()),
     };
-
-    let addr = addr.unwrap_or_else(|| "vault_service".to_string());
 
     let response: Vec<u8> = ctx
         .send_and_receive(
@@ -116,27 +141,26 @@ pub async fn start_vault_service(
         _ => Err(anyhow!("Unexpected response received from node")),
     };
     match res {
-        Ok(o) => println!("{o}"),
+        Ok(o) => {
+            println!("{o}");
+            Ok(())
+        }
         Err(err) => {
             eprintln!("{err}");
             std::process::exit(exitcode::IOERR);
         }
-    };
-
-    stop_node(ctx).await
+    }
 }
 
 pub async fn start_identity_service(
-    ctx: ockam::Context,
+    ctx: &mut Context,
     cmd: StartCommand,
     mut base_route: Route,
-) -> anyhow::Result<()> {
+) -> Result<()> {
     let addr = match cmd.create_subcommand {
         StartSubCommand::Identity { addr, .. } => addr,
         _ => return Err(ApiError::generic("Internal logic error").into()),
     };
-
-    let addr = addr.unwrap_or_else(|| "identity_service".to_string());
 
     let response: Vec<u8> = ctx
         .send_and_receive(
@@ -166,27 +190,26 @@ pub async fn start_identity_service(
         _ => Err(anyhow!("Unexpected response received from node")),
     };
     match res {
-        Ok(o) => println!("{o}"),
+        Ok(o) => {
+            println!("{o}");
+            Ok(())
+        }
         Err(err) => {
             eprintln!("{err}");
             std::process::exit(exitcode::IOERR);
         }
-    };
-
-    stop_node(ctx).await
+    }
 }
 
 pub async fn start_authenticated_service(
-    ctx: ockam::Context,
+    ctx: &mut Context,
     cmd: StartCommand,
     mut base_route: Route,
-) -> anyhow::Result<()> {
+) -> Result<()> {
     let addr = match cmd.create_subcommand {
         StartSubCommand::Authenticated { addr, .. } => addr,
         _ => return Err(ApiError::generic("Internal logic error").into()),
     };
-
-    let addr = addr.unwrap_or_else(|| "authenticated".to_string());
 
     let response: Vec<u8> = ctx
         .send_and_receive(
@@ -216,21 +239,22 @@ pub async fn start_authenticated_service(
         _ => Err(anyhow!("Unexpected response received from node")),
     };
     match res {
-        Ok(o) => println!("{o}"),
+        Ok(o) => {
+            println!("{o}");
+            Ok(())
+        }
         Err(err) => {
             eprintln!("{err}");
             std::process::exit(exitcode::IOERR);
         }
-    };
-
-    stop_node(ctx).await
+    }
 }
 
 pub async fn start_verifier_service(
-    ctx: ockam::Context,
+    ctx: &mut Context,
     cmd: StartCommand,
     mut route: Route,
-) -> anyhow::Result<()> {
+) -> Result<()> {
     let addr = match cmd.create_subcommand {
         StartSubCommand::Verifier { addr } => addr,
         _ => unreachable!(),
@@ -249,27 +273,25 @@ pub async fn start_verifier_service(
 
     if let Some(Status::Ok) = hdr.status() {
         println!("Verifier service started at address: {addr}");
-        return stop_node(ctx).await;
+        return Ok(());
     }
 
     if hdr.has_body() {
         if let Ok(err) = dec.decode::<Error>() {
             if let Some(msg) = err.message() {
-                eprintln!("Failed to start verifier service: {}", msg);
-                return stop_node(ctx).await;
+                return Err(anyhow!("Failed to start verifier service: {}", msg));
             }
         }
     }
 
-    eprintln!("Failed to start verifier service");
-    stop_node(ctx).await
+    Err(anyhow!("Failed to start verifier service"))
 }
 
 pub async fn start_authenticator_service(
-    ctx: ockam::Context,
+    ctx: &mut Context,
     cmd: StartCommand,
     mut route: Route,
-) -> anyhow::Result<()> {
+) -> Result<()> {
     let (addr, enrollers, project) = match cmd.create_subcommand {
         StartSubCommand::Authenticator {
             addr: a,
@@ -296,18 +318,16 @@ pub async fn start_authenticator_service(
 
     if let Some(Status::Ok) = hdr.status() {
         println!("Authenticator service started at address: {addr}");
-        return stop_node(ctx).await;
+        return Ok(());
     }
 
     if hdr.has_body() {
         if let Ok(err) = dec.decode::<Error>() {
             if let Some(msg) = err.message() {
-                eprintln!("Failed to start authenticator service: {}", msg);
-                return stop_node(ctx).await;
+                return Err(anyhow!("Failed to start authenticator service: {}", msg));
             }
         }
     }
 
-    eprintln!("Failed to start authenticator service");
-    stop_node(ctx).await
+    Err(anyhow!("Failed to start authenticator service"))
 }
