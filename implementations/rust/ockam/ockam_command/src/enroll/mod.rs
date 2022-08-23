8b3fba20331fb27034852a226d7cf8728b4a6b9c
Mrinal Wadhwa <mrinal@ockam.io>
2022-08-23 16:48:35 -0700

    feat(rust): remove email enroll and enrollment token commands

--- implementations/rust/ockam/ockam_command/src/enroll/mod.rs
+++ implementations/rust/ockam/ockam_command/src/enroll/mod.rs
@@ -2,18 +2,12 @@ use clap::Args;
 
 pub use auth0::Auth0Service;
 use auth0::*;
-use enrollment_token_authenticate::*;
-pub use enrollment_token_generate::GenerateEnrollmentTokenCommand;
 
-use crate::enroll::email::EnrollEmailCommand;
 use crate::node::NodeOpts;
 use crate::util::api::CloudOpts;
 use crate::CommandGlobalOpts;
 
 mod auth0;
-mod email;
-mod enrollment_token_authenticate;
-mod enrollment_token_generate;
 
 #[derive(Clone, Debug, Args)]
 pub struct EnrollCommand {
@@ -22,24 +16,10 @@ pub struct EnrollCommand {
 
     #[clap(flatten)]
     pub cloud_opts: CloudOpts,
-
-    /// Authenticates an enrollment token
-    #[clap(display_order = 1003, long, group = "enroll_params")]
-    pub token: Option<String>,
-
-    /// Enroll using the Auth0 flow
-    #[clap(display_order = 1004, long, group = "enroll_params")]
-    auth0: bool,
 }
 
 impl EnrollCommand {
     pub fn run(opts: CommandGlobalOpts, cmd: EnrollCommand) {
-        if cmd.token.is_some() {
-            AuthenticateEnrollmentTokenCommand::run(opts, cmd)
-        } else if cmd.auth0 {
-            EnrollAuth0Command::run(opts, cmd)
-        } else {
-            EnrollEmailCommand::run(opts, cmd)
-        }
+        EnrollAuth0Command::run(opts, cmd)
     }
 }
