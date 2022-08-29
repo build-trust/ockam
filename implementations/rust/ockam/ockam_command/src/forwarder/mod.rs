c3b08f193518bfa27db1d62b18a4ff5df06b8a3f
Mrinal Wadhwa <mrinal@ockam.io>
2022-08-29 01:33:55 -0700

    feat(rust): improve forwarder help and args for `forwarder create`

--- implementations/rust/ockam/ockam_command/src/forwarder/mod.rs
+++ implementations/rust/ockam/ockam_command/src/forwarder/mod.rs
@@ -7,24 +7,53 @@ use crate::{help, CommandGlobalOpts};
 mod create;
 
 const HELP_DETAIL: &str = "\
-EXAMPLES:
+ABOUT:
+    Forwarders enable an ockam node to register a forwarding address on another node.
+    Any message that arrives at this forwarding address is immediately dispatched
+    to the node that registered the forwarding address.
 
 ```sh
-    # Create two nodes
-    $ ockam node create n1
-    $ ockam node create n2
+    # Create two nodes blue and green
+    $ ockam node create blue
+    $ ockam node create green
 
     # Create a forwarder to node n2 at node n1
-    $ ockam forwarder create --from forwarder_to_n2 --for /node/n2 --at /node/n1
+    $ ockam forwarder create blue --at /node/green --to /node/blue
+    /service/forward_to_blue
 
-    # Send message via the forwarder
-    $ ockam message send hello --to /node/n1/service/forwarder_to_n2/service/uppercase
+    # Send a message to the uppercase service on blue via its forwarder on green
+    $ ockam message send hello --to /node/green/service/forward_to_blue/service/uppercase
 ```
+
+    This can be very useful in establishing communication between applications
+    that cannot otherwise reach each other over the network.
+
+    For instance, we can use forwarders to create an end-to-end secure channel between
+    two nodes that are behind private NATs
+
+```sh
+    # Create another node called yellow
+    $ ockam node create yellow
+
+    # Create an end-to-end secure channel between yellow and blue.
+    # This secure channel is created trough blue's forwarder at green and we can
+    # send end-to-end encrypted messages through it.
+    $ ockam secure-channel create --from /node/yellow --to /node/green/service/forward_to_blue/service/api \\
+        | ockam message send hello --from /node/yellow --to -/service/uppercase
+```
+
+    In this topology green acts an an encrypted relay between yellow and blue. Yellow and
+    blue can be running in completely separate private networks. Green needs to be reachable
+    from both yellow and blue and only sees encrypted traffic.
 ";
 
 /// Manage Forwarders
 #[derive(Clone, Debug, Args)]
-#[clap(help_template = help::template(HELP_DETAIL))]
+#[clap(
+    arg_required_else_help = true,
+    subcommand_required = true,
+    help_template = help::template(HELP_DETAIL)
+)]
 pub struct ForwarderCommand {
     #[clap(subcommand)]
     subcommand: ForwarderSubCommand,
