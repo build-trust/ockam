a2fbd75c75087c6fb5a5f4736e6746633e85d9f4
Akshay Kulkarni <akshayk.gj@gmail.com>
2022-10-06 23:54:23 +0530

    fix(rust): get default node command to stop mapping errors manually

--- implementations/rust/ockam/ockam_command/src/configuration/get_default_node.rs
+++ implementations/rust/ockam/ockam_command/src/configuration/get_default_node.rs
@@ -1,19 +1,30 @@
-use crate::{util::exitcode, CommandGlobalOpts};
+use anyhow::anyhow;
 use clap::Args;
+
+use crate::exitcode::UNAVAILABLE;
+use crate::CommandGlobalOpts;
 
 #[derive(Clone, Debug, Args)]
 pub struct GetDefaultNodeCommand {}
 
 impl GetDefaultNodeCommand {
     pub fn run(self, options: CommandGlobalOpts) {
-        match options.config.get_default_node() {
-            Some(name) => {
-                println!("Current Default Node: {}", name)
-            }
-            None => {
-                eprintln!("Default Node is not set");
-                std::process::exit(exitcode::UNAVAILABLE);
-            }
+        if let Err(e) = run_impl(options) {
+            eprintln!("{}", e);
+            std::process::exit(e.code());
         }
+    }
+}
+
+fn run_impl(opts: CommandGlobalOpts) -> crate::Result<()> {
+    match opts.config.get_default_node() {
+        Some(name) => {
+            println!("Current Default Node: {}", name);
+            Ok(())
+        }
+        None => Err(crate::error::Error::new(
+            UNAVAILABLE,
+            anyhow!("Default Node is not set"),
+        )),
     }
 }
