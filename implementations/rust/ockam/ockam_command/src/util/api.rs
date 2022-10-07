bde4ef8f384be331f5c9d10356a6f67b282728e6
windlessstorm <vikrant.biswas@gmail.com>
2022-10-07 22:55:34 +0530

    feat(rust): validate project/space name for create command

--- implementations/rust/ockam/ockam_command/src/util/api.rs
+++ implementations/rust/ockam/ockam_command/src/util/api.rs
@@ -1,9 +1,10 @@
 //! API shim to make it nicer to interact with the ockam messaging API
 
+use regex::Regex;
 use std::path::Path;
 use std::str::FromStr;
 
-use anyhow::Context;
+use anyhow::{anyhow, Context};
 use clap::Args;
 // TODO: maybe we can remove this cross-dependency inside the CLI?
 use minicbor::Decoder;
@@ -457,5 +458,70 @@ impl CloudOpts {
         MultiAddr::from_str(&route)
             .context(format!("invalid Controller route: {route}"))
             .unwrap()
+    }
+}
+
+////////////// !== validators
+
+pub(crate) fn validate_cloud_resource_name(s: &str) -> anyhow::Result<()> {
+    let project_name_regex = Regex::new(r"^[a-zA-Z0-9]+([a-zA-Z0-9-_\.]?[a-zA-Z0-9])*$").unwrap();
+    let is_project_name_valid = project_name_regex.is_match(s);
+    if !is_project_name_valid {
+        Err(anyhow!("Invalid name"))
+    } else {
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test {
+
+    use crate::util::api::validate_cloud_resource_name;
+
+    #[test]
+    fn test_validate_cloud_resource_name() {
+        let valid_names: Vec<&str> = vec![
+            "name",
+            "0001",
+            "321_11-11-22",
+            "0_0",
+            "6.9",
+            "0-9",
+            "name_with_underscores",
+            "name-with-dashes",
+            "name.with.dots",
+            "name1with2numbers3",
+            "11name22with33numbers00",
+            "76123long.name_with-underscores.and-dashes_and3dots00and.numbers",
+        ];
+        for name in valid_names {
+            assert!(validate_cloud_resource_name(name).is_ok());
+        }
+
+        let invalid_names: Vec<&str> = vec![
+            "name with spaces in between",
+            " name-with-leading-space",
+            "name.with.trailing.space ",
+            " name-with-leading-and-trailing-space ",
+            "     name_with_multiple_leading_space",
+            "name__with_consecutive_underscore",
+            "_name_with_leading_underscore",
+            "name-with-traling-underscore_",
+            "name_with_consecutive---dashes",
+            "name_with_trailing_dashes--",
+            "---name_with_leading_dashes",
+            "name-with-consecutive...dots",
+            "name.with.trailing.dots....",
+            ".name_with-leading.dot",
+            "name_.with._consecutive-_-dots.-.dashes-._underscores",
+            "1 2 3 4",
+            "  1234",
+            "_",
+            "__",
+            ". _ .",
+        ];
+        for name in invalid_names {
+            assert!(validate_cloud_resource_name(name).is_err());
+        }
     }
 }
