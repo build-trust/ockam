93566fdb1aa83f75cad4bce1fdb94aacac5dc640
micheal <keinesmicheal@gmail.com>
2022-10-29 20:24:41 +0530

    feat(rust): added option to create identity using a vault

--- implementations/rust/ockam/ockam_command/src/identity/create.rs
+++ implementations/rust/ockam/ockam_command/src/identity/create.rs
@@ -1,17 +1,30 @@
-use crate::help;
 use crate::node::NodeOpts;
+use crate::util::exitcode::CANTCREAT;
 use crate::util::{node_rpc, Rpc};
 use crate::CommandGlobalOpts;
-use clap::Args;
+use anyhow::anyhow;
+use clap::{ArgGroup, Args};
 use ockam::Context;
+use ockam_api::config::cli;
 use ockam_api::nodes::models::identity::CreateIdentityResponse;
 use ockam_core::api::Request;
+use ockam_vault::storage::FileStorage;
+use slug::slugify;
 
 #[derive(Clone, Debug, Args)]
-#[command(hide = help::hide())]
+#[command(group(
+   ArgGroup::new("i")
+    .args(["node", "identity_name"]),
+), arg_required_else_help = true)]
 pub struct CreateCommand {
     #[command(flatten)]
-    node_opts: NodeOpts,
+    node_opts: Option<NodeOpts>,
+
+    #[arg(long = "identity", conflicts_with = "node", requires = "vault_name")]
+    identity_name: Option<String>,
+
+    #[arg(long = "vault", conflicts_with = "node", requires = "identity_name")]
+    vault_name: Option<String>,
 }
 
 impl CreateCommand {
@@ -24,10 +37,45 @@ async fn run_impl(
     ctx: Context,
     (options, cmd): (CommandGlobalOpts, CreateCommand),
 ) -> crate::Result<()> {
-    let mut rpc = Rpc::background(&ctx, &options, &cmd.node_opts.api_node)?;
-    let request = Request::post("/node/identity");
-    rpc.request(request).await?;
-    let res = rpc.parse_response::<CreateIdentityResponse>()?;
-    println!("Identity {} created!", res.identity_id);
-    Ok(())
+    match (cmd.node_opts, cmd.identity_name, cmd.vault_name) {
+        (Some(node_opts), None, None) => {
+            let mut rpc = Rpc::background(&ctx, &options, &node_opts.api_node)?;
+            let request = Request::post("/node/identity");
+            rpc.request(request).await?;
+            let res = rpc.parse_response::<CreateIdentityResponse>()?;
+            println!("Identity {} created!", res.identity_id);
+            Ok(())
+        }
+        (None, Some(identity_name), Some(vault_name)) => {
+            let dirs = cli::OckamConfig::directories();
+            let dir = dirs
+                .config_dir()
+                .to_path_buf()
+                .join("vaults")
+                .join(slugify(&format!("vault-{}", vault_name)));
+
+            if !dir.as_path().exists() {
+                return Err(crate::error::Error::new(
+                    CANTCREAT,
+                    anyhow!("Vault with name {} doesn't exists!", vault_name),
+                ));
+            }
+
+            let identity_path = format!("{}.json", slugify(&format!("identity-{}", identity_name)));
+
+            if dir.join(&identity_path).as_path().exists() {
+                return Err(crate::error::Error::new(
+                    CANTCREAT,
+                    anyhow!("Identity with name {} already exists!", identity_name),
+                ));
+            }
+
+            let file = dir.join(&identity_path);
+            let _ = FileStorage::create(file.clone()).await?;
+
+            println!("Identity {} created in vault {}", identity_name, vault_name);
+            Ok(())
+        }
+        _ => unreachable!(),
+    }
 }
