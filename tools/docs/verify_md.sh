abc293244ee141b1165d1f86e6904bf2ea80b337
Andrew Grangaard <granny-github@ofb.net>
2022-10-30 23:51:24 -0700

    ci: apply easy shellcheck warnings

--- tools/docs/verify_md.sh
+++ tools/docs/verify_md.sh
@@ -2,12 +2,11 @@
 
 FILE=$1
 TMP=$(mktemp)
-example_blocks $FILE >$TMP
-cmp -s $FILE $TMP
-if [[ $? -ne 0 ]]; then
+example_blocks "$FILE" >"$TMP"
+if ! cmp -s "$FILE" "$TMP"; then
   echo "$FILE examples are not up to date. See diff below."
   echo "===================="
-  diff -c $FILE $TMP
+  diff -c "$FILE" "$TMP"
   echo "===================="
   echo
   exit 1
--- tools/scripts/release/changelog.sh
+++ tools/scripts/release/changelog.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 set -e
 
 # This script generates changelog for all Ockam crates that
@@ -12,13 +12,13 @@ for crate in $(echo "$updated_crates"); do
   # the below message.
   echo "Generating changelog for $crate with tag $last_git_tag"
   with_commit_msg="feat: updated dependencies"
-  git-cliff $last_git_tag.. --config tools/cliff/cliff.toml --with-commit "$with_commit_msg" --include-path implementations/rust/ockam/$crate/**/*.rs --prepend implementations/rust/ockam/$crate/CHANGELOG.md
+  git-cliff "$last_git_tag".. --config tools/cliff/cliff.toml --with-commit "$with_commit_msg" --include-path "$crate"/**/*.rs --prepend "$crate"/CHANGELOG.md
   # Replace ## unreleased text to bumped version
-  version=$(eval "tomlq package.version -f implementations/rust/ockam/$crate/Cargo.toml")
+  version=$(eval "tomlq package.version -f $crate/Cargo.toml")
 
   search="## unreleased"
   replace="## $version - $(date +'%Y-%m-%d')"
-  sed -i -e "s/$search/$replace/" implementations/rust/ockam/$crate/CHANGELOG.md
+  sed -i -e "s/$search/$replace/" "$crate"/CHANGELOG.md
 done
 
 echo "Changelog has been generated. Please review and commit."
