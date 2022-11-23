02611d6130178d68f6a55f2840be60b1725c6561
Oleksandr Deundiak <olek@ockam.io>
2022-11-23 16:00:18 +0100

    feat(rust): improve access control support for `ockam::node` macro

--- implementations/rust/ockam/ockam_macros/src/internals/symbol.rs
+++ implementations/rust/ockam/ockam_macros/src/internals/symbol.rs
@@ -7,7 +7,8 @@ use syn::{Ident, Path};
 pub(crate) struct Symbol(&'static str);
 
 // Attributes
-pub(crate) const ACCESS_CONTROL: Symbol = Symbol("access_control");
+pub(crate) const INCOMING_ACCESS_CONTROL: Symbol = Symbol("incoming");
+pub(crate) const OUTGOING_ACCESS_CONTROL: Symbol = Symbol("outgoing");
 pub(crate) const NO_MAIN: Symbol = Symbol("no_main");
 pub(crate) const OCKAM_CRATE: Symbol = Symbol("crate");
 pub(crate) const TIMEOUT_MS: Symbol = Symbol("timeout");
