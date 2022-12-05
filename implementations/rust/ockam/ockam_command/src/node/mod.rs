5f2c7e79ce2e311ab6ca76641eb98de86652595d
etorreborre <etorreborre@yahoo.com>
2022-12-05 16:17:12 +0100

    fix(rust): fixed the display of the help for the node option
    in ockam identity show --help

--- implementations/rust/ockam/ockam_command/src/node/mod.rs
+++ implementations/rust/ockam/ockam_command/src/node/mod.rs
@@ -158,6 +158,13 @@ impl NodeCommand {
 #[derive(Clone, Debug, Args)]
 pub struct NodeOpts {
     /// Override the default API node
-    #[arg(global = true, id = "node", short, long, default_value = "default")]
+    #[arg(
+        global = true,
+        id = "node",
+        value_name = "NODE",
+        short,
+        long,
+        default_value = "default"
+    )]
     pub api_node: String,
 }
