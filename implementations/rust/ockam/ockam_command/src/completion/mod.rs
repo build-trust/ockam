bda1c7164c12ea35518da3b62e9250ae1dc9b3da
Brian Mego <brianmego@gmail.com>
2022-12-09 13:11:22 -0600

    refactor(rust): extract large strings into constants directory

--- implementations/rust/ockam/ockam_command/src/completion/mod.rs
+++ implementations/rust/ockam/ockam_command/src/completion/mod.rs
@@ -3,28 +3,7 @@ use clap::{Args, CommandFactory};
 use clap_complete::{generate, Shell};
 use std::io;
 
-const HELP_DETAIL: &str = "\
-About:
-    Generate shell completion scripts for Ockam commands.
-
-    If you’ve installed `ockam` command using a package manager, you likely
-    don't need to do any additional shell configuration to gain completion support.
-
-    If you need to set up completions manually, follow the instructions below.
-    The exact configuration file locations might vary based on your system. Remember
-    to restart your shell before testing whether completions are working.
-
-```sh
-    # BASH
-    $ ockam completion --shell bash > /usr/share/bash-completion/completions/ockam.bash
-
-    # ZSH
-    $ ockam completion --shell zsh > /usr/local/share/zsh/site-functions/_ockam
-
-    # FISH
-    $ ockam completion --shell fish > ~/.config/fish/completions/ockam.fish
-```
-";
+const HELP_DETAIL: &str = include_str!("../constants/completion/help_detail.txt");
 
 /// Generate shell completion scripts
 #[derive(Clone, Debug, Args)]
