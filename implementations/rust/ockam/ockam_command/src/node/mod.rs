5ed06645ec9bbb9be01994b369fb260bc5288d1b
Mrinal Wadhwa <mrinal@ockam.io>
2022-12-18 21:33:41 +0800

    chore(rust): fix clippy warning
    large size difference between variants

--- implementations/rust/ockam/ockam_command/src/node/mod.rs
+++ implementations/rust/ockam/ockam_command/src/node/mod.rs
@@ -129,7 +129,7 @@ pub struct NodeCommand {
 #[derive(Clone, Debug, Subcommand)]
 pub enum NodeSubcommand {
     #[command(display_order = 800)]
-    Create(CreateCommand),
+    Create(Box<CreateCommand>),
     #[command(display_order = 800)]
     Delete(DeleteCommand),
     #[command(display_order = 800)]
