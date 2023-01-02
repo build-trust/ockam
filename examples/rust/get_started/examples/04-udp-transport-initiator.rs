a06041e475c72de5536c7a41b16cbd8be26dca29
Oleksandr Deundiak <olek@ockam.io>
2023-01-02 17:51:03 +0100

    chore(rust): update udp example to use fixes port number

--- examples/rust/get_started/examples/04-udp-transport-initiator.rs
+++ examples/rust/get_started/examples/04-udp-transport-initiator.rs
@@ -9,10 +9,7 @@ async fn main(mut ctx: Context) -> Result<()> {
     let _udp = UdpTransport::create(&ctx).await?;
 
     // Send a message to the "echoer" worker, on a different node, over an udp transport.
-    // Wait to receive a reply and print it.
-    // Use port 4000, unless otherwise specified by command line argument.
-    let port = std::env::args().nth(1).unwrap_or_else(|| "4000".to_string());
-    let r = route![(UDP, format!("localhost:{port}")), "echoer"];
+    let r = route![(UDP, "localhost:4000"), "echoer"];
     ctx.send(r, "Hello Ockam!".to_string()).await?;
 
     // Wait to receive a reply and print it.
