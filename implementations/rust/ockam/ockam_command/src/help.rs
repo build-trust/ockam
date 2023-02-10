fbe310a185a64d024596746bacad05a7b1d74e0c
Davide Baldo <davide@baldo.me>
2023-02-10 23:24:02 +0100

    fix(rust): reduce cli bootstrap time by an order of magnitude for both release and dev profiles

--- implementations/rust/ockam/ockam_command/src/help.rs
+++ implementations/rust/ockam/ockam_command/src/help.rs
@@ -1,5 +1,7 @@
 use crate::terminal::{Terminal, TerminalBackground};
 use colorful::Colorful;
+use once_cell::sync::Lazy;
+use syntect::highlighting::Theme;
 use syntect::{
     easy::HighlightLines,
     highlighting::{Style, ThemeSet},
@@ -27,51 +29,59 @@ pub(crate) fn template(body: &str) -> &'static str {
     Box::leak(highlighted.into_boxed_str())
 }
 
-pub fn highlight_syntax(input: String) -> String {
+static SYNTAX_SET: Lazy<SyntaxSet> = Lazy::new(SyntaxSet::load_defaults_newlines);
+static RE: Lazy<Regex> = Lazy::new(|| Regex::new("^[A-Za-z][A-Za-z0-9 ]+:$".into()));
+static THEME: Lazy<Option<Theme>> = Lazy::new(|| {
     let theme_name = match Terminal::detect_background_color() {
         TerminalBackground::Light => "base16-ocean.light",
         TerminalBackground::Dark => "base16-ocean.dark",
-        TerminalBackground::Unknown => return input,
+        TerminalBackground::Unknown => return None,
     };
-    let theme_set = ThemeSet::load_defaults();
-    let theme = &theme_set.themes[theme_name];
-    let syntax_set = SyntaxSet::load_defaults_newlines();
-    let syntax = syntax_set.find_syntax_by_extension("sh").unwrap();
-    let mut highlighter = HighlightLines::new(syntax, theme);
+    let mut theme_set = ThemeSet::load_defaults();
+    let theme = theme_set.themes.remove(theme_name).unwrap();
+    Some(theme)
+});
 
+pub fn highlight_syntax(input: String) -> String {
     let mut highlighted: Vec<String> = Vec::new();
     let mut in_fenced_block = false;
 
-    for line in LinesWithEndings::from(input.as_str()) {
-        if line == "```sh\n" {
-            in_fenced_block = true;
-            continue;
-        }
+    if let Some(theme) = &*THEME {
+        let syntax_reference = SYNTAX_SET.find_syntax_by_extension("sh").unwrap();
 
-        if !in_fenced_block {
-            let re = Regex::new("^[A-Za-z][A-Za-z0-9 ]+:$".into());
-            if re.is_match(line) {
-                highlighted.push(line.to_string().bold().underlined().to_string());
-            } else {
-                highlighted.push(line.to_string());
+        let mut highlighter = HighlightLines::new(syntax_reference, theme);
+        for line in LinesWithEndings::from(input.as_str()) {
+            if line == "```sh\n" {
+                in_fenced_block = true;
+                continue;
             }
-            continue;
+
+            if !in_fenced_block {
+                if RE.is_match(line) {
+                    highlighted.push(line.to_string().bold().underlined().to_string());
+                } else {
+                    highlighted.push(line.to_string());
+                }
+                continue;
+            }
+
+            if line == "```\n" {
+                // Push a reset to clear the coloring.
+                highlighted.push("\x1b[0m".to_string());
+                in_fenced_block = false;
+                continue;
+            }
+
+            let ranges: Vec<(Style, &str)> = highlighter
+                .highlight_line(line, &SYNTAX_SET)
+                .unwrap_or_default();
+            highlighted.push(as_24_bit_terminal_escaped(&ranges[..], false));
         }
 
-        if line == "```\n" {
-            // Push a reset to clear the coloring.
-            highlighted.push("\x1b[0m".to_string());
-            in_fenced_block = false;
-            continue;
-        }
-
-        let ranges: Vec<(Style, &str)> = highlighter
-            .highlight_line(line, &syntax_set)
-            .unwrap_or_default();
-        highlighted.push(as_24_bit_terminal_escaped(&ranges[..], false));
+        highlighted.join("")
+    } else {
+        input
     }
-
-    highlighted.join("")
 }
 
 pub(crate) fn hide() -> bool {
