5c05fa87c574e1ccd61b2d05895ba722e4118f17
etorreborre <etorreborre@yahoo.com>
2023-02-20 15:28:15 +0100

    feat(rust): preload the credential issuer with attributes for alice and bob

--- examples/rust/get_started/examples/06-credentials-exchange-issuer.rs
+++ examples/rust/get_started/examples/06-credentials-exchange-issuer.rs
@@ -1,8 +1,18 @@
 use ockam::identity::credential_issuer::CredentialIssuer;
-use ockam::{Context, TcpTransport};
+use ockam::identity::TrustEveryonePolicy;
+use ockam::{Context, IdentityIdAccessControl, TcpTransport};
 use ockam_core::{AllowAll, Result};
 
 /// This node starts a temporary credential issuer accessible via TCP on localhost:5000
+///
+/// In a real-life scenario this node would be an "Authority", a node holding
+/// attributes for a number of identities and able to issue credentials signed with its own key.
+///
+/// The process by which we declare to that Authority which identity holds which attributes is an
+/// enrollment process and would be driven by an "enroller node".
+/// For the simplicity of the example provided here we preload the credential issues node with some existing attributes
+/// for both Alice's and Bob's identities.
+///
 #[ockam::node]
 async fn main(ctx: Context) -> Result<()> {
     // Initialize the TCP Transport.
@@ -11,7 +21,28 @@ async fn main(ctx: Context) -> Result<()> {
     // Create a TCP listener and wait for incoming connections.
     tcp.listen("127.0.0.1:5000").await?;
 
+    // Create a CredentialIssuer which stores attributes for Alice and Bob, knowing their identity
     let issuer = CredentialIssuer::create(&ctx).await?;
-    ctx.start_worker("issuer", issuer, AllowAll, AllowAll).await?;
+    let alice = "P529d43ac7b01e23d3818d00e083508790bfe8825714644b98134db6c1a7a6602".try_into()?;
+    let bob = "P0189a2aec3799fe9d0dc0f982063022b697f18562a403eb46fa3d32be5bd31f8".try_into()?;
+
+    issuer.put_attribute_value(&alice, "name", "alice").await?;
+    issuer.put_attribute_value(&bob, "name", "bob").await?;
+
+    // Start a secure channel listener that alice and bob can use to retrieve their credential
+    issuer
+        .identity()
+        .create_secure_channel_listener("issuer_listener", TrustEveryonePolicy)
+        .await?;
+    println!("created a secure channel listener");
+
+    ctx.start_worker(
+        "issuer",
+        issuer,
+        IdentityIdAccessControl::new(vec![alice, bob]),
+        AllowAll,
+    )
+    .await?;
+
     Ok(())
 }
--- examples/rust/get_started/src/lib.rs
+++ examples/rust/get_started/src/lib.rs
@@ -5,11 +5,13 @@ mod hop;
 pub use hop::*;
 
 mod credentials;
+mod identity;
 mod logger;
 mod project;
 mod token;
 
 pub use credentials::*;
+pub use identity::*;
 pub use logger::*;
 pub use project::*;
 pub use token::*;
