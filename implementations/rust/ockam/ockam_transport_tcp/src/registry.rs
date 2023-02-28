9194226e0ed5cf6fc89322c8734cbd5d0676a2d3
Oleksandr Deundiak <olek@ockam.io>
2023-02-28 18:16:14 +0100

    chore(rust): simplify `TcpRegistry`

--- implementations/rust/ockam/ockam_transport_tcp/src/registry.rs
+++ implementations/rust/ockam/ockam_transport_tcp/src/registry.rs
@@ -1,85 +1,7 @@
 use ockam_core::compat::sync::{Arc, RwLock};
 use ockam_core::Address;
 
-#[derive(Default)]
-struct InternalRegistry {
-    portal_workers: Vec<Address>,
-    portal_receiver_processors: Vec<Address>,
-    inlet_listener_processors: Vec<Address>,
-    outlet_listener_workers: Vec<Address>,
-    listener_processors: Vec<Address>,
-    sender_workers: Vec<Address>,
-    receiver_processors: Vec<Address>,
-}
-
-impl InternalRegistry {
-    fn add_portal_worker(&mut self, addr: &Address) {
-        self.portal_workers.push(addr.clone())
-    }
-    fn remove_portal_worker(&mut self, addr: &Address) {
-        if let Some(index) = self.portal_workers.iter().position(|x| x == addr) {
-            self.portal_workers.remove(index);
-        }
-    }
-    fn add_portal_receiver_processor(&mut self, addr: &Address) {
-        self.portal_receiver_processors.push(addr.clone())
-    }
-    fn remove_portal_receiver_processor(&mut self, addr: &Address) {
-        if let Some(index) = self
-            .portal_receiver_processors
-            .iter()
-            .position(|x| x == addr)
-        {
-            self.portal_receiver_processors.remove(index);
-        }
-    }
-    fn add_inlet_listener_processor(&mut self, addr: &Address) {
-        self.inlet_listener_processors.push(addr.clone())
-    }
-    fn remove_inlet_listener_processor(&mut self, addr: &Address) {
-        if let Some(index) = self
-            .inlet_listener_processors
-            .iter()
-            .position(|x| x == addr)
-        {
-            self.inlet_listener_processors.remove(index);
-        }
-    }
-    fn add_outlet_listener_worker(&mut self, addr: &Address) {
-        self.outlet_listener_workers.push(addr.clone())
-    }
-    fn remove_outlet_listener_worker(&mut self, addr: &Address) {
-        if let Some(index) = self.outlet_listener_workers.iter().position(|x| x == addr) {
-            self.outlet_listener_workers.remove(index);
-        }
-    }
-    fn add_listener_processor(&mut self, addr: &Address) {
-        self.listener_processors.push(addr.clone())
-    }
-    fn remove_listener_processor(&mut self, addr: &Address) {
-        if let Some(index) = self.listener_processors.iter().position(|x| x == addr) {
-            self.listener_processors.remove(index);
-        }
-    }
-    fn add_sender_worker(&mut self, addr: &Address) {
-        self.sender_workers.push(addr.clone())
-    }
-    fn remove_sender_worker(&mut self, addr: &Address) {
-        if let Some(index) = self.sender_workers.iter().position(|x| x == addr) {
-            self.sender_workers.remove(index);
-        }
-    }
-    fn add_receiver_processor(&mut self, addr: &Address) {
-        self.receiver_processors.push(addr.clone())
-    }
-    fn remove_receiver_processor(&mut self, addr: &Address) {
-        if let Some(index) = self.receiver_processors.iter().position(|x| x == addr) {
-            self.receiver_processors.remove(index);
-        }
-    }
-}
-
-/// Registry of all active workers and processors in TCP Transport
+/// Registry of all active workers and processors in TCP Transport to ease their lifecycle management
 #[derive(Default, Clone)]
 pub struct TcpRegistry {
     registry: Arc<RwLock<InternalRegistry>>,
@@ -155,5 +77,61 @@ impl TcpRegistry {
         if let Ok(mut lock) = self.registry.write() {
             lock.remove_receiver_processor(addr);
         }
+    }
+}
+
+#[derive(Default)]
+struct InternalRegistry {
+    portal_workers: Vec<Address>,
+    portal_receiver_processors: Vec<Address>,
+    inlet_listener_processors: Vec<Address>,
+    outlet_listener_workers: Vec<Address>,
+    listener_processors: Vec<Address>,
+    sender_workers: Vec<Address>,
+    receiver_processors: Vec<Address>,
+}
+
+impl InternalRegistry {
+    fn add_portal_worker(&mut self, addr: &Address) {
+        self.portal_workers.push(addr.clone())
+    }
+    fn remove_portal_worker(&mut self, addr: &Address) {
+        self.portal_workers.retain(|x| x != addr);
+    }
+    fn add_portal_receiver_processor(&mut self, addr: &Address) {
+        self.portal_receiver_processors.push(addr.clone())
+    }
+    fn remove_portal_receiver_processor(&mut self, addr: &Address) {
+        self.portal_receiver_processors.retain(|x| x != addr);
+    }
+    fn add_inlet_listener_processor(&mut self, addr: &Address) {
+        self.inlet_listener_processors.push(addr.clone())
+    }
+    fn remove_inlet_listener_processor(&mut self, addr: &Address) {
+        self.inlet_listener_processors.retain(|x| x != addr);
+    }
+    fn add_outlet_listener_worker(&mut self, addr: &Address) {
+        self.outlet_listener_workers.push(addr.clone())
+    }
+    fn remove_outlet_listener_worker(&mut self, addr: &Address) {
+        self.outlet_listener_workers.retain(|x| x != addr);
+    }
+    fn add_listener_processor(&mut self, addr: &Address) {
+        self.listener_processors.push(addr.clone())
+    }
+    fn remove_listener_processor(&mut self, addr: &Address) {
+        self.listener_processors.retain(|x| x != addr);
+    }
+    fn add_sender_worker(&mut self, addr: &Address) {
+        self.sender_workers.push(addr.clone())
+    }
+    fn remove_sender_worker(&mut self, addr: &Address) {
+        self.sender_workers.retain(|x| x != addr);
+    }
+    fn add_receiver_processor(&mut self, addr: &Address) {
+        self.receiver_processors.push(addr.clone())
+    }
+    fn remove_receiver_processor(&mut self, addr: &Address) {
+        self.receiver_processors.retain(|x| x != addr);
     }
 }
