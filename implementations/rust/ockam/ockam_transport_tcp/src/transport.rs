3c320f7eef3c9a8da9907d3ad42395f15514e34a
Oleksandr Deundiak <olek@ockam.io>
2023-03-07 10:33:39 +0100

    chore(rust): update tcp doc comment

--- implementations/rust/ockam/ockam_transport_tcp/src/transport.rs
+++ implementations/rust/ockam/ockam_transport_tcp/src/transport.rs
@@ -31,26 +31,26 @@ pub(crate) const CLUSTER_NAME: &str = "_internals.transport.tcp";
 /// establishing a connection upon arrival of an initial message.
 ///
 /// ```rust
-/// use ockam_transport_tcp::TcpTransport;
+/// use ockam_transport_tcp::{TcpConnectionTrustOptions, TcpListenerTrustOptions, TcpTransport};
 /// # use ockam_node::Context;
 /// # use ockam_core::Result;
 /// # async fn test(ctx: Context) -> Result<()> {
 /// let tcp = TcpTransport::create(&ctx).await?;
-/// tcp.listen("127.0.0.1:8000").await?; // Listen on port 8000
-/// tcp.connect("127.0.0.1:5000").await?; // And connect to port 5000
+/// tcp.listen("127.0.0.1:8000", TcpListenerTrustOptions::new()).await?; // Listen on port 8000
+/// tcp.connect("127.0.0.1:5000", TcpConnectionTrustOptions::new()).await?; // And connect to port 5000
 /// # Ok(()) }
 /// ```
 ///
 /// The same `TcpTransport` can also bind to multiple ports.
 ///
 /// ```rust
-/// use ockam_transport_tcp::TcpTransport;
+/// use ockam_transport_tcp::{TcpListenerTrustOptions, TcpTransport};
 /// # use ockam_node::Context;
 /// # use ockam_core::Result;
 /// # async fn test(ctx: Context) -> Result<()> {
 /// let tcp = TcpTransport::create(&ctx).await?;
-/// tcp.listen("127.0.0.1:8000").await?; // Listen on port 8000
-/// tcp.listen("127.0.0.1:9000").await?; // Listen on port 9000
+/// tcp.listen("127.0.0.1:8000", TcpListenerTrustOptions::new()).await?; // Listen on port 8000
+/// tcp.listen("127.0.0.1:9000", TcpListenerTrustOptions::new()).await?; // Listen on port 9000
 /// # Ok(()) }
 /// ```
 #[derive(AsyncTryClone)]
