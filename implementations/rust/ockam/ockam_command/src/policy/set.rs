85470946fe7b324b6f32f6168591a9ff6dfb9f24
Adrian Benavides <adri.benavides312@gmail.com>
2023-03-15 18:03:08 +0700

    refactor(rust): split policy subcommands in separated files

---
+++ implementations/rust/ockam/ockam_command/src/policy/set.rs
@@ -0,0 +1,44 @@
+use crate::policy::policy_path;
+use crate::util::{extract_address_value, node_rpc, Rpc};
+use crate::{CommandGlobalOpts, Result};
+use clap::Args;
+use ockam::Context;
+use ockam_abac::{Action, Expr, Resource};
+use ockam_api::nodes::models::policy::Policy;
+use ockam_core::api::Request;
+
+#[derive(Clone, Debug, Args)]
+pub struct SetCommand {
+    /// Node on which to start the tcp inlet.
+    #[arg(long, display_order = 900, id = "NODE")]
+    at: String,
+
+    #[arg(short, long)]
+    resource: Resource,
+
+    #[arg(short, long, default_value = "handle_message")]
+    action: Action,
+
+    #[arg(short, long)]
+    expression: Expr,
+}
+
+impl SetCommand {
+    pub fn run(self, options: CommandGlobalOpts) {
+        node_rpc(rpc, (options, self));
+    }
+}
+
+async fn rpc(mut ctx: Context, (opts, cmd): (CommandGlobalOpts, SetCommand)) -> Result<()> {
+    run_impl(&mut ctx, opts, cmd).await
+}
+
+async fn run_impl(ctx: &mut Context, opts: CommandGlobalOpts, cmd: SetCommand) -> Result<()> {
+    let node = extract_address_value(&cmd.at)?;
+    let bdy = Policy::new(cmd.expression);
+    let req = Request::post(policy_path(&cmd.resource, &cmd.action)).body(bdy);
+    let mut rpc = Rpc::background(ctx, &opts, &node)?;
+    rpc.request(req).await?;
+    rpc.is_ok()?;
+    Ok(())
+}
