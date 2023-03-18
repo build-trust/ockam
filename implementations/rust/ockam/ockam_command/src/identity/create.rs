395b22f96901f5cafa9ac7281f9a84238f769e6e
etorreborre <etorreborre@yahoo.com>
2023-03-18 13:32:49 +0100

    feat(rust): create an authority identity if it has not been created before

--- implementations/rust/ockam/ockam_command/src/identity/create.rs
+++ implementations/rust/ockam/ockam_command/src/identity/create.rs
@@ -4,7 +4,7 @@ use clap::Args;
 use ockam::Context;
 use ockam_api::cli_state::{self, VaultConfig};
 use ockam_core::compat::sync::Arc;
-use ockam_identity::Identity;
+use ockam_identity::{Identity, PublicIdentity};
 use rand::prelude::random;
 
 #[derive(Clone, Debug, Args)]
@@ -18,41 +18,53 @@ pub struct CreateCommand {
 }
 
 impl CreateCommand {
-    pub fn run(self, options: CommandGlobalOpts) {
-        node_rpc(run_impl, (options, self))
+    pub fn new(name: String, vault: Option<String>) -> CreateCommand {
+        CreateCommand { name, vault }
     }
-}
 
-async fn run_impl(
-    ctx: Context,
-    (options, cmd): (CommandGlobalOpts, CreateCommand),
-) -> crate::Result<()> {
-    let vault_state = if let Some(vault_name) = cmd.vault {
-        options.state.vaults.get(&vault_name)?
-    } else if options.state.vaults.default().is_err() {
-        let vault_name = hex::encode(random::<[u8; 4]>());
-        let state = options
+    pub fn run(self, options: CommandGlobalOpts) {
+        node_rpc(Self::run_impl, (options, self))
+    }
+
+    async fn run_impl(
+        ctx: Context,
+        (options, cmd): (CommandGlobalOpts, CreateCommand),
+    ) -> crate::Result<()> {
+        cmd.create_identity(ctx, options).await.map(|_| ())
+    }
+
+    pub async fn create_identity(
+        &self,
+        ctx: Context,
+        options: CommandGlobalOpts,
+    ) -> crate::Result<PublicIdentity> {
+        let vault_state = if let Some(vault_name) = self.vault.clone() {
+            options.state.vaults.get(&vault_name)?
+        } else if options.state.vaults.default().is_err() {
+            let vault_name = hex::encode(random::<[u8; 4]>());
+            let state = options
+                .state
+                .vaults
+                .create(&vault_name, VaultConfig::default())
+                .await?;
+            println!("Default vault created: {}", &vault_name);
+            state
+        } else {
+            options.state.vaults.default()?
+        };
+        let vault = vault_state.get().await?;
+        let identity = Identity::create_ext(
+            &ctx,
+            options.state.identities.authenticated_storage().await?,
+            Arc::new(vault),
+        )
+        .await?;
+        let identity_config = cli_state::IdentityConfig::new(&identity).await;
+        let identity_state = options
             .state
-            .vaults
-            .create(&vault_name, VaultConfig::default())
-            .await?;
-        println!("Default vault created: {}", &vault_name);
-        state
-    } else {
-        options.state.vaults.default()?
-    };
-    let vault = vault_state.get().await?;
-    let identity = Identity::create_ext(
-        &ctx,
-        options.state.identities.authenticated_storage().await?,
-        Arc::new(vault),
-    )
-    .await?;
-    let identity_config = cli_state::IdentityConfig::new(&identity).await;
-    options
-        .state
-        .identities
-        .create(&cmd.name, identity_config)?;
-    println!("Identity created: {}", identity.identifier());
-    Ok(())
+            .identities
+            .create(&self.name, identity_config)?;
+        println!("Identity created: {}", identity.identifier());
+        Ok(identity_state.config.public_identity())
+    }
 }
