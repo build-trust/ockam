3deb1d989681d9d18b292b8314c1ef7556f755e2
Oleksandr Deundiak <olek@ockam.io>
2023-03-21 16:09:38 +0100

    feat(rust): split `Context` into multiple files

---
+++ implementations/rust/ockam/ockam_node/src/context/receive_message.rs
@@ -0,0 +1,154 @@
+use crate::debugger;
+use crate::tokio::time::timeout;
+use crate::{error::*, parser};
+use crate::{Cancel, Context, DEFAULT_TIMEOUT};
+use core::sync::atomic::Ordering;
+use core::time::Duration;
+use ockam_core::{Address, LocalMessage, Message, RelayMessage, Result};
+
+impl Context {
+    /// Wait for the next message from the mailbox
+    pub(crate) async fn receiver_next(&mut self) -> Result<Option<RelayMessage>> {
+        loop {
+            let relay_msg = if let Some(msg) = self.receiver.recv().await.map(|msg| {
+                trace!("{}: received new message!", self.address());
+
+                // First we update the mailbox fill metrics
+                self.mailbox_count.fetch_sub(1, Ordering::Acquire);
+
+                msg
+            }) {
+                msg
+            } else {
+                // no more messages
+                return Ok(None);
+            };
+
+            debugger::log_incoming_message(self, &relay_msg);
+
+            if !self.mailboxes.is_incoming_authorized(&relay_msg).await? {
+                warn!(
+                    "Message received from {} for {} did not pass incoming access control",
+                    relay_msg.return_route(),
+                    relay_msg.destination()
+                );
+                continue;
+            }
+
+            return Ok(Some(relay_msg));
+        }
+    }
+
+    /// A convenience function to get a data 3-tuple from the mailbox
+    ///
+    /// The reason this function doesn't construct a `Cancel<_, M>` is
+    /// to avoid the lifetime collision between the mutation on `self`
+    /// and the ref to `Context` passed to `Cancel::new(..)`
+    ///
+    /// This function will block and re-queue messages into the
+    /// mailbox until it can receive the correct message payload.
+    ///
+    /// WARNING: this will temporarily create a busyloop, this
+    /// mechanism should be replaced with a waker system that lets the
+    /// mailbox work not yield another message until the relay worker
+    /// has woken it.
+    async fn next_from_mailbox<M: Message>(&mut self) -> Result<(M, LocalMessage, Address)> {
+        loop {
+            let msg = self
+                .receiver_next()
+                .await?
+                .ok_or_else(|| NodeError::Data.not_found())?;
+            let addr = msg.destination().clone();
+            let local_msg = msg.into_local_message();
+
+            // FIXME: make message parsing idempotent to avoid cloning
+            match parser::message(&local_msg.transport().payload).ok() {
+                Some(msg) => break Ok((msg, local_msg, addr)),
+                None => {
+                    // Requeue
+                    self.forward(local_msg).await?;
+                }
+            }
+        }
+    }
+
+    /// Block the current worker to wait for a typed message
+    ///
+    /// **Warning** this function will wait until its running ockam
+    /// node is shut down.  A safer variant of this function is
+    /// [`receive`](Self::receive) and
+    /// [`receive_timeout`](Self::receive_timeout).
+    pub async fn receive_block<M: Message>(&mut self) -> Result<Cancel<'_, M>> {
+        let (msg, data, addr) = self.next_from_mailbox().await?;
+        Ok(Cancel::new(msg, data, addr, self))
+    }
+
+    /// Block the current worker to wait for a typed message
+    ///
+    /// This function may return a `Err(FailedLoadData)` if the
+    /// underlying worker was shut down, or `Err(Timeout)` if the call
+    /// was waiting for longer than the `default timeout`.  Use
+    /// [`receive_timeout`](Context::receive_timeout) to adjust the
+    /// timeout period.
+    ///
+    /// Will return `None` if the corresponding worker has been
+    /// stopped, or the underlying Node has shut down.
+    pub async fn receive<M: Message>(&mut self) -> Result<Cancel<'_, M>> {
+        self.receive_timeout(DEFAULT_TIMEOUT).await
+    }
+
+    /// Wait to receive a message up to a specified timeout
+    ///
+    /// See [`receive`](Self::receive) for more details.
+    pub async fn receive_duration_timeout<M: Message>(
+        &mut self,
+        timeout_duration: Duration,
+    ) -> Result<Cancel<'_, M>> {
+        let (msg, data, addr) = timeout(timeout_duration, async { self.next_from_mailbox().await })
+            .await
+            .map_err(|e| NodeError::Data.with_elapsed(e))??;
+        Ok(Cancel::new(msg, data, addr, self))
+    }
+
+    /// Wait to receive a message up to a specified timeout
+    ///
+    /// See [`receive`](Self::receive) for more details.
+    pub async fn receive_timeout<M: Message>(
+        &mut self,
+        timeout_secs: u64,
+    ) -> Result<Cancel<'_, M>> {
+        self.receive_duration_timeout(Duration::from_secs(timeout_secs))
+            .await
+    }
+
+    /// Block the current worker to wait for a message satisfying a conditional
+    ///
+    /// Will return `Err` if the corresponding worker has been
+    /// stopped, or the underlying node has shut down.  This operation
+    /// has a [default timeout](DEFAULT_TIMEOUT).
+    ///
+    /// Internally this function uses [`receive`](Self::receive), so
+    /// is subject to the same timeout.
+    pub async fn receive_match<M, F>(&mut self, check: F) -> Result<Cancel<'_, M>>
+    where
+        M: Message,
+        F: Fn(&M) -> bool,
+    {
+        let (m, data, addr) = timeout(Duration::from_secs(DEFAULT_TIMEOUT), async {
+            loop {
+                match self.next_from_mailbox().await {
+                    Ok((m, data, addr)) if check(&m) => break Ok((m, data, addr)),
+                    Ok((_, data, _)) => {
+                        // Requeue
+                        self.forward(data).await?;
+                    }
+                    e => break e,
+                }
+            }
+        })
+        .await
+        .map_err(|e| NodeError::Data.with_elapsed(e))??;
+
+        Ok(Cancel::new(m, data, addr, self))
+    }
+}
---
+++ implementations/rust/ockam/ockam_node/src/context/worker_lifecycle.rs
@@ -0,0 +1,133 @@
+use crate::{Context, NodeError, NodeMessage, NodeReason};
+use crate::{ProcessorBuilder, WorkerBuilder};
+use ockam_core::compat::sync::Arc;
+use ockam_core::{
+    Address, IncomingAccessControl, Mailboxes, Message, OutgoingAccessControl, Processor, Result,
+    Worker,
+};
+
+enum AddressType {
+    Worker,
+    Processor,
+}
+
+impl AddressType {
+    fn str(&self) -> &'static str {
+        match self {
+            AddressType::Worker => "worker",
+            AddressType::Processor => "processor",
+        }
+    }
+}
+
+impl Context {
+    /// Start a new worker instance at the given address
+    ///
+    /// A worker is an asynchronous piece of code that can send and
+    /// receive messages of a specific type.  This type is encoded via
+    /// the [`Worker`](ockam_core::Worker) trait.  If your code relies
+    /// on a manual run-loop you may want to use
+    /// [`start_processor()`](Self::start_processor) instead!
+    ///
+    /// Each address in the set must be unique and unused on the
+    /// current node.  Workers must implement the Worker trait and be
+    /// thread-safe.  Workers run asynchronously and will be scheduled
+    /// independently of each other.  To wait for the initialisation
+    /// of your worker to complete you can use
+    /// [`wait_for()`](Self::wait_for).
+    ///
+    /// ```rust
+    /// use ockam_core::{AllowAll, Result, Worker, worker};
+    /// use ockam_node::Context;
+    ///
+    /// struct MyWorker;
+    ///
+    /// #[worker]
+    /// impl Worker for MyWorker {
+    ///     type Context = Context;
+    ///     type Message = String;
+    /// }
+    ///
+    /// async fn start_my_worker(ctx: &mut Context) -> Result<()> {
+    ///     ctx.start_worker("my-worker-address", MyWorker, AllowAll, AllowAll).await
+    /// }
+    /// ```
+    pub async fn start_worker<NM, NW>(
+        &self,
+        address: impl Into<Address>,
+        worker: NW,
+        incoming: impl IncomingAccessControl,
+        outgoing: impl OutgoingAccessControl,
+    ) -> Result<()>
+    where
+        NM: Message + Send + 'static,
+        NW: Worker<Context = Context, Message = NM>,
+    {
+        WorkerBuilder::with_mailboxes(
+            Mailboxes::main(address, Arc::new(incoming), Arc::new(outgoing)),
+            worker,
+        )
+        .start(self)
+        .await?;
+
+        Ok(())
+    }
+
+    /// Start a new processor instance at the given address
+    ///
+    /// A processor is an asynchronous piece of code that runs a
+    /// custom run loop, with access to a worker context to send and
+    /// receive messages.  If your code is built around responding to
+    /// message events, consider using
+    /// [`start_worker()`](Self::start_worker) instead!
+    ///
+    pub async fn start_processor<P>(
+        &self,
+        address: impl Into<Address>,
+        processor: P,
+        incoming: impl IncomingAccessControl,
+        outgoing: impl OutgoingAccessControl,
+    ) -> Result<()>
+    where
+        P: Processor<Context = Context>,
+    {
+        ProcessorBuilder::with_mailboxes(
+            Mailboxes::main(address.into(), Arc::new(incoming), Arc::new(outgoing)),
+            processor,
+        )
+        .start(self)
+        .await?;
+
+        Ok(())
+    }
+
+    /// Shut down a local worker by its primary address
+    pub async fn stop_worker<A: Into<Address>>(&self, addr: A) -> Result<()> {
+        self.stop_address(addr.into(), AddressType::Worker).await
+    }
+
+    /// Shut down a local processor by its address
+    pub async fn stop_processor<A: Into<Address>>(&self, addr: A) -> Result<()> {
+        self.stop_address(addr.into(), AddressType::Processor).await
+    }
+
+    async fn stop_address(&self, addr: Address, t: AddressType) -> Result<()> {
+        debug!("Shutting down {} {}", t.str(), addr);
+
+        // Send the stop request
+        let (req, mut rx) = match t {
+            AddressType::Worker => NodeMessage::stop_worker(addr, false),
+            AddressType::Processor => NodeMessage::stop_processor(addr),
+        };
+        self.sender
+            .send(req)
+            .await
+            .map_err(NodeError::from_send_err)?;
+
+        // Then check that address was properly shut down
+        rx.recv()
+            .await
+            .ok_or_else(|| NodeError::NodeState(NodeReason::Unknown).internal())??;
+        Ok(())
+    }
+}
