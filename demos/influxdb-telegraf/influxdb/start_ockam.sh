6f6e77f3407ad8bc4532489f4a11c470382e69ec
Mrinal Wadhwa <mrinal@ockam.io>
2023-03-21 00:00:37 -0700

    docs: fix influxdb demo
    1. download using the install script pinned to 0.82.0
    2. use project authenticate instead of the --enrollment-token option

--- demos/influxdb-telegraf/influxdb/start_ockam.sh
+++ demos/influxdb-telegraf/influxdb/start_ockam.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 NODE="influxdb"
-ockam node create $NODE --project /config/project.json --enrollment-token $OCKAM_TOKEN
+ockam identity create influxdb
+ockam project authenticate --identity influxdb --token $OCKAM_TOKEN --project-path /config/project.json
+ockam node create $NODE --project /config/project.json --identity influxdb
 ockam policy set --at $NODE --resource tcp-outlet --expression '(= subject.component "telegraf")'
 ockam tcp-outlet create --at /node/$NODE --from /service/outlet --to 127.0.0.1:8086
 ockam forwarder create $NODE --at /project/$OCKAM_PROJECT_NAME --to /node/$NODE
--- demos/influxdb-telegraf/telegraf/start_ockam.sh
+++ demos/influxdb-telegraf/telegraf/start_ockam.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 NODE="telegraf"
-ockam node create $NODE --project /config/project.json --enrollment-token $OCKAM_TOKEN
+ockam identity create telegraf
+ockam project authenticate --identity telegraf --token $OCKAM_TOKEN --project-path /config/project.json
+ockam node create $NODE --project /config/project.json --identity telegraf
 ockam policy set --at $NODE --resource tcp-inlet --expression '(= subject.component "influxdb")'
 sleep 30
 ockam tcp-inlet create --at /node/$NODE --from 127.0.0.1:8086 --to /project/$OCKAM_PROJECT_NAME/service/forward_to_influxdb/secure/api/service/outlet
