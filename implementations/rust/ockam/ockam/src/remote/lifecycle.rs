a2ed01449f93c11d257f4b2b5560948061f5f318
Oleksandr Deundiak <olek@ockam.io>
2023-03-22 18:21:14 +0100

    chore(rust): fix doc-commentc

--- implementations/rust/ockam/ockam/src/remote/lifecycle.rs
+++ implementations/rust/ockam/ockam/src/remote/lifecycle.rs
@@ -81,7 +81,7 @@ impl RemoteForwarder {
         }
     }
 
-    /// Create and start static RemoteForwarder at predefined address with given Ockam Hub route
+    /// Create and start static RemoteForwarder at predefined address with given Ockam Orchestrator route
     pub async fn create_static(
         ctx: &Context,
         hub_route: impl Into<Route>,
@@ -176,9 +176,9 @@ impl RemoteForwarder {
     }
 
     /// Create and start new static RemoteForwarder without heart beats
-    // This is a temporary kind of RemoteForwarder that will only run on
-    // rust nodes (hence the `forwarding_service` addr to create static forwarders).
-    // We will use it while we don't have heartbeats implemented on rust nodes.
+    /// This is a temporary kind of RemoteForwarder that will only run on
+    /// rust nodes (hence the `forwarding_service` addr to create static forwarders).
+    /// We will use it while we don't have heartbeats implemented on rust nodes.
     pub async fn create_static_without_heartbeats(
         ctx: &Context,
         hub_route: impl Into<Route>,
