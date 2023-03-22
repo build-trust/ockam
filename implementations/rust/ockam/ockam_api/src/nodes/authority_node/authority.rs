9ce7c23780bb13e8344fbc7404b7ec5ea110ebf5
etorreborre <etorreborre@yahoo.com>
2023-03-22 16:16:48 +0100

    feat(rust): remove the legacy storage migration code
    it is not required anymore and was causing issues when restarting pods

--- implementations/rust/ockam/ockam_api/src/nodes/authority_node/authority.rs
+++ implementations/rust/ockam/ockam_api/src/nodes/authority_node/authority.rs
@@ -134,7 +134,6 @@ impl Authority {
         let direct = crate::authenticator::direct::DirectAuthenticator::new(
             configuration.clone().project_identifier(),
             self.attributes_storage().clone(),
-            self.identity.authenticated_storage().clone(),
         )
         .await?;
 
