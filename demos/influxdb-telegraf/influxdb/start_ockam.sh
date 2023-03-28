d7e22780f6a78c52f20167ee1c822ce00fff600f
etorreborre <etorreborre@yahoo.com>
2023-03-28 10:01:15 +0200

    feat(rust): use ockam policy create

--- demos/influxdb-telegraf/influxdb/start_ockam.sh
+++ demos/influxdb-telegraf/influxdb/start_ockam.sh
@@ -4,6 +4,6 @@ NODE="influxdb"
 ockam identity create influxdb
 ockam project authenticate --identity influxdb --token $OCKAM_TOKEN --project-path /config/project.json
 ockam node create $NODE --project /config/project.json --identity influxdb
-ockam policy set --at $NODE --resource tcp-outlet --expression '(= subject.component "telegraf")'
+ockam policy create --at $NODE --resource tcp-outlet --expression '(= subject.component "telegraf")'
 ockam tcp-outlet create --at /node/$NODE --from /service/outlet --to 127.0.0.1:8086
 ockam forwarder create $NODE --at /project/$OCKAM_PROJECT_NAME --to /node/$NODE
