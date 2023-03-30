d4053944c86fe4264532a942f47b0c71fd9e0a6d
Oleksandr Deundiak <olek@ockam.io>
2023-03-30 17:31:09 +0200

    feat(rust): add constructor for `SessionId`

--- implementations/rust/ockam/ockam_core/src/sessions/session_id.rs
+++ implementations/rust/ockam/ockam_core/src/sessions/session_id.rs
@@ -1,11 +1,24 @@
 use crate::compat::rand::distributions::{Distribution, Standard};
 use crate::compat::rand::Rng;
-use crate::compat::string::String;
+use crate::compat::string::{String, ToString};
 use serde::{Deserialize, Serialize};
 
 /// Unique random identifier of a session
 #[derive(Clone, Eq, PartialEq, Debug, Ord, PartialOrd, Serialize, Deserialize)]
 pub struct SessionId(String);
+
+impl SessionId {
+    /// Constructor
+    pub fn new(str: &str) -> Self {
+        Self(str.to_string())
+    }
+}
+
+impl ToString for SessionId {
+    fn to_string(&self) -> String {
+        self.0.clone()
+    }
+}
 
 impl Distribution<SessionId> for Standard {
     fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> SessionId {
