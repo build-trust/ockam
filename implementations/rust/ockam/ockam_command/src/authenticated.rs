0f30caa48307c942edf8c6cc3332224604766025
Oleksandr Deundiak <olek@ockam.io>
2023-04-07 12:55:39 +0200

    feat(rust): fix `authenticated` command & `Sessions`

--- implementations/rust/ockam/ockam_command/src/authenticated.rs
+++ implementations/rust/ockam/ockam_command/src/authenticated.rs
@@ -8,6 +8,7 @@ use ockam::compat::collections::HashMap;
 use ockam::{Context, TcpTransport};
 use ockam_api::auth;
 use ockam_api::is_local_node;
+use ockam_core::sessions::Sessions;
 use ockam_identity::authenticated_storage::AttributesEntry;
 use ockam_identity::IdentityIdentifier;
 use ockam_multiaddr::MultiAddr;
@@ -123,9 +124,12 @@ fn print_entries(entries: &[(IdentityIdentifier, AttributesEntry)]) {
 }
 
 async fn client(ctx: &Context, tcp: &TcpTransport, addr: &MultiAddr) -> Result<auth::Client> {
-    let to = ockam_api::multiaddr_to_route(addr, tcp)
+    let sessions = Sessions::default();
+    let to = ockam_api::multiaddr_to_route(addr, tcp, &sessions)
         .await
         .ok_or_else(|| anyhow!("failed to parse address: {addr}"))?;
-    let cl = auth::Client::new(to, ctx).await?;
+    let cl = auth::Client::new(to.route, ctx)
+        .await?
+        .with_session(&sessions);
     Ok(cl)
 }
