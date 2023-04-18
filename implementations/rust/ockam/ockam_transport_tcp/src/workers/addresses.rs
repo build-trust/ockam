31c2291613deb1db03dc8f57f98e0f655a0f451c
Oleksandr Deundiak <olek@ockam.io>
2023-04-18 20:16:48 +0200

    fix(rust): fix `TCP` access controls

--- implementations/rust/ockam/ockam_transport_tcp/src/workers/addresses.rs
+++ implementations/rust/ockam/ockam_transport_tcp/src/workers/addresses.rs
@@ -4,11 +4,13 @@ use ockam_core::Address;
 #[derive(Clone, Debug)]
 pub(crate) struct Addresses {
     /// Sender internal address to receive messages from the Receiver (about the connection drop)
-    sender_internal_addr: Address,
+    sender_internal_address: Address,
     /// Used to receive messages from other workers which are then serialized and sent over the wire
     sender_address: Address,
     /// Receiver Processor Address
     receiver_address: Address,
+    /// Receiver Processor Internal Address (to send messages to the Sender)
+    receiver_internal_address: Address,
 }
 
 impl Addresses {
@@ -16,23 +18,29 @@ impl Addresses {
         let role_str = role.str();
 
         let sender_address = Address::random_tagged(&format!("TcpSendWorker_tx_addr_{}", role_str));
-        let sender_internal_addr =
+        let sender_internal_address =
             Address::random_tagged(&format!("TcpSendWorker_int_addr_{}", role_str));
         let receiver_address = Address::random_tagged(&format!("TcpRecvProcessor_{}", role_str));
+        let receiver_internal_address =
+            Address::random_tagged(&format!("TcpRecvProcessor_int_addr_{}", role_str));
 
         Self {
             sender_address,
-            sender_internal_addr,
+            sender_internal_address,
             receiver_address,
+            receiver_internal_address,
         }
     }
-    pub fn sender_internal_addr(&self) -> &Address {
-        &self.sender_internal_addr
+    pub fn sender_internal_address(&self) -> &Address {
+        &self.sender_internal_address
     }
     pub fn sender_address(&self) -> &Address {
         &self.sender_address
     }
     pub fn receiver_address(&self) -> &Address {
         &self.receiver_address
+    }
+    pub fn receiver_internal_address(&self) -> &Address {
+        &self.receiver_internal_address
     }
 }
