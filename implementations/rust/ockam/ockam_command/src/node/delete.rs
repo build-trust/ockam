bcb578001c29fac9692df3ff1412689ff087a27e
Ajay Chowdhury <ajaychowdhury1029@gmail.com>
2023-05-09 02:25:28 +0530

    ci : ockam space delete fix

--- implementations/rust/ockam/ockam_command/src/node/delete.rs
+++ implementations/rust/ockam/ockam_command/src/node/delete.rs
@@ -1,71 +1,82 @@
-use crate::node::util::{delete_all_nodes, delete_node};
-use crate::node::{default_node_name, node_name_parser};
-use crate::{docs, CommandGlobalOpts};
-use anyhow::anyhow;
 use clap::Args;
 use colorful::Colorful;
-use ockam_api::cli_state::{CliStateError, StateDirTrait};
+use ockam::Context;
+
+use crate::node::util::{delete_embedded_node, start_embedded_node};
+use crate::space::util::config;
+use crate::util::api::{self, CloudOpts};
+use crate::util::{node_rpc, RpcBuilder};
+use crate::{docs, CommandGlobalOpts};
 
 const LONG_ABOUT: &str = include_str!("./static/delete/long_about.txt");
 const AFTER_LONG_HELP: &str = include_str!("./static/delete/after_long_help.txt");
 
-/// Delete nodes
 #[derive(Clone, Debug, Args)]
 #[command(
+    arg_required_else_help = true,
     long_about = docs::about(LONG_ABOUT),
     after_long_help = docs::after_help(AFTER_LONG_HELP)
 )]
 pub struct DeleteCommand {
-    /// Name of the node.
-    #[arg(default_value_t = default_node_name(), value_parser = node_name_parser, group = "nodes")]
-    node_name: String,
+    #[arg(display_order = 1001)]
+    pub name: String,
 
-    /// Terminate all node processes and delete all node configurations
-    #[arg(long, short, group = "nodes")]
-    all: bool,
-
-    /// Terminate node process(es) immediately (uses SIGKILL instead of SIGTERM)
-    #[arg(display_order = 901, long, short)]
-    force: bool,
+    #[command(flatten)]
+    pub cloud_opts: CloudOpts,
 }
 
 impl DeleteCommand {
-    pub fn run(self, opts: CommandGlobalOpts) {
-        if let Err(e) = run_impl(opts, self) {
-            eprintln!("{e}");
-            std::process::exit(e.code());
-        }
+    pub fn run(self, options: CommandGlobalOpts) {
+        node_rpc(rpc, (options, self));
     }
 }
 
-fn run_impl(opts: CommandGlobalOpts, cmd: DeleteCommand) -> crate::Result<()> {
-    if cmd.all {
-        delete_all_nodes(opts, cmd.force)?;
-    } else {
-        let state = &opts.state.nodes;
-        match state.get(&cmd.node_name) {
-            // If it exists, proceed
-            Ok(_) => {
-                delete_node(&opts, &cmd.node_name, cmd.force)?;
-                opts.terminal
-                    .stdout()
-                    .plain(format!(
-                        "{} Node with name '{}' has been deleted.",
-                        "✔︎".light_green(),
-                        &cmd.node_name
-                    ))
-                    .machine(&cmd.node_name)
-                    .json(serde_json::json!({ "node": { "name": &cmd.node_name } }))
-                    .write_line()?;
-            }
-            // Else, return the appropriate error
-            Err(err) => match err {
-                CliStateError::NotFound => {
-                    return Err(anyhow!("Node '{}' not found", &cmd.node_name).into())
-                }
-                _ => return Err(err.into()),
-            },
+async fn rpc(mut ctx: Context, (opts, cmd): (CommandGlobalOpts, DeleteCommand)) -> crate::Result<()> {
+    run_impl(&mut ctx, opts, cmd).await
+}
+
+async fn run_impl(
+    ctx: &mut Context,
+    opts: CommandGlobalOpts,
+    cmd: DeleteCommand,
+) -> crate::Result<()> {
+    let node_name = start_embedded_node(ctx, &opts, None).await?;
+    let controller_route = &cmd.cloud_opts.route();
+
+    let _ = config::remove_space(&opts.config, &cmd.name);
+
+    let id = match config::get_space(ctx, &opts, &cmd.name, &node_name, &cmd.cloud_opts.route()).await {
+        Ok(id) => id,
+        Err(ConfigError::NotFound(_)) => {
+            opts.terminal
+                .stdout()
+                .plain(format!("Space '{}' not found.", &cmd.name))
+                .write_line()?;
+            return Err(Box::new(anyhow::anyhow!("Space not found")));
         }
-    }
+        Err(err) => return Err(Box::new(err.into())),
+    };
+
+    let mut rpc = RpcBuilder::new(ctx, &opts, &node_name).build();
+    rpc.request(api::space::delete(&id, controller_route)).await?;
+    rpc.is_ok()?;
+
+    let _ = config::remove_space(&opts.config, &cmd.name);
+    // TODO: remove projects associated to the space.
+    //  Currently we are not storing that association in the project config file.
+
+    delete_embedded_node(&opts, rpc.node_name()).await;
+
+    opts.terminal
+        .stdout()
+        .plain(format!(
+            "{} Space with name '{}' has been deleted.",
+            "✔︎".light_green(),
+            &cmd.name
+        ))  
+        .machine(&cmd.name)
+        .json(serde_json::json!({ "space": { "name": &cmd.name } }))
+        .write_line()?;
+
     Ok(())
 }
