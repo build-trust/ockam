08272dff56f15b3ca75d406729fe815eadd17e3a
Oleksandr Deundiak <olek@ockam.io>
2023-05-08 18:07:46 +0200

    feat(rust): improve `FlowControlId` struct

--- implementations/rust/ockam/ockam_core/src/flow_control/policy.rs
+++ implementations/rust/ockam/ockam_core/src/flow_control/policy.rs
@@ -1,10 +1,14 @@
+use minicbor::{Decode, Encode};
+
 /// Policy according to which a Consumer wants to receive messages from a Producer or Spawner
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, Debug, Decode, Encode)]
+#[rustfmt::skip]
+#[cbor(index_only)]
 pub enum FlowControlPolicy {
     /// Producer is allowed to send any number of messages to the Consumer
-    ProducerAllowMultiple,
+    #[n(0)] ProducerAllowMultiple,
     /// Spawner is allowed to send only one message to the Consumer
-    SpawnerAllowOnlyOneMessage,
+    #[n(1)] SpawnerAllowOnlyOneMessage,
     /// Spawner is allowed to send any number of messages to the Consumer
-    SpawnerAllowMultipleMessages,
+    #[n(2)] SpawnerAllowMultipleMessages,
 }
