6dc70c1d8f97fcba5451d15572f805d6bdd04bb5
etorreborre <etorreborre@yahoo.com>
2023-05-10 11:12:05 +0200

    refactor(rust): remove some unused code

--- examples/rust/get_started/src/lib.rs
+++ examples/rust/get_started/src/lib.rs
@@ -8,12 +8,10 @@ mod hop;
 pub use forwarder::*;
 pub use hop::*;
 
-mod identities;
 mod logger;
 mod project;
 mod token;
 
-pub use identities::*;
 pub use logger::*;
 pub use project::*;
 pub use token::*;
