51018ab45da6e1ad75f4422e6ea65750f94737e5
Shane Sveller <shane.sveller@ockam.io>
2023-05-11 13:02:06 -0500

    fixup! build: add nix flake-based development environment

--- tools/nix/parts/all.nix
+++ tools/nix/parts/all.nix
@@ -16,7 +16,7 @@
       inputsFrom = with config.devShells; [elixir rust tooling];
 
       inherit (config.devShells.elixir) ASDF_ELIXIR_VERSION ASDF_ERLANG_VERSION;
-      inherit (config.devShells.rust) nativeBuildInputs shellHook OCKAM_DISABLE_UPGRADE_CHECK RUSTFLAGS RUST_SRC_PATH;
+      inherit (config.devShells.rust) nativeBuildInputs shellHook CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER OCKAM_DISABLE_UPGRADE_CHECK RUSTFLAGS RUST_SRC_PATH;
       inherit (config.devShells.tooling) BATS_LIB;
     };
   };
