42a00aed0a9061aa65a2678b7859bf930177779f
Oleksandr Deundiak <olek@ockam.io>
2023-05-14 12:59:08 +0200

    feat(rust): improve relay command ux

--- implementations/rust/ockam/ockam/src/remote/info.rs
+++ implementations/rust/ockam/ockam/src/remote/info.rs
@@ -1,17 +1,33 @@
 use crate::Message;
 use ockam_core::compat::string::String;
+use ockam_core::flow_control::FlowControlId;
 use ockam_core::{Address, Route};
 use serde::{Deserialize, Serialize};
 
 /// Information about a remotely forwarded worker.
 #[derive(Serialize, Deserialize, Clone, PartialEq, Eq, Debug, Message)]
 pub struct RemoteForwarderInfo {
-    pub(super) forwarding_route: Route,
-    pub(super) remote_address: String,
-    pub(super) worker_address: Address,
+    forwarding_route: Route,
+    remote_address: String,
+    worker_address: Address,
+    flow_control_id: Option<FlowControlId>,
 }
 
 impl RemoteForwarderInfo {
+    /// Constructor
+    pub fn new(
+        forwarding_route: Route,
+        remote_address: String,
+        worker_address: Address,
+        flow_control_id: Option<FlowControlId>,
+    ) -> Self {
+        Self {
+            forwarding_route,
+            remote_address,
+            worker_address,
+            flow_control_id,
+        }
+    }
     /// Returns the forwarding route.
     pub fn forwarding_route(&self) -> &Route {
         &self.forwarding_route
@@ -23,5 +39,9 @@ impl RemoteForwarderInfo {
     /// Returns the worker address.
     pub fn worker_address(&self) -> &Address {
         &self.worker_address
+    }
+    /// Corresponding [`FlowControlId`]
+    pub fn flow_control_id(&self) -> &Option<FlowControlId> {
+        &self.flow_control_id
     }
 }
