78085bf6b46587de776e657f22af28c49ebc0b37
etorreborre <etorreborre@yahoo.com>
2023-05-18 16:09:47 +0200

    test(rust): add a feature flag for vault tests

--- implementations/rust/ockam/ockam_vault/src/traits/mod.rs
+++ implementations/rust/ockam/ockam_vault/src/traits/mod.rs
@@ -4,12 +4,17 @@ mod security_module;
 mod signer;
 mod symmetric_vault;
 
-pub use asymmetric_vault::tests::*;
 pub use asymmetric_vault::*;
-pub use secrets_store::tests::*;
 pub use secrets_store::*;
 pub use security_module::*;
-pub use signer::tests::*;
 pub use signer::*;
-pub use symmetric_vault::tests::*;
 pub use symmetric_vault::*;
+
+#[cfg(feature = "vault_tests")]
+pub use asymmetric_vault::tests::*;
+#[cfg(feature = "vault_tests")]
+pub use secrets_store::tests::*;
+#[cfg(feature = "vault_tests")]
+pub use signer::tests::*;
+#[cfg(feature = "vault_tests")]
+pub use symmetric_vault::tests::*;
