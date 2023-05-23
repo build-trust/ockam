0aa601610892e58dec0e254a243fd1902b603e64
Tyler McFadden <neruelin@gmail.com>
2023-05-22 23:40:58 -0400

    feat(rust): doc comment + trim static text
    Added a comment explaining the purpose of the environment command.
    Trimmed unnecessary lines from environment static text.

--- implementations/rust/ockam/ockam_command/src/environment/mod.rs
+++ implementations/rust/ockam/ockam_command/src/environment/mod.rs
@@ -2,6 +2,7 @@ use clap::Args;
 
 const ENVTEXT: &str = include_str!("./static/envtext.txt");
 
+// Outputs information about environment variables used by Ockam
 #[derive(Clone, Debug, Args)]
 pub struct EnvironmentCommand {}
 
