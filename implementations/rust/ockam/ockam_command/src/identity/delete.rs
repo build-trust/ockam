c204be97f12b8d1138c2bb55ffb17164f8c3f424
rjtch <tchuinkoufongue@gmail.com>
2023-06-10 16:13:08 +0200

    complete delete function by enforcing TTY --yes flag

--- implementations/rust/ockam/ockam_command/src/identity/delete.rs
+++ implementations/rust/ockam/ockam_command/src/identity/delete.rs
@@ -1,10 +1,13 @@
-use crate::util::node_rpc;
-use crate::{docs, CommandGlobalOpts};
+use anyhow::anyhow;
 use clap::Args;
-use miette::miette;
+
 use ockam::Context;
-use ockam_api::cli_state::traits::StateDirTrait;
 use ockam_api::cli_state::CliStateError;
+use ockam_api::cli_state::traits::StateDirTrait;
+
+use crate::{CommandGlobalOpts, docs};
+use crate::terminal::ConfirmResult;
+use crate::util::node_rpc;
 
 const LONG_ABOUT: &str = include_str!("./static/delete/long_about.txt");
 const AFTER_LONG_HELP: &str = include_str!("./static/delete/after_long_help.txt");
@@ -19,6 +22,10 @@ after_long_help = docs::after_help(AFTER_LONG_HELP)
 pub struct DeleteCommand {
     /// Name of the identity to be deleted
     name: String,
+
+    /// Confirm the deletion without prompting
+    #[arg(display_order = 901, long, short)]
+    yes: bool,
 }
 
 impl DeleteCommand {
@@ -27,23 +34,47 @@ impl DeleteCommand {
     }
 }
 
-async fn run_impl(
-    _ctx: Context,
-    (opts, cmd): (CommandGlobalOpts, DeleteCommand),
-) -> crate::Result<()> {
+async fn run_impl(_ctx: Context, (opts, cmd): (CommandGlobalOpts, DeleteCommand)) -> crate::Result<()> {
     let state = opts.state;
-    // Check if exists
-    match state.identities.get(&cmd.name) {
-        // If it exists, proceed
-        Ok(identity_state) => {
-            state.delete_identity(identity_state)?;
-            println!("Identity '{}' deleted", cmd.name);
-            Ok(())
+    // Check if --yes is provides
+    if cmd.yes {
+        // check if exists
+        match state.identities.get(&cmd.name) {
+            Ok(identity_state) => {
+                state.delete_identity(identity_state)?;
+                println!("Identity '{}' deleted", cmd.name);
+                Ok(())
+            }
+            // Return the appropriate error
+            Err(err) => match err {
+                CliStateError::NotFound => Err(anyhow!("Identity '{}' not found", &cmd.name).into()),
+                _ => Err(err.into()),
+            },
         }
-        // Return the appropriate error
-        Err(err) => match err {
-            CliStateError::NotFound => Err(miette!("Identity '{}' not found", &cmd.name).into()),
-            _ => Err(err.into()),
-        },
+    } else {
+        // Is yes is not provided provide it using TTY
+        match state.identities.get(&cmd.name) {
+            // If it exists, proceed
+            Ok(identity_state) => {
+                match opts.terminal.confirm("This will delete the selected Identity. Are you sure?")? {
+                    ConfirmResult::Yes => {}
+                    ConfirmResult::No => {
+                        println!("Use --yes to confirm");
+                        return Ok(());
+                    }
+                    ConfirmResult::NonTTY => {
+                        return Err(anyhow!("Use --yes to confirm").into());
+                    }
+                }
+                state.delete_identity(identity_state)?;
+                println!("Identity '{}' deleted", cmd.name);
+                Ok(())
+            }
+            // Return the appropriate error
+            Err(err) => match err {
+                CliStateError::NotFound => Err(anyhow!("Identity '{}' not found", &cmd.name).into()),
+                _ => Err(err.into()),
+            },
+        }
     }
 }
--- implementations/rust/ockam/ockam_command/src/node/delete.rs
+++ implementations/rust/ockam/ockam_command/src/node/delete.rs
@@ -1,10 +1,13 @@
-use crate::node::util::{delete_all_nodes, delete_node};
-use crate::node::{get_node_name, initialize_node_if_default};
-use crate::{docs, CommandGlobalOpts};
+use anyhow::anyhow;
 use clap::Args;
 use colorful::Colorful;
 use miette::ErrReport as Report;
+
 use ockam_api::cli_state::{CliStateError, StateDirTrait};
+
+use crate::{CommandGlobalOpts, docs};
+use crate::node::{get_node_name, initialize_node_if_default};
+use crate::node::util::{delete_all_nodes, delete_node};
 
 const LONG_ABOUT: &str = include_str!("./static/delete/long_about.txt");
 const AFTER_LONG_HELP: &str = include_str!("./static/delete/after_long_help.txt");
@@ -12,8 +15,8 @@ const AFTER_LONG_HELP: &str = include_str!("./static/delete/after_long_help.txt"
 /// Delete nodes
 #[derive(Clone, Debug, Args)]
 #[command(
-    long_about = docs::about(LONG_ABOUT),
-    after_long_help = docs::after_help(AFTER_LONG_HELP)
+long_about = docs::about(LONG_ABOUT),
+after_long_help = docs::after_help(AFTER_LONG_HELP)
 )]
 pub struct DeleteCommand {
     /// Name of the node.
@@ -27,6 +30,10 @@ pub struct DeleteCommand {
     /// Terminate node process(es) immediately (uses SIGKILL instead of SIGTERM)
     #[arg(display_order = 901, long, short)]
     force: bool,
+
+    /// Confirm the deletion without prompting
+    #[arg(display_order = 901, long, short)]
+    yes: bool,
 }
 
 impl DeleteCommand {
@@ -44,42 +51,56 @@ impl DeleteCommand {
 }
 
 fn run_impl(opts: CommandGlobalOpts, cmd: DeleteCommand) -> crate::Result<()> {
+    let state = &opts.state.nodes;
+    let node_name = get_node_name(&opts.state, &cmd.node_name);
     if cmd.all {
         delete_all_nodes(opts, cmd.force)?;
     } else {
-        let state = &opts.state.nodes;
-        let node_name = get_node_name(&opts.state, &cmd.node_name);
-        match state.get(&node_name) {
-            // If it exists, proceed
-            Ok(_) => {
-                delete_node(&opts, &node_name, cmd.force)?;
-                opts.terminal
-                    .stdout()
-                    .plain(format!(
-                        "{} Node with name '{}' has been deleted.",
-                        "✔︎".light_green(),
-                        &node_name
-                    ))
-                    .machine(&node_name)
-                    .json(serde_json::json!({ "node": { "name": &node_name } }))
-                    .write_line()?;
+        if cmd.yes {
+            match state.get(&node_name) {
+                // If it exists, proceed
+                Ok(_) => {
+                    delete_node(&opts, &node_name, cmd.force)?;
+                    opts.terminal
+                        .stdout()
+                        .plain(format!(
+                            "{} Node with name '{}' has been deleted.",
+                            "✔︎".light_green(),
+                            &node_name
+                        ))
+                        .machine(&node_name)
+                        .json(serde_json::json!({ "node": { "name": &node_name } }))
+                        .write_line()?;
+                }
+                // Else, return the appropriate error
+                Err(err) => match err {
+                    CliStateError::NotFound => {
+                        return Err(anyhow!("Node '{}' not found", &node_name).into())
+                    }
+                    _ => return Err(err.into()),
+                },
             }
-            // Else, return the appropriate error
-            Err(err) => match err {
-                CliStateError::NotFound => {
-                    return Err(crate::Error::NotFound {
-                        resource: "Node".to_string(),
-                        resource_name: node_name,
-                    })
-                }
-                e => {
-                    return Err(crate::Error::new_internal_error(
-                        "Unable to delete node:",
-                        &e.to_string(),
-                    ))
-                }
-            },
+        } else {
+
         }
     }
     Ok(())
 }
+
+//helper method to return appropriate error-msg
+/*fn return_appropriate_error(err: CliStateError, node_name: String) {
+    match err {
+        CliStateError::NotFound => {
+            Err(crate::Error::NotFound {
+                resource: "Node".to_string(),
+                resource_name: node_name,
+            });
+        }
+        e => {
+            Err(crate::Error::new_internal_error(
+                "Unable to delete node:",
+                &e.to_string(),
+            ));
+        }
+    }
+}*/
