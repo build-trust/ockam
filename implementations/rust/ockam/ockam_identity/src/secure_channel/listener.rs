1927644f17a8e4b0974c2a8dab4a8c04d9872de7
etorreborre <etorreborre@yahoo.com>
2023-06-13 20:10:00 +0200

    refactor(rust): fix a no_std issue

--- implementations/rust/ockam/ockam_identity/src/secure_channel/listener.rs
+++ implementations/rust/ockam/ockam_identity/src/secure_channel/listener.rs
@@ -5,6 +5,7 @@ use crate::secure_channel::role::Role;
 use crate::secure_channels::secure_channels::SecureChannels;
 use crate::{Credential, IdentityIdentifier};
 use ockam_core::compat::boxed::Box;
+use ockam_core::compat::fmt::Vec;
 use ockam_core::compat::sync::Arc;
 use ockam_core::{Address, Any, Result, Routed, Worker};
 use ockam_node::Context;
