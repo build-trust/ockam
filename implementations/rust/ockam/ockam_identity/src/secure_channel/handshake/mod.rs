440dc6abbc3838b6d7c59be429c1601c8cdd2a22
etorreborre <etorreborre@yahoo.com>
2023-06-14 10:04:38 +0200

    refactor(rust): make sure that we only compile the handshake code on a little endian system

--- implementations/rust/ockam/ockam_identity/src/secure_channel/handshake/mod.rs
+++ implementations/rust/ockam/ockam_identity/src/secure_channel/handshake/mod.rs
@@ -1,5 +1,8 @@
 mod error;
 
+/// This directive makes sure that we only run the handshake protocol if it has been compiled
+/// on a little endian system since it is not supporting a big endian one at the moment
+#[cfg(target_endian = "little")]
 #[allow(clippy::module_inception)]
 mod handshake;
 mod handshake_state_machine;
