f6771907465612f563abc71b86e55609cd3e221b
Michał Szpakowski <michal.szpakowski@ockam.io>
2023-06-16 11:54:15 +0200

    feat(elixir): add more healthcheck target metric labels

--- implementations/elixir/ockam/ockam_healthcheck/lib/healthcheck/metrics.ex
+++ implementations/elixir/ockam/ockam_healthcheck/lib/healthcheck/metrics.ex
@@ -11,7 +11,15 @@ defmodule Ockam.Healthcheck.Metrics do
       Metrics.last_value("ockam.healthcheck.result.status",
         event_name: [:ockam, :healthcheck, :result],
         measurement: :status,
-        tags: [:target_name, :target_host, :target_port, :api_worker, :healthcheck_worker],
+        tags: [
+          :target_name,
+          :target_host,
+          :target_port,
+          :target_method,
+          :target_path,
+          :api_worker,
+          :healthcheck_worker
+        ],
         tag_values: &expand_target/1
       ),
       Metrics.distribution("ockam.healthcheck.ok.duration",
@@ -19,7 +27,15 @@ defmodule Ockam.Healthcheck.Metrics do
         measurement: :duration,
         unit: {:native, :millisecond},
         reporter_options: [buckets: [1, 10, 50, 100, 1000]],
-        tags: [:target_name, :target_host, :target_port, :api_worker, :healthcheck_worker],
+        tags: [
+          :target_name,
+          :target_host,
+          :target_port,
+          :target_method,
+          :target_path,
+          :api_worker,
+          :healthcheck_worker
+        ],
         tag_values: &expand_target/1
       ),
       Metrics.distribution("ockam.healthcheck.error.duration",
@@ -31,6 +47,8 @@ defmodule Ockam.Healthcheck.Metrics do
           :target_name,
           :target_host,
           :target_port,
+          :target_method,
+          :target_path,
           :api_worker,
           :healthcheck_worker,
           :reason
@@ -60,6 +78,8 @@ defmodule Ockam.Healthcheck.Metrics do
       target_name: Map.get(target, :name, ""),
       target_host: Map.get(target, :host, ""),
       target_port: Map.get(target, :port, 0),
+      target_method: Map.get(target, :method, ""),
+      target_path: Map.get(target, :path, ""),
       api_worker: Map.get(target, :api_worker, ""),
       healthcheck_worker: Map.get(target, :healthcheck_worker, "")
     })
