ec169b31cb77b36788248933af8bd3673829be80
etorreborre <etorreborre@yahoo.com>
2023-06-16 17:30:08 +0200

    refactor(rust): emit a proper compilation error message if the system is not little endian

--- implementations/rust/ockam/ockam_identity/src/secure_channel/handshake/mod.rs
+++ implementations/rust/ockam/ockam_identity/src/secure_channel/handshake/mod.rs
@@ -1,8 +1,9 @@
 mod error;
 
-/// This directive makes sure that we only run the handshake protocol if it has been compiled
-/// on a little endian system since it is not supporting a big endian one at the moment
-#[cfg(target_endian = "little")]
+// This directive makes sure that we only run the handshake protocol if it has been compiled
+// on a little endian system since it is not supporting a big endian one at the moment
+#[cfg(not(target_endian = "little"))]
+compile_error!("Key Exchange is only supported on little-endian machines");
 #[allow(clippy::module_inception)]
 mod handshake;
 mod handshake_state_machine;
