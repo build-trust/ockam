4e930cbb48863ccba36ee9f7e3442073fede07cc
Oleksandr Deundiak <olek@ockam.io>
2023-06-19 18:55:00 +0200

    test(rust): extend channel test

--- implementations/rust/ockam/ockam_identity/tests/channel.rs
+++ implementations/rust/ockam/ockam_identity/tests/channel.rs
@@ -6,11 +6,10 @@ use ockam_identity::secure_channels::secure_channels;
 use ockam_identity::{
     AuthorityService, CredentialData, DecryptionResponse, EncryptionRequest, EncryptionResponse,
     IdentityAccessControlBuilder, IdentityIdentifier, IdentitySecureChannelLocalInfo,
-    SecureChannelListenerOptions, SecureChannelOptions, TrustContext, TrustEveryonePolicy,
-    TrustIdentifierPolicy,
+    SecureChannelListenerOptions, SecureChannelOptions, SecureChannels, TrustContext,
+    TrustEveryonePolicy, TrustIdentifierPolicy,
 };
 use ockam_node::{Context, MessageReceiveOptions, WorkerBuilder};
-use ockam_vault::KeyId;
 use tokio::time::sleep;
 
 #[ockam_macros::test]
@@ -926,13 +925,27 @@ async fn access_control__no_secure_channel__should_not_pass_messages(
 
 #[ockam_macros::test]
 async fn test_channel_delete_ephemeral_keys(ctx: &mut Context) -> Result<()> {
-    let secure_channels = secure_channels();
-    let identities_creation = secure_channels.identities().identities_creation();
+    let secure_channels_alice = SecureChannels::builder().build();
+    let secure_channels_bob = SecureChannels::builder().build();
 
-    let alice = identities_creation.create_identity().await?;
-    let bob = identities_creation.create_identity().await?;
+    let identities_creation_alice = secure_channels_alice.identities().identities_creation();
+    let identities_creation_bob = secure_channels_bob.identities().identities_creation();
 
-    let bob_listener = secure_channels
+    assert!(secure_channels_alice
+        .vault()
+        .list_ephemeral_secrets()
+        .await?
+        .is_empty());
+    assert!(secure_channels_bob
+        .vault()
+        .list_ephemeral_secrets()
+        .await?
+        .is_empty());
+
+    let alice = identities_creation_alice.create_identity().await?;
+    let bob = identities_creation_bob.create_identity().await?;
+
+    let bob_listener = secure_channels_bob
         .create_secure_channel_listener(
             ctx,
             &bob.identifier(),
@@ -941,7 +954,18 @@ async fn test_channel_delete_ephemeral_keys(ctx: &mut Context) -> Result<()> {
         )
         .await?;
 
-    let alice_channel = secure_channels
+    assert!(secure_channels_alice
+        .vault()
+        .list_ephemeral_secrets()
+        .await?
+        .is_empty());
+    assert!(secure_channels_bob
+        .vault()
+        .list_ephemeral_secrets()
+        .await?
+        .is_empty());
+
+    let alice_channel = secure_channels_alice
         .create_secure_channel(
             ctx,
             &alice.identifier(),
@@ -949,6 +973,26 @@ async fn test_channel_delete_ephemeral_keys(ctx: &mut Context) -> Result<()> {
             SecureChannelOptions::new(),
         )
         .await?;
+
+    ctx.sleep(Duration::from_secs(1)).await;
+
+    // Only k1 and k2 should exist
+    assert_eq!(
+        secure_channels_alice
+            .vault()
+            .list_ephemeral_secrets()
+            .await?
+            .len(),
+        2
+    );
+    assert_eq!(
+        secure_channels_bob
+            .vault()
+            .list_ephemeral_secrets()
+            .await?
+            .len(),
+        2
+    );
 
     let mut child_ctx = ctx
         .new_detached_with_mailboxes(Mailboxes::main(
@@ -971,11 +1015,37 @@ async fn test_channel_delete_ephemeral_keys(ctx: &mut Context) -> Result<()> {
     let msg = child_ctx.receive::<String>().await?;
     assert_eq!("Hello, Bob!", msg.body());
 
+    // Only k1 and k2 should exist
+    assert_eq!(
+        secure_channels_alice
+            .vault()
+            .list_ephemeral_secrets()
+            .await?
+            .len(),
+        2
+    );
+    assert_eq!(
+        secure_channels_bob
+            .vault()
+            .list_ephemeral_secrets()
+            .await?
+            .len(),
+        2
+    );
+
     ctx.stop().await?;
 
     // when the channel is closed all the ephemeral keys must be have been removed from memory
-    let ephemeral_keys = secure_channels.vault().list_ephemeral_secrets().await?;
-    assert_eq!(ephemeral_keys, vec![] as Vec<KeyId>);
+    assert!(secure_channels_alice
+        .vault()
+        .list_ephemeral_secrets()
+        .await?
+        .is_empty());
+    assert!(secure_channels_bob
+        .vault()
+        .list_ephemeral_secrets()
+        .await?
+        .is_empty());
 
     Ok(())
 }
