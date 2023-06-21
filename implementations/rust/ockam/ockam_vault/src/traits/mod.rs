4367b2b6164f55dfad7516818fa959eb49766cf6
Oleksandr Deundiak <olek@ockam.io>
2023-06-21 17:14:54 +0200

    fix(rust): fir ambiguous global re-exports

--- implementations/rust/ockam/ockam_vault/src/traits/mod.rs
+++ implementations/rust/ockam/ockam_vault/src/traits/mod.rs
@@ -1,16 +1,18 @@
 #[cfg(test)]
 pub use asymmetric_vault::tests::*;
-pub use asymmetric_vault::*;
+pub use asymmetric_vault::AsymmetricVault;
 #[cfg(test)]
 pub use secrets_store::tests::*;
-pub use secrets_store::*;
+pub use secrets_store::{
+    EphemeralSecretsStore, PersistentSecretsStore, SecretsStore, SecretsStoreReader,
+};
 pub use security_module::*;
 #[cfg(test)]
 pub use signer::tests::*;
-pub use signer::*;
+pub use signer::Signer;
 #[cfg(test)]
 pub use symmetric_vault::tests::*;
-pub use symmetric_vault::*;
+pub use symmetric_vault::SymmetricVault;
 
 mod asymmetric_vault;
 pub(crate) mod secrets_store;
