8cf0649bfadee53f6f321c4009c571362d07e0a9
Shane Sveller <shane.sveller@ockam.io>
2023-07-04 09:58:58 -0500

    fix: honor typescript version setting, pin pnpm package correctly

--- tools/nix/parts/typescript.nix
+++ tools/nix/parts/typescript.nix
@@ -1,6 +1,5 @@
 {
   config,
-  inputs,
   lib,
   ...
 }: let
@@ -12,10 +11,6 @@ in {
       nodeVersion = mkOption {
         type = types.str;
         default = "18_x";
-      };
-      pnpmVersion = mkOption {
-        type = types.str;
-        default = "6.7.6";
       };
     };
   };
@@ -30,11 +25,30 @@ in {
     }: {
       devShells = {
         typescript = pkgs.mkShell {
-          buildInputs = [
-            pkgs.nodejs
-            (pkgs.nodePackages.pnpm.override {version = cfg.pnpmVersion;})
-          ];
+          buildInputs = with config.packages; [nodejs pnpm];
         };
+      };
+
+      packages = {
+        nodejs =
+          if pkgs ? "nodejs-${cfg.nodeVersion}"
+          then pkgs."nodejs-${cfg.nodeVersion}"
+          else throw "unsupported nodejs version for nixpkgs: ${cfg.nodeVersion}";
+        # TODO: Latest is 8.6.6 as of 2023-07-02
+        pnpm = let
+          pname = "pnpm";
+          # NOTE: Bumping version must always be accompanied by changing the sha512 hash
+          version = "6.7.6";
+          # https://raw.githubusercontent.com/nixos/nixpkgs/nixpkgs-unstable/pkgs/development/node-packages/node-packages.nix
+          src = pkgs.fetchurl {
+            url = "https://registry.npmjs.org/pnpm/-/${pname}-${version}.tgz";
+            sha512 = "sha512-VhO6zVIuhVkKXP3kWMZs9W5b3rhcztq524WoAc9OEwjmj7SiKyp0UNltaaLR0VRjFGJPuQOcqDbNkWwzao6dUw==";
+          };
+        in
+          pkgs.nodePackages.pnpm.override {
+            inherit src version;
+            node = config.packages.nodejs;
+          };
       };
     };
   };
