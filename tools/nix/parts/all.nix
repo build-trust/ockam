355094f2555569d3228537b6007c9acc8f43e57a
Shane Sveller <shane.sveller@ockam.io>
2023-07-04 09:00:28 -0500

    refactor: calculate dynamic linker fallback path directly using derivation path

--- tools/nix/parts/all.nix
+++ tools/nix/parts/all.nix
@@ -17,12 +17,11 @@
       inputsFrom = with config.devShells; [elixir rust tooling typescript];
 
       inherit (config.devShells.elixir) ASDF_ELIXIR_VERSION ASDF_ERLANG_VERSION;
-      inherit (config.devShells.rust) nativeBuildInputs CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER OCKAM_DISABLE_UPGRADE_CHECK RUSTFLAGS RUST_SRC_PATH;
+      inherit (config.devShells.rust) nativeBuildInputs CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER DYLD_FALLBACK_LIBRARY_PATH OCKAM_DISABLE_UPGRADE_CHECK RUSTFLAGS RUST_SRC_PATH;
       inherit (config.devShells.tooling) BATS_LIB;
 
       shellHook = ''
         ${config.devShells.rust.shellHook or ""}
-        export DYLD_FALLBACK_LIBRARY_PATH=$(rustc --print sysroot)/lib
 
         export HOME=$PWD/.home
         export PATH=$PWD/.home/.cargo/bin:$PATH
