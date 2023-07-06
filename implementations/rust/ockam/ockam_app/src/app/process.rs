812017c7ec8de85aa1886cdd254fccea29592f15
etorreborre <etorreborre@yahoo.com>
2023-07-06 11:32:46 +0200

    feat(rust): toggle between enroll and reset
    reset is visible if a project already exists
    enroll is visible if there is no default project

--- implementations/rust/ockam/ockam_app/src/app/process.rs
+++ implementations/rust/ockam/ockam_app/src/app/process.rs
@@ -1,14 +1,19 @@
 use tauri::{AppHandle, RunEvent, SystemTrayEvent, Wry};
+use tracing::error;
 
 use crate::{enroll, quit};
 
 /// This is the function dispatching events for the SystemTray
 pub fn process_system_tray_event(app: &AppHandle<Wry>, event: SystemTrayEvent) {
     if let SystemTrayEvent::MenuItemClick { id, .. } = event {
-        match id.as_str() {
-            enroll::ENROLL_MENU_ID => enroll::on_enroll(app).unwrap(),
-            quit::QUIT_MENU_ID => quit::on_quit(app).unwrap(),
-            _ => {}
+        let result = match id.as_str() {
+            enroll::ENROLL_MENU_ID => enroll::on_enroll(app),
+            enroll::RESET_MENU_ID => enroll::on_reset(app),
+            quit::QUIT_MENU_ID => quit::on_quit(app),
+            _ => Ok(()),
+        };
+        if let Err(e) = result {
+            error!("{:?}", e)
         }
     }
 }
--- implementations/rust/ockam/ockam_app/src/enroll/menu_items.rs
+++ implementations/rust/ockam/ockam_app/src/enroll/menu_items.rs
@@ -1,15 +1,40 @@
 use tauri::{AppHandle, CustomMenuItem, Wry};
 
+use ockam_api::cli_state::StateDirTrait;
+use ockam_command::{CommandGlobalOpts, GlobalArgs};
+
 use crate::enroll::enroll_user::enroll_user;
+use crate::enroll::reset::reset;
 
 pub const ENROLL_MENU_ID: &str = "enroll";
+pub const RESET_MENU_ID: &str = "reset";
 
 pub fn menu_items() -> Vec<CustomMenuItem> {
-    vec![CustomMenuItem::new(ENROLL_MENU_ID, "Enroll...").accelerator("cmd+e")]
+    let options = CommandGlobalOpts::new(GlobalArgs::default());
+
+    let enroll_menu_item = CustomMenuItem::new(ENROLL_MENU_ID, "Enroll...").accelerator("cmd+e");
+    let reset_menu_item = CustomMenuItem::new(RESET_MENU_ID, "Reset...").accelerator("cmd+r");
+    if options.state.projects.default().is_ok() {
+        vec![enroll_menu_item.disabled(), reset_menu_item]
+    } else {
+        vec![enroll_menu_item, reset_menu_item.disabled()]
+    }
 }
 
 /// Enroll the user and show that it has been enrolled
 pub fn on_enroll(app: &AppHandle<Wry>) -> tauri::Result<()> {
     enroll_user();
-    app.tray_handle().get_item("enroll").set_title("Enrolled")
+    app.tray_handle()
+        .get_item(ENROLL_MENU_ID)
+        .set_enabled(false)?;
+    app.tray_handle().get_item(RESET_MENU_ID).set_enabled(true)
+}
+
+/// Reset the persistent state
+pub fn on_reset(app: &AppHandle<Wry>) -> tauri::Result<()> {
+    reset();
+    app.tray_handle()
+        .get_item(ENROLL_MENU_ID)
+        .set_enabled(true)?;
+    app.tray_handle().get_item(RESET_MENU_ID).set_enabled(false)
 }
