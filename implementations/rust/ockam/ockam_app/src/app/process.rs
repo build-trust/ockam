741b540a1e11dd587f5cb72acccb4f8bffadac6d
etorreborre <etorreborre@yahoo.com>
2023-07-06 15:41:54 +0200

    refactor(rust): refactor the enroll code

--- implementations/rust/ockam/ockam_app/src/app/process.rs
+++ implementations/rust/ockam/ockam_app/src/app/process.rs
@@ -1,14 +1,15 @@
 use tauri::{AppHandle, RunEvent, SystemTrayEvent, Wry};
 use tracing::error;
 
+use crate::enroll::DefaultBackend;
 use crate::{enroll, quit};
 
 /// This is the function dispatching events for the SystemTray
 pub fn process_system_tray_event(app: &AppHandle<Wry>, event: SystemTrayEvent) {
     if let SystemTrayEvent::MenuItemClick { id, .. } = event {
         let result = match id.as_str() {
-            enroll::ENROLL_MENU_ID => enroll::on_enroll(app),
-            enroll::RESET_MENU_ID => enroll::on_reset(app),
+            enroll::ENROLL_MENU_ID => enroll::on_enroll(DefaultBackend, app),
+            enroll::RESET_MENU_ID => enroll::on_reset(DefaultBackend, app),
             quit::QUIT_MENU_ID => quit::on_quit(app),
             _ => Ok(()),
         };
