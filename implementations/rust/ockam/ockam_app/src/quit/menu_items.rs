12129c79105c316f05a105abfb205feb49387034
etorreborre <etorreborre@yahoo.com>
2023-07-07 12:17:14 +0200

    refactor(rust): move the reset action near quit

--- implementations/rust/ockam/ockam_app/src/quit/menu_items.rs
+++ implementations/rust/ockam/ockam_app/src/quit/menu_items.rs
@@ -2,8 +2,16 @@ use tauri::{AppHandle, CustomMenuItem, Wry};
 
 pub const QUIT_MENU_ID: &str = "quit";
 
-pub fn menu_items() -> Vec<CustomMenuItem> {
-    vec![CustomMenuItem::new("quit".to_string(), "Quit").accelerator("cmd+q")]
+#[derive(Clone)]
+pub struct QuitActions {
+    pub(crate) quit: CustomMenuItem,
+}
+
+impl QuitActions {
+    pub fn new() -> QuitActions {
+        let quit = CustomMenuItem::new("quit".to_string(), "Quit").accelerator("cmd+q");
+        QuitActions { quit }
+    }
 }
 
 /// Quit the application when the user wants to
