8ea5c1c0a0ea63280a7a6d1228d1a9c47e1777e6
Shane Sveller <shane.sveller@ockam.io>
2023-07-13 16:06:09 -0500

    build(typescript): upgrade pnpm to nixpkgs version
    The current lockfile indicates we were already developing with 8.x.

--- tools/nix/parts/typescript.nix
+++ tools/nix/parts/typescript.nix
@@ -34,21 +34,9 @@ in {
           if pkgs ? "nodejs-${cfg.nodeVersion}"
           then pkgs."nodejs-${cfg.nodeVersion}"
           else throw "unsupported nodejs version for nixpkgs: ${cfg.nodeVersion}";
-        # TODO: Latest is 8.6.6 as of 2023-07-02
-        pnpm = let
-          pname = "pnpm";
-          # NOTE: Bumping version must always be accompanied by changing the sha512 hash
-          version = "6.7.6";
-          # https://raw.githubusercontent.com/nixos/nixpkgs/nixpkgs-unstable/pkgs/development/node-packages/node-packages.nix
-          src = pkgs.fetchurl {
-            url = "https://registry.npmjs.org/pnpm/-/${pname}-${version}.tgz";
-            sha512 = "sha512-VhO6zVIuhVkKXP3kWMZs9W5b3rhcztq524WoAc9OEwjmj7SiKyp0UNltaaLR0VRjFGJPuQOcqDbNkWwzao6dUw==";
-          };
-        in
-          pkgs.nodePackages.pnpm.override {
-            inherit src version;
-            node = config.packages.nodejs;
-          };
+        pnpm = pkgs.nodePackages.pnpm.override {
+          node = config.packages.nodejs;
+        };
       };
     };
   };
