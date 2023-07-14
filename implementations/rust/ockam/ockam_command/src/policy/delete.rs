e1b63bee21102e6d2d9979206a963e903a2a8b17
aleksandarskrbic <skrbic.alexa@gmail.com>
2023-07-14 08:06:22 +0200

    feat(rust): use default node when -at argument is none

--- implementations/rust/ockam/ockam_command/src/policy/delete.rs
+++ implementations/rust/ockam/ockam_command/src/policy/delete.rs
@@ -1,10 +1,9 @@
+use crate::node::get_node_name;
 use crate::policy::policy_path;
-
 use crate::util::{node_rpc, parse_node_name, Rpc};
 use crate::{fmt_ok, CommandGlobalOpts};
 use clap::Args;
 use colorful::Colorful;
-
 use ockam::Context;
 use ockam_abac::{Action, Resource};
 use ockam_core::api::Request;
@@ -12,7 +11,7 @@ use ockam_core::api::Request;
 #[derive(Clone, Debug, Args)]
 pub struct DeleteCommand {
     #[arg(long, display_order = 900, id = "NODE_NAME")]
-    at: String,
+    at: Option<String>,
 
     #[arg(short, long)]
     resource: Resource,
@@ -43,21 +42,30 @@ async fn run_impl(
     opts: CommandGlobalOpts,
     cmd: DeleteCommand,
 ) -> miette::Result<()> {
-    let node = parse_node_name(&cmd.at)?;
+    let at = get_node_name(&opts.state, &cmd.at);
+    let node_name = parse_node_name(&at)?;
     if opts
         .terminal
         .confirmed_with_flag_or_prompt(cmd.yes, "Are you sure you want to delete this policy?")?
     {
-        let req = Request::delete(policy_path(&cmd.resource, &cmd.action));
-        let mut rpc = Rpc::background(ctx, &opts, &node)?;
+        let policy_path = policy_path(&cmd.resource, &cmd.action);
+        let req = Request::delete(&policy_path);
+        let mut rpc = Rpc::background(ctx, &opts, &node_name)?;
         rpc.request(req).await?;
         rpc.is_ok()?;
 
         opts.terminal
             .stdout()
-            .plain(fmt_ok!("Policy with name '{}' has been deleted", &cmd.at))
-            .machine(&cmd.at)
-            .json(serde_json::json!({ "policy": { "at": &cmd.at } }))
+            .plain(fmt_ok!(
+                "Policy with path '{}' has been deleted",
+                &policy_path
+            ))
+            .machine(&policy_path)
+            .json(serde_json::json!({ "policy": {
+                "resource": &cmd.resource.to_string(),
+                "action": &cmd.action.to_string(),
+                "at": &node_name}
+            }))
             .write_line()?;
     }
     Ok(())
