f20b642bd7ee0203cce21043a5a821a3b6fde1fc
Shane Sveller <shane.sveller@ockam.io>
2023-07-14 09:02:05 -0500

    chore(typescript): re-style enroll button after tailwind reset

--- implementations/typescript/ockam/ockam_app/src/components/Enroll.svelte
+++ implementations/typescript/ockam/ockam_app/src/components/Enroll.svelte
@@ -8,7 +8,7 @@
   }
 </script>
 
-<div>
-  <button on:click="{enroll}">Enroll...</button>
+<div class="m-2 max-w-full text-center">
+  <button class="bg-secondary-light m-2 outline-natural-light p-2 rounded-lg text-natural-white" on:click="{enroll}">Enroll...</button>
   {#if visible}<p>Enrolled</p>{/if}
 </div>
