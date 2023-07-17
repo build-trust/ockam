e82f278c7b4994ef370316828005e3e6ee005efc
etorreborre <etorreborre@yahoo.com>
2023-07-17 11:03:48 +0200

    feat(rust): use the tauri state to pass the command options around

---
+++ implementations/typescript/ockam/ockam_app/src/components/Reset.svelte
@@ -0,0 +1,13 @@
+<script>
+    import {invoke} from '@tauri-apps/api/tauri'
+
+    let done = false;
+
+    async function reset() {
+        await invoke('reset')
+    }
+</script>
+
+<div>
+    <button on:click="{reset}">Reset...</button>
+</div>
