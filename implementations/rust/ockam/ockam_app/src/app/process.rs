ff09449b87fb7a13593ee5752f2f4c2123024241
etorreborre <etorreborre@yahoo.com>
2023-07-17 10:45:37 +0200

    refactor(rust): remove the backend abstraction
    this was introduced to help with testing but it is premature at the moment

--- implementations/rust/ockam/ockam_app/src/app/process.rs
+++ implementations/rust/ockam/ockam_app/src/app/process.rs
@@ -2,7 +2,6 @@ use tauri::{AppHandle, RunEvent, SystemTrayEvent, Wry};
 use tracing::error;
 
 use crate::ctx::TauriCtx;
-use crate::enroll::DefaultBackend;
 use crate::{enroll, quit, tcp};
 
 /// This is the function dispatching events for the SystemTray
@@ -10,9 +9,9 @@ pub fn process_system_tray_event(app: &AppHandle<Wry>, event: SystemTrayEvent) {
     let ctx = TauriCtx::new(app.clone());
     if let SystemTrayEvent::MenuItemClick { id, .. } = event {
         let result = match id.as_str() {
-            enroll::ENROLL_MENU_ID => enroll::on_enroll(DefaultBackend, ctx),
+            enroll::ENROLL_MENU_ID => enroll::on_enroll(ctx),
             tcp::outlet::TCP_OUTLET_CREATE_MENU_ID => tcp::outlet::on_create(ctx),
-            enroll::RESET_MENU_ID => enroll::on_reset(DefaultBackend, ctx),
+            enroll::RESET_MENU_ID => enroll::on_reset(ctx),
             quit::QUIT_MENU_ID => quit::on_quit(ctx),
             _ => Ok(()),
         };
