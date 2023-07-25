ba39c55ca25a50ca8d1ba6c348d6378c20650e77
Shane Sveller <shane.sveller@ockam.io>
2023-07-25 15:58:19 -0500

    feat(rust): add ockam_api client for creating project/space invitations

--- implementations/rust/ockam/ockam_api/src/cloud/share/create.rs
+++ implementations/rust/ockam/ockam_api/src/cloud/share/create.rs
@@ -3,7 +3,19 @@ use serde::Serialize;
 
 use ockam_identity::IdentityIdentifier;
 
-use super::SentInvitation;
+use super::{RoleInShare, SentInvitation, ShareScope};
+
+#[derive(Clone, Debug, Decode, Encode, Serialize)]
+#[cbor(map)]
+#[rustfmt::skip]
+pub struct CreateInvitation {
+    #[n(1)] pub expires_at: Option<String>,
+    #[n(2)] pub grant_role: RoleInShare,
+    #[n(3)] pub recipient_email: Option<String>,
+    #[n(4)] pub remaining_uses: Option<usize>,
+    #[n(5)] pub scope: ShareScope,
+    #[n(6)] pub target_id: String,
+}
 
 #[derive(Clone, Debug, Decode, Encode, Serialize)]
 #[cbor(map)]
@@ -37,6 +49,48 @@ mod node {
     const API_SERVICE: &str = "users";
 
     impl NodeManager {
+        pub async fn create_invitation(
+            &self,
+            ctx: &Context,
+            req: CreateInvitation,
+            route: &MultiAddr,
+            identity_name: Option<String>,
+        ) -> Result<SentInvitation> {
+            Response::parse_response_body(
+                self.create_invitation_response(
+                    ctx,
+                    CloudRequestWrapper::new(req, route, identity_name),
+                )
+                .await?
+                .as_slice(),
+            )
+        }
+
+        pub(crate) async fn create_invitation_response(
+            &self,
+            ctx: &Context,
+            req_wrapper: CloudRequestWrapper<CreateInvitation>,
+        ) -> Result<Vec<u8>> {
+            let cloud_multiaddr = req_wrapper.multiaddr()?;
+            let req_body = req_wrapper.req;
+
+            let label = "create_invitation";
+            trace!(%req_body.scope, target_id = %req_body.target_id, "creating invitation");
+
+            let req_builder = Request::post("/v0/invites").body(req_body);
+
+            self.request_controller(
+                ctx,
+                label,
+                "create_invitation",
+                &cloud_multiaddr,
+                API_SERVICE,
+                req_builder,
+                None,
+            )
+            .await
+        }
+
         pub async fn create_service_invitation(
             &self,
             ctx: &Context,
@@ -81,6 +135,30 @@ mod node {
     }
 
     impl NodeManagerWorker {
+        pub async fn create_invitation(
+            &self,
+            ctx: &Context,
+            req: CreateInvitation,
+            route: &MultiAddr,
+            identity_name: Option<String>,
+        ) -> Result<SentInvitation> {
+            let node_manager = self.inner().read().await;
+            node_manager
+                .create_invitation(ctx, req, route, identity_name)
+                .await
+        }
+
+        pub(crate) async fn create_invitation_response(
+            &self,
+            ctx: &Context,
+            req_wrapper: CloudRequestWrapper<CreateInvitation>,
+        ) -> Result<Vec<u8>> {
+            let node_manager = self.inner().read().await;
+            node_manager
+                .create_invitation_response(ctx, req_wrapper)
+                .await
+        }
+
         pub async fn create_service_invitation(
             &self,
             ctx: &Context,
