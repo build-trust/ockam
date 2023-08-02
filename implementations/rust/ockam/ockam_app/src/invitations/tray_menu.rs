987437b2f95166dcac67fa758077e00675739e87
Shane Sveller <shane.sveller@ockam.io>
2023-08-02 14:50:21 -0500

    feat(rust): render invitations as submenus with info and verbs

--- implementations/rust/ockam/ockam_app/src/invitations/tray_menu.rs
+++ implementations/rust/ockam/ockam_app/src/invitations/tray_menu.rs
@@ -1,5 +1,7 @@
-use tauri::{AppHandle, CustomMenuItem, Manager, State, SystemTrayMenu};
-use tauri_runtime::menu::SystemTrayMenuItem;
+use tauri::{
+    AppHandle, CustomMenuItem, Manager, State, SystemTrayMenu, SystemTrayMenuItem,
+    SystemTraySubmenu,
+};
 use tracing::debug;
 
 use ockam_api::cloud::share::{InvitationWithAccess, ReceivedInvitation, SentInvitation};
@@ -41,16 +43,27 @@ fn add_pending_menu(tray_menu: SystemTrayMenu, sent: &[SentInvitation]) -> Syste
     } else {
         "Pending Invitations"
     };
-    sent.iter().map(sent_menu_item).fold(
+    sent.iter().map(sent_invitation_menu).fold(
         tray_menu.add_item(
             CustomMenuItem::new(INVITATIONS_PENDING_HEADER_MENU_ID, header_text).disabled(),
         ),
-        |menu, entry| menu.add_item(entry),
+        |menu, submenu| menu.add_submenu(submenu),
     )
 }
 
-fn sent_menu_item(invitation: &SentInvitation) -> CustomMenuItem {
-    CustomMenuItem::new(invitation.id.to_owned(), invitation.id.to_owned())
+fn sent_invitation_menu(invitation: &SentInvitation) -> SystemTraySubmenu {
+    let id = invitation.id.to_owned();
+    let submenu = SystemTrayMenu::new()
+        .add_item(CustomMenuItem::new(id.clone(), id.clone()).disabled())
+        .add_item(CustomMenuItem::new(id.clone(), invitation.recipient_email.to_owned()).disabled())
+        .add_item(
+            CustomMenuItem::new(
+                format!("invitation-sent-cancel-{}", invitation.id),
+                "Cancel",
+            )
+            .disabled(),
+        );
+    SystemTraySubmenu::new(id, submenu)
 }
 
 fn add_received_menu(tray_menu: SystemTrayMenu, received: &[ReceivedInvitation]) -> SystemTrayMenu {
@@ -59,16 +72,48 @@ fn add_received_menu(tray_menu: SystemTrayMenu, received: &[ReceivedInvitation])
     } else {
         "Received Invitations"
     };
-    received.iter().map(received_menu_item).fold(
+    received.iter().map(received_invite_menu).fold(
         tray_menu.add_item(
             CustomMenuItem::new(INVITATIONS_RECEIVED_HEADER_MENU_ID, header_text).disabled(),
         ),
-        |menu, entry| menu.add_item(entry),
+        |menu, submenu| menu.add_submenu(submenu),
     )
 }
 
-fn received_menu_item(invitation: &ReceivedInvitation) -> CustomMenuItem {
-    CustomMenuItem::new(invitation.id.to_owned(), invitation.id.to_owned())
+fn received_invite_menu(invitation: &ReceivedInvitation) -> SystemTraySubmenu {
+    let id = invitation.id.to_owned();
+    let submenu = SystemTrayMenu::new()
+        .add_item(CustomMenuItem::new(id.clone(), id.clone()).disabled())
+        .add_item(
+            CustomMenuItem::new(id.clone(), format!("Sent by: {}", invitation.owner_email))
+                .disabled(),
+        )
+        .add_item(
+            CustomMenuItem::new(
+                id.clone(),
+                format!("Grants role: {}", invitation.grant_role),
+            )
+            .disabled(),
+        )
+        .add_item(
+            CustomMenuItem::new(
+                id.clone(),
+                format!("Target: {} {}", invitation.scope, invitation.target_id),
+            )
+            .disabled(),
+        )
+        .add_item(CustomMenuItem::new(
+            format!("invitation-received-accept-{}", invitation.id),
+            "Accept",
+        ))
+        .add_item(
+            CustomMenuItem::new(
+                format!("invitation-received-decline-{}", invitation.id),
+                "Decline",
+            )
+            .disabled(),
+        );
+    SystemTraySubmenu::new(id, submenu)
 }
 
 fn add_accepted_menu(
@@ -76,21 +121,60 @@ fn add_accepted_menu(
     accepted: &[InvitationWithAccess],
 ) -> SystemTrayMenu {
     let header_text = if accepted.is_empty() {
-        "No accepted Invitations"
+        "No Accepted Invitations"
     } else {
-        "accepted Invitations"
+        "Accepted Invitations"
     };
-    accepted.iter().map(accepted_menu_item).fold(
+    accepted.iter().map(accepted_invite_menu).fold(
         tray_menu.add_item(
             CustomMenuItem::new(INVITATIONS_ACCEPTED_HEADER_MENU_ID, header_text).disabled(),
         ),
-        |menu, entry| menu.add_item(entry),
+        |menu, submenu| menu.add_submenu(submenu),
     )
 }
 
-fn accepted_menu_item(invitation: &InvitationWithAccess) -> CustomMenuItem {
-    CustomMenuItem::new(
-        invitation.invitation.id.to_owned(),
-        invitation.invitation.id.to_owned(),
-    )
+fn accepted_invite_menu(invitation: &InvitationWithAccess) -> SystemTraySubmenu {
+    let id = invitation.invitation.id.to_owned();
+    let submenu = SystemTrayMenu::new()
+        .add_item(CustomMenuItem::new(id.clone(), id.clone()).disabled())
+        .add_item(
+            CustomMenuItem::new(
+                id.clone(),
+                format!("Sent by: {}", invitation.invitation.owner_email),
+            )
+            .disabled(),
+        )
+        .add_item(
+            CustomMenuItem::new(
+                id.clone(),
+                format!("Grants role: {}", invitation.invitation.grant_role),
+            )
+            .disabled(),
+        )
+        .add_item(
+            CustomMenuItem::new(
+                id.clone(),
+                format!(
+                    "Target: {} {}",
+                    invitation.invitation.scope, invitation.invitation.target_id
+                ),
+            )
+            .disabled(),
+        )
+        .add_item(
+            CustomMenuItem::new(
+                format!("invitation-accepted-connect-{}", invitation.invitation.id),
+                "Connect",
+            )
+            .disabled(),
+        )
+        .add_item(
+            CustomMenuItem::new(
+                format!("invitation-accepted-leave-{}", invitation.invitation.id),
+                "Leave",
+            )
+            .disabled(),
+        );
+
+    SystemTraySubmenu::new(id, submenu)
 }
