662bd23486c2abada70e007fa3eac34d11ca08df
Shane Sveller <shane.sveller@ockam.io>
2023-08-07 16:00:47 -0500

    refactor(rust): scope existing clippy exemption based on target platform

--- implementations/rust/ockam/ockam_app/src/platform.rs
+++ implementations/rust/ockam/ockam_app/src/platform.rs
@@ -1,6 +1,6 @@
 use tauri::App;
 
-#[allow(unused_variables)]
+#[cfg_attr(not(target_os = "macos"), allow(unused_variables))]
 pub fn set_platform_activation_policy(app: &mut App) {
     #[cfg(target_os = "macos")]
     app.set_activation_policy(tauri::ActivationPolicy::Accessory);
