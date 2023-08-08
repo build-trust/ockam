f4c7361f57ad6f13950d3b4bf6ef6ef6d3ca70a1
Shane Sveller <shane.sveller@ockam.io>
2023-08-08 10:10:00 -0500

    refactor: remove remaining_uses from invitation cli arguments

--- implementations/rust/ockam/ockam_command/src/share/service.rs
+++ implementations/rust/ockam/ockam_command/src/share/service.rs
@@ -114,11 +114,10 @@ async fn run_impl(
     delete_embedded_node(&opts, rpc.node_name()).await;
 
     let plain = fmt_ok!(
-        "Invitation {} to {} {} created, with {} remaining uses and expiring at {}. {} will be notified via email.",
+        "Invitation {} to {} {} created, expiring at {}. {} will be notified via email.",
         sent.id,
         sent.scope,
         sent.target_id,
-        sent.remaining_uses,
         sent.expires_at,
         sent.recipient_email
     );
