bd95cea787f1ef138717d2db340253069cd2ef2b
Shane Sveller <shane.sveller@ockam.io>
2023-08-08 14:33:34 -0500

    feat(typescript): add form to create service invitations

---
+++ implementations/typescript/ockam/ockam_app/src/routes/invite/[outlet_addr]/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+  import CreateServiceInvitation from "./CreateServiceInvitation.svelte";
+
+  /** @type {import('./$types').PageData} */
+  export let data;
+</script>
+
+<CreateServiceInvitation {...data} />
---
+++ implementations/typescript/ockam/ockam_app/src/routes/invite/[outlet_addr]/+page.ts
@@ -0,0 +1,5 @@
+export const load = ({ params }) => {
+  return {
+    outlet_addr: params.outlet_addr,
+  };
+};
