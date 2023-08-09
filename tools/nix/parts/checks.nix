04f608ff587f8373e17cabf4e1df3bed0d07e763
Shane Sveller <shane.sveller@ockam.io>
2023-08-09 09:13:53 -0500

    build: configure pre-commit hook for commitlint

--- tools/nix/parts/checks.nix
+++ tools/nix/parts/checks.nix
@@ -1,6 +1,11 @@
 {
   config = {
-    perSystem = {config, ...}: {
+    perSystem = {
+      config,
+      lib,
+      pkgs,
+      ...
+    }: {
       devShells.pre-commit = config.pre-commit.devShell;
 
       # https://flake.parts/options/pre-commit-hooks-nix.html
@@ -9,6 +14,16 @@
         settings = {
           hooks = {
             alejandra.enable = true;
+
+            # not implemented upstream in pre-commit-hooks.nix
+            commitlint = {
+              enable = true;
+              entry = "${lib.getExe pkgs.commitlint} --config ${../../commitlint/commitlint.config.js} --edit";
+              name = "commitlint";
+              stages = ["commit-msg"];
+              types = [];
+            };
+
             statix.enable = true;
           };
 
