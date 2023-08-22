b3dd4038a5f5c8dc054091cf7ebf04453414c340
Shane Sveller <shane.sveller@ockam.io>
2023-08-22 09:15:33 -0500

    feat: add developer menu with diagnostic information

--- implementations/rust/ockam/ockam_app/src/options/tray_menu.rs
+++ implementations/rust/ockam/ockam_app/src/options/tray_menu.rs
@@ -1,10 +1,12 @@
 #[cfg(all(debug_assertions, feature = "invitations"))]
 use tauri::Manager;
-use tauri::{AppHandle, CustomMenuItem, SystemTrayMenu, Wry};
+use tauri::{AppHandle, CustomMenuItem, SystemTrayMenu, SystemTraySubmenu, Wry};
 
 use crate::app::AppState;
 use crate::options::reset;
 
+#[cfg(debug_assertions)]
+pub const DEV_MENU_ID: &str = "developer";
 #[cfg(debug_assertions)]
 pub const REFRESH_MENU_ID: &str = "refresh";
 pub const RESET_MENU_ID: &str = "reset";
@@ -16,12 +18,30 @@ pub(crate) async fn build_options_section(
 ) -> SystemTrayMenu {
     let tm = if app_state.is_enrolled().await {
         #[cfg(debug_assertions)]
-        let tray_menu = tray_menu.add_item(CustomMenuItem::new(REFRESH_MENU_ID, "Dev: Refresh"));
+        let tray_menu = build_developer_submenu(app_state, tray_menu);
         tray_menu.add_item(CustomMenuItem::new(RESET_MENU_ID, "Reset").accelerator("cmd+r"))
     } else {
         tray_menu
     };
     tm.add_item(CustomMenuItem::new(QUIT_MENU_ID, "Quit Ockam").accelerator("cmd+q"))
+}
+
+fn build_developer_submenu(app_state: &AppState, tray_menu: SystemTrayMenu) -> SystemTrayMenu {
+    let submenu = SystemTrayMenu::new()
+        .add_item(CustomMenuItem::new(REFRESH_MENU_ID, "Refresh Data"))
+        .add_item(
+            CustomMenuItem::new(
+                DEV_MENU_ID,
+                format!("Controller Address: {}", app_state.controller_address()),
+            )
+            .disabled(),
+        )
+        .add_item(
+            CustomMenuItem::new(DEV_MENU_ID, "Last Successful Poll: Not Implemented").disabled(),
+        )
+        .add_item(CustomMenuItem::new(DEV_MENU_ID, "Last Failed Poll: Not Implemented").disabled());
+
+    tray_menu.add_submenu(SystemTraySubmenu::new("Developer Tools", submenu))
 }
 
 #[cfg(debug_assertions)]
