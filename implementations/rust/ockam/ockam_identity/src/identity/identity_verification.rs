31dcb7b28dbb2ea6a1f7e5387a4267da565351b5
Oleksandr Deundiak <olek@ockam.io>
2023-08-23 22:58:00 +0200

    feat(rust): improve `Identity` verification

--- implementations/rust/ockam/ockam_identity/src/identity/identity_verification.rs
+++ implementations/rust/ockam/ockam_identity/src/identity/identity_verification.rs
@@ -1,30 +1,24 @@
 use super::super::models::{Change, ChangeData, ChangeHash, ChangeSignature, CHANGE_HASH_LEN};
 use super::super::verified_change::VerifiedChange;
 use super::super::{Identity, IdentityError};
+use arrayref::array_ref;
 
 use ockam_core::compat::sync::Arc;
 use ockam_core::compat::vec::Vec;
 use ockam_core::Result;
+use ockam_vault::constants::SHA256_LENGTH;
 use ockam_vault::{PublicKey, SecretType, Signature, VerifyingVault};
 
 struct ChangeDetails {
     version: u8,
     change_hash: ChangeHash,
+    change_full_hash: [u8; SHA256_LENGTH],
     change_data: ChangeData,
 }
 
 impl Identity {
-    pub(crate) async fn compute_change_hash_from_data(
-        data: &[u8],
-        vault: Arc<dyn VerifyingVault>,
-    ) -> Result<ChangeHash> {
-        let hash = vault.sha256(data).await?;
-        Self::compute_change_hash_from_hash(hash)
-    }
-
     pub(crate) fn compute_change_hash_from_hash(hash: [u8; 32]) -> Result<ChangeHash> {
-        let change_hash = hash[0..CHANGE_HASH_LEN].try_into().unwrap(); // FIXME
-        Ok(ChangeHash::new(change_hash))
+        Ok(ChangeHash::new(*array_ref!(hash, 0, CHANGE_HASH_LEN)))
     }
 
     /// Check consistency of changes that are being added
@@ -32,7 +26,8 @@ impl Identity {
         changes: &[Change],
         verifying_vault: Arc<dyn VerifyingVault>,
     ) -> Result<Vec<VerifiedChange>> {
-        let to_be_verified_changes = Self::check_consistency(&[], changes, verifying_vault).await?;
+        let to_be_verified_changes =
+            Self::check_consistency(None, changes, verifying_vault).await?;
 
         Ok(to_be_verified_changes)
     }
@@ -41,7 +36,8 @@ impl Identity {
         change: &Change,
         vault: Arc<dyn VerifyingVault>,
     ) -> Result<ChangeDetails> {
-        let change_hash = Self::compute_change_hash_from_data(&change.data, vault).await?;
+        let change_full_hash = vault.sha256(&change.data).await?;
+        let change_hash = Self::compute_change_hash_from_hash(change_full_hash)?;
         let versioned_data = change.get_versioned_data()?;
 
         if versioned_data.version != 1 {
@@ -53,19 +49,20 @@ impl Identity {
         Ok(ChangeDetails {
             version: versioned_data.version,
             change_hash,
+            change_full_hash,
             change_data,
         })
     }
 
-    /// Check consistency of changes that are been added
+    /// Check consistency of changes that are being added
     async fn check_consistency(
-        existing_changes: &[Change],
+        last_known_change: Option<&Change>,
         new_changes: &[Change],
         vault: Arc<dyn VerifyingVault>,
     ) -> Result<Vec<VerifiedChange>> {
         let mut to_be_verified_changes = Vec::with_capacity(new_changes.len());
 
-        let mut previous_change_details = match existing_changes.last() {
+        let mut previous_change_details = match last_known_change {
             Some(previous_change) => {
                 Some(Self::get_change_details(previous_change, vault.clone()).await?)
             }
@@ -81,14 +78,24 @@ impl Identity {
                     return Err(IdentityError::IdentityVerificationFailed.into());
                 }
 
-                if let Some(previous_change_hash) = &change_details.change_data.previous_change {
-                    if &previous_change_details.change_hash != previous_change_hash {
-                        // Corrupted changes sequence
-                        return Err(IdentityError::IdentityVerificationFailed.into());
-                    }
+                if previous_change_details.change_data.created_at
+                    > change_details.change_data.created_at
+                {
+                    // The older key can't be created after the newer
+                    return Err(IdentityError::IdentityVerificationFailed.into());
+                }
+
+                // This is intentionally allowed:
+                // change_details.change_data.created_at > previous_change_details.change_data.expires_at
+
+                if Some(&previous_change_details.change_hash)
+                    != change_details.change_data.previous_change.as_ref()
+                {
+                    // Corrupted changes sequence
+                    return Err(IdentityError::IdentityVerificationFailed.into());
                 }
             } else if change_details.change_data.previous_change.is_some() {
-                // Should empty
+                // Should be empty
                 return Err(IdentityError::IdentityVerificationFailed.into());
             }
 
@@ -110,15 +117,19 @@ impl Identity {
         changes: &[Change],
         vault: Arc<dyn VerifyingVault>,
     ) -> Result<()> {
-        let len = to_be_verified_changes.len();
-        if len != changes.len() {
+        if to_be_verified_changes.len() != changes.len() {
             return Err(IdentityError::IdentityVerificationFailed.into());
         }
 
-        for i in 0..len {
-            let existing_changes = &to_be_verified_changes[..i];
+        for i in 0..to_be_verified_changes.len() {
+            let last_verified_change = if i == 0 {
+                None
+            } else {
+                Some(&to_be_verified_changes[i - 1])
+            };
+
             let new_change = &changes[i];
-            Self::verify_change(existing_changes, new_change, vault.clone()).await?
+            Self::verify_change_signatures(last_verified_change, new_change, vault.clone()).await?
         }
         Ok(())
     }
@@ -149,26 +160,18 @@ impl Identity {
 
     /// WARNING: This function assumes all existing changes in chain are verified.
     /// WARNING: Correctness of changes sequence is not verified here.
-    async fn verify_change(
-        existing_changes: &[VerifiedChange],
+    async fn verify_change_signatures(
+        last_verified_change: Option<&VerifiedChange>,
         new_change: &Change,
         vault: Arc<dyn VerifyingVault>,
     ) -> Result<()> {
-        let hash = vault.sha256(&new_change.data).await?;
+        let new_change_details = Self::get_change_details(new_change, vault.clone()).await?;
 
-        let versioned_data = new_change.get_versioned_data()?;
-
-        if versioned_data.version != 1 {
-            return Err(IdentityError::UnknownIdentityVersion.into());
-        }
-
-        let change_data = ChangeData::get_data(&versioned_data)?;
-
-        if let Some(last_verified_change) = existing_changes.last() {
+        if let Some(last_verified_change) = last_verified_change {
             if let Some(previous_signature) = &new_change.previous_signature {
                 if !Self::verify_change_signature(
                     last_verified_change.primary_public_key(),
-                    hash,
+                    new_change_details.change_full_hash,
                     previous_signature,
                     vault.clone(),
                 )
@@ -177,13 +180,18 @@ impl Identity {
                     return Err(IdentityError::IdentityVerificationFailed.into());
                 }
             } else {
+                // Previous signature should be present if it's not the first change
                 return Err(IdentityError::IdentityVerificationFailed.into());
             }
         }
 
         if !Self::verify_change_signature(
-            &change_data.primary_public_key.clone().into(),
-            hash,
+            &new_change_details
+                .change_data
+                .primary_public_key
+                .clone()
+                .into(),
+            new_change_details.change_full_hash,
             &new_change.signature,
             vault,
         )
