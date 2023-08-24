bd954adfecd90e69308bacbc4d36e463f70b3cf9
etorreborre <etorreborre@yahoo.com>
2023-08-24 12:09:44 +0200

    chore: fix a pre-rendering error when running cargo tauri build

--- implementations/typescript/ockam/ockam_app/src/routes/invite/[outlet_addr]/+page.ts
+++ implementations/typescript/ockam/ockam_app/src/routes/invite/[outlet_addr]/+page.ts
@@ -1,3 +1,14 @@
+/** @type {import('./$types').EntryGenerator}
+ * Since this page has a dynamic address we need to specify the entries function
+ * so that it can be pre-rendered.
+ * See: https://kit.svelte.dev/docs/page-options#prerender-troubleshooting
+ */
+export function entries() {
+  return [
+    { outlet_addr: '12345' },
+  ];
+}
+
 export const load = ({ params }) => {
   return {
     outlet_addr: params.outlet_addr,
