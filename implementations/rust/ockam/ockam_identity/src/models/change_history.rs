d2d232d5e40b2470355288d15b5a977c0563e0ec
Oleksandr Deundiak <olek@ockam.io>
2023-09-05 19:28:14 +0200

    feat(rust): move models conversions

--- implementations/rust/ockam/ockam_identity/src/models/change_history.rs
+++ implementations/rust/ockam/ockam_identity/src/models/change_history.rs
@@ -2,11 +2,9 @@ use crate::models::{
     ChangeHash, Ed25519PublicKey, Ed25519Signature, P256ECDSAPublicKey, P256ECDSASignature,
     TimestampInSeconds,
 };
-use crate::IdentityError;
 use minicbor::{Decode, Encode};
 use ockam_core::compat::vec::Vec;
 use ockam_core::Result;
-use ockam_vault::{PublicKey, SecretType, Signature};
 
 /// Identity Change History
 #[derive(Clone, Debug, PartialEq, Eq, Encode, Decode)]
@@ -86,71 +84,4 @@ pub enum PrimaryPublicKey {
     #[n(1)] Ed25519PublicKey(#[n(0)] Ed25519PublicKey),
     /// ECDSA P256 Public Key
     #[n(2)] P256ECDSAPublicKey(#[n(0)] P256ECDSAPublicKey),
-}
-
-impl From<PrimaryPublicKey> for PublicKey {
-    fn from(value: PrimaryPublicKey) -> Self {
-        match value {
-            PrimaryPublicKey::Ed25519PublicKey(value) => Self::from(value),
-            PrimaryPublicKey::P256ECDSAPublicKey(value) => Self::from(value),
-        }
-    }
-}
-
-impl TryFrom<PublicKey> for PrimaryPublicKey {
-    type Error = ockam_core::Error;
-
-    fn try_from(value: PublicKey) -> Result<Self> {
-        match value.stype() {
-            SecretType::Ed25519 => Ok(Self::Ed25519PublicKey(Ed25519PublicKey(
-                value
-                    .data()
-                    .try_into()
-                    .map_err(|_| IdentityError::InvalidKeyData)?,
-            ))),
-            SecretType::NistP256 => Ok(Self::P256ECDSAPublicKey(P256ECDSAPublicKey(
-                value
-                    .data()
-                    .try_into()
-                    .map_err(|_| IdentityError::InvalidKeyData)?,
-            ))),
-
-            SecretType::X25519 | SecretType::Buffer | SecretType::Aes => {
-                Err(IdentityError::InvalidKeyType.into())
-            }
-        }
-    }
-}
-
-impl From<ChangeSignature> for Signature {
-    fn from(value: ChangeSignature) -> Self {
-        match value {
-            ChangeSignature::Ed25519Signature(value) => Self::new(value.0.to_vec()),
-            ChangeSignature::P256ECDSASignature(value) => Self::new(value.0.to_vec()),
-        }
-    }
-}
-
-impl ChangeSignature {
-    /// Try to create a [`ChangeSignature`] using a binary [`Signature`] and its type
-    pub fn try_from_signature(signature: Signature, stype: SecretType) -> Result<Self> {
-        match stype {
-            SecretType::Ed25519 => Ok(Self::Ed25519Signature(Ed25519Signature(
-                signature
-                    .as_ref()
-                    .try_into()
-                    .map_err(|_| IdentityError::InvalidSignatureData)?,
-            ))),
-            SecretType::NistP256 => Ok(Self::P256ECDSASignature(P256ECDSASignature(
-                signature
-                    .as_ref()
-                    .try_into()
-                    .map_err(|_| IdentityError::InvalidSignatureData)?,
-            ))),
-
-            SecretType::Buffer | SecretType::Aes | SecretType::X25519 => {
-                Err(IdentityError::InvalidKeyType.into())
-            }
-        }
-    }
 }
--- implementations/rust/ockam/ockam_identity/src/models/public_keys.rs
+++ implementations/rust/ockam/ockam_identity/src/models/public_keys.rs
@@ -1,7 +1,4 @@
-use ockam_core::{Error, Result};
-use ockam_vault::{PublicKey, SecretType};
-
-use crate::IdentityError;
+use ockam_core::Result;
 
 use core::ops::Deref;
 use minicbor::bytes::ByteArray;
@@ -71,71 +68,5 @@ impl<'b, C> Decode<'b, C> for P256ECDSAPublicKey {
         let data = ByteArray::<65>::decode(d, ctx)?;
 
         Ok(Self(*data.deref()))
-    }
-}
-
-impl From<Ed25519PublicKey> for PublicKey {
-    fn from(value: Ed25519PublicKey) -> Self {
-        Self::new(value.0.to_vec(), SecretType::Ed25519)
-    }
-}
-
-impl From<X25519PublicKey> for PublicKey {
-    fn from(value: X25519PublicKey) -> Self {
-        Self::new(value.0.to_vec(), SecretType::X25519)
-    }
-}
-
-impl From<P256ECDSAPublicKey> for PublicKey {
-    fn from(value: P256ECDSAPublicKey) -> Self {
-        Self::new(value.0.to_vec(), SecretType::NistP256)
-    }
-}
-
-impl TryFrom<PublicKey> for Ed25519PublicKey {
-    type Error = Error;
-
-    fn try_from(value: PublicKey) -> Result<Self> {
-        match value.stype() {
-            SecretType::Ed25519 => {
-                let data = value.data()[0..32]
-                    .try_into()
-                    .map_err(|_| IdentityError::InvalidKeyData)?;
-                Ok(Self(data))
-            }
-            _ => Err(IdentityError::InvalidKeyType.into()),
-        }
-    }
-}
-
-impl TryFrom<PublicKey> for X25519PublicKey {
-    type Error = Error;
-
-    fn try_from(value: PublicKey) -> Result<Self> {
-        match value.stype() {
-            SecretType::X25519 => {
-                let data = value.data()[0..32]
-                    .try_into()
-                    .map_err(|_| IdentityError::InvalidKeyData)?;
-                Ok(Self(data))
-            }
-            _ => Err(IdentityError::InvalidKeyType.into()),
-        }
-    }
-}
-
-impl TryFrom<PublicKey> for P256ECDSAPublicKey {
-    type Error = Error;
-
-    fn try_from(value: PublicKey) -> Result<Self> {
-        match value.stype() {
-            SecretType::NistP256 => {
-                let data = value.data()[0..64]
-                    .try_into()
-                    .map_err(|_| IdentityError::InvalidKeyData)?;
-                Ok(Self(data))
-            }
-            _ => Err(IdentityError::InvalidKeyType.into()),
-        }
     }
 }
