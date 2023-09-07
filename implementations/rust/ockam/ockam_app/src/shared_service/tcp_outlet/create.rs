b7253f50ca9afa4d73583fa1380f5d4b6782a087
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2023-09-07 13:24:20 +0300

    feat(rust): rename the port field into address and support change the address and the port
    The changes include:
    * Replace the Port field name to Address
    * Support the formats Host:Port, IP:Port and Port (defaulting to 127.0.0.1 aka `localhost`)
    * Adapt the app back-end to parse the address correctly.
    
    - Closes: #5685

--- implementations/rust/ockam/ockam_app/src/shared_service/tcp_outlet/create.rs
+++ implementations/rust/ockam/ockam_app/src/shared_service/tcp_outlet/create.rs
@@ -1,6 +1,6 @@
 use miette::{IntoDiagnostic, WrapErr};
 use ockam_api::address::extract_address_value;
-use std::net::SocketAddr;
+use ockam_transport_tcp::resolve_peer;
 use tauri::{AppHandle, Manager, Wry};
 use tracing::{debug, error, info};
 
@@ -9,16 +9,19 @@ use crate::app::AppState;
 use crate::error::Error;
 use crate::invitations::commands::create_service_invitation;
 
+/// The default host to use when creating a TCP outlet if the user doesn't specify one.
+const DEFAULT_HOST: &str = "localhost";
+
 /// Create a TCP outlet within the default node.
 #[tauri::command]
 pub async fn tcp_outlet_create(
     app: AppHandle<Wry>,
     service: String,
-    port: String,
+    address: String,
     email: String,
 ) -> Result<(), String> {
     let email = if email.is_empty() { None } else { Some(email) };
-    tcp_outlet_create_impl(app, service, port, email)
+    tcp_outlet_create_impl(app, service, address, email)
         .await
         .map_err(|e| {
             error!("{:?}", e);
@@ -30,15 +33,19 @@ pub async fn tcp_outlet_create(
 async fn tcp_outlet_create_impl(
     app: AppHandle<Wry>,
     service: String,
-    port: String,
+    address: String,
     email: Option<String>,
 ) -> crate::Result<()> {
-    debug!(%service, %port, "Creating an outlet");
+    debug!(%service, %address, "Creating an outlet");
     let app_state = app.state::<AppState>();
-    let socket_addr: SocketAddr = format!("127.0.0.1:{port}")
-        .parse()
+    let addr = if let Some((host, port)) = address.split_once(':') {
+        format!("{host}:{port}")
+    } else {
+        format!("{DEFAULT_HOST}:{address}")
+    };
+    let socket_addr = resolve_peer(addr)
         .into_diagnostic()
-        .wrap_err("Invalid port")?;
+        .wrap_err("Invalid address. The expected formats are 'host:port', 'ip:port' or 'port'")?;
     let worker_addr = extract_address_value(&service).wrap_err("Invalid service address")?;
     let node_manager_worker = app_state.node_manager_worker().await;
     let mut node_manager = node_manager_worker.inner().write().await;
