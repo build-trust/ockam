f2e399804652c8afe0ffaa82feedb94ebde743a0
Adrian Benavides <adri.benavides312@gmail.com>
2023-09-11 09:32:54 +0200

    refactor(rust): tray menu event processor, so each module handles privately their events

--- implementations/rust/ockam/ockam_app/src/enroll/tray_menu.rs
+++ implementations/rust/ockam/ockam_app/src/enroll/tray_menu.rs
@@ -1,4 +1,4 @@
-use tauri::menu::{IconMenuItemBuilder, MenuBuilder, MenuItemBuilder, NativeIcon};
+use tauri::menu::{IconMenuItemBuilder, MenuBuilder, MenuEvent, MenuItemBuilder, NativeIcon};
 use tauri::{AppHandle, Manager, Runtime, State};
 use tracing::error;
 
@@ -6,10 +6,10 @@ use crate::app::events::SystemTrayOnUpdatePayload;
 use crate::app::AppState;
 use crate::enroll::enroll_user::enroll_user;
 
-pub const ENROLL_MENU_EMAIL: &str = "user-email";
-pub const ENROLL_MENU_HEADER_ID: &str = "enroll-header";
-pub const ENROLL_MENU_ID: &str = "enroll";
-pub const ENROLL_MENU_USER_NAME: &str = "user-name";
+const ENROLL_MENU_EMAIL: &str = "user-email";
+const ENROLL_MENU_HEADER_ID: &str = "enroll-header";
+const ENROLL_MENU_ID: &str = "enroll";
+const ENROLL_MENU_USER_NAME: &str = "user-name";
 
 pub(crate) async fn build_user_info_section<'a, R: Runtime, M: Manager<R>>(
     app_handle: &AppHandle<R>,
@@ -71,9 +71,19 @@ pub(crate) async fn build_enroll_section<'a, R: Runtime, M: Manager<R>>(
     builder
 }
 
+pub fn process_tray_menu_event<R: Runtime>(
+    app: &AppHandle<R>,
+    event: &MenuEvent,
+) -> tauri::Result<()> {
+    match event.id.as_ref() {
+        ENROLL_MENU_ID => on_enroll(app),
+        _ => Ok(()),
+    }
+}
+
 /// Event listener for the "Enroll" menu item
 /// Enroll the user and show that it has been enrolled
-pub fn on_enroll<R: Runtime>(app: &AppHandle<R>) -> tauri::Result<()> {
+fn on_enroll<R: Runtime>(app: &AppHandle<R>) -> tauri::Result<()> {
     let app_handle = app.clone();
     tauri::async_runtime::spawn(async move {
         enroll_user(&app_handle)
