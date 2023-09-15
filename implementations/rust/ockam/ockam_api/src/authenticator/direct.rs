56da89991ea4a1e42700e15c61849ff9cb660345
Oleksandr Deundiak <olek@ockam.io>
2023-09-15 19:48:15 +0200

    feat(rust): delete `LegacyApiConverter`

--- implementations/rust/ockam/ockam_api/src/authenticator/direct.rs
+++ implementations/rust/ockam/ockam_api/src/authenticator/direct.rs
@@ -8,7 +8,7 @@ use ockam::identity::OneTimeCode;
 use ockam::identity::{secure_channel_required, TRUST_CONTEXT_ID};
 use ockam::identity::{AttributesEntry, IdentityAttributesReader, IdentityAttributesWriter};
 use ockam::identity::{Identifier, IdentitySecureChannelLocalInfo};
-use ockam_core::api::{self, Method, Request, Response, Status};
+use ockam_core::api::{self, Method, Request, Response};
 use ockam_core::compat::sync::{Arc, RwLock};
 use ockam_core::errcode::{Kind, Origin};
 use ockam_core::{self, CowStr, Result, Routed, Worker};
@@ -16,136 +16,12 @@ use ockam_node::{Context, RpcClient};
 use std::collections::HashMap;
 use std::num::NonZeroUsize;
 use std::time::{Duration, Instant};
-use tracing::{trace, warn};
+use tracing::trace;
 use types::AddMember;
 
 use crate::authenticator::direct::types::CreateToken;
 
 const MAX_TOKEN_DURATION: Duration = Duration::from_secs(600);
-
-/// Schema identifier for a project membership credential.
-///
-/// The credential will consist of the following attributes:
-///
-/// - `project_id` : bytes
-/// - `role`: b"member"
-pub const LEGACY_MEMBER: &str = "member"; // fixme
-
-// This acts as a facade, modifying and forwarding incoming messages from legacy clients
-// to the new endpoints.   It's going to be removed once we don't need to maintain compatibility
-// with old clients anymore.
-pub struct LegacyApiConverter {}
-
-impl LegacyApiConverter {
-    pub fn new() -> Self {
-        Self {}
-    }
-}
-
-// Keep clippy happy
-impl Default for LegacyApiConverter {
-    fn default() -> Self {
-        LegacyApiConverter::new()
-    }
-}
-
-#[ockam_core::worker]
-impl Worker for LegacyApiConverter {
-    type Context = Context;
-    type Message = Vec<u8>;
-
-    async fn handle_message(
-        &mut self,
-        ctx: &mut Context,
-        msg: Routed<Self::Message>,
-    ) -> Result<()> {
-        let body = msg.as_body().clone();
-        let mut dec = Decoder::new(&body);
-        let mut message = msg.into_local_message();
-        let mut second_msg = message.clone(); // Borrow checker.  When authenticating using an enrollment token,
-                                              // to adhere to the previous API this legacy worker actually issues
-                                              // _two_ request on behalf of the user: one to enroll, other to get
-                                              // the credential
-        let transport_message = message.transport_mut();
-
-        // Remove my address from the onward_route
-        transport_message.onward_route.step()?;
-
-        match dec.decode::<Request>() {
-            Ok(req) => match (req.method(), req.path()) {
-                (Some(Method::Post), "/tokens") => {
-                    transport_message
-                        .onward_route
-                        .modify()
-                        .append("enrollment_token_issuer");
-                    ctx.forward(message).await
-                }
-                (Some(Method::Post), "/members") => {
-                    transport_message
-                        .onward_route
-                        .modify()
-                        .append("direct_authenticator");
-                    ctx.forward(message).await
-                }
-                (Some(Method::Post), "/credential") if req.has_body() => {
-                    transport_message
-                        .onward_route
-                        .modify()
-                        .append("enrollment_token_acceptor");
-
-                    // We don't want the 200 OK to be routed back to the client here,
-                    // as legacy client is expecting the response to contain the credential.
-                    transport_message
-                        .return_route
-                        .modify()
-                        .prepend(ctx.address());
-                    ctx.forward(message).await?;
-                    // Give time for the enrollment to be done before asking for a credential.
-                    // A better alternative is to wait for the response on handle_message,
-                    // then decode it and issue the next message, then returning the credential
-                    // to the client.   But it's too cumbersome for this that is a workaround
-                    // to get previous clients to work, and we are removing this code soon after.
-                    ockam_node::compat::tokio::time::sleep(Duration::from_millis(2000)).await;
-                    // Request the credential,  note the return route points to the client
-                    let body = Request::post("/credential").to_vec()?;
-                    let transport_message = second_msg.transport_mut();
-                    transport_message
-                        .onward_route
-                        .modify()
-                        .append("credential_issuer");
-                    transport_message.payload = body;
-                    ctx.forward(second_msg).await?;
-                    Ok(())
-                }
-                (Some(Method::Post), "/credential") => {
-                    transport_message
-                        .onward_route
-                        .modify()
-                        .append("credential_issuer");
-                    ctx.forward(message).await
-                }
-                (_, _) => {
-                    warn!("Legacy Authority Compatibility Worker received request at unknown path: {req:?}");
-                    Ok(())
-                }
-            },
-            Err(_) => {
-                let mut dec = Decoder::new(&body);
-                match dec.decode::<Response>() {
-                    Ok(resp) => {
-                        if resp.status() == Some(Status::Ok) {
-                            debug!("Received resp: {resp:?}");
-                        } else {
-                            warn!("Received a non-ok response {resp:?}");
-                        }
-                    }
-                    _ => warn!("Received and discarded a non request/response message {message:?}"),
-                }
-                Ok(())
-            }
-        }
-    }
-}
 
 pub struct DirectAuthenticator {
     trust_context: String,
