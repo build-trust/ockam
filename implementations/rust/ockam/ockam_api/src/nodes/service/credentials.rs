84329ed7e7119a577ce2776bbd646aa031e45c72
etorreborre <etorreborre@yahoo.com>
2023-09-21 15:24:55 +0200

    refactor(rust): implement the credentials trait for the background node

--- implementations/rust/ockam/ockam_api/src/nodes/service/credentials.rs
+++ implementations/rust/ockam/ockam_api/src/nodes/service/credentials.rs
@@ -37,6 +37,8 @@ pub trait Credentials {
         overwrite: bool,
         identity_name: Option<String>,
     ) -> miette::Result<CredentialAndPurposeKey>;
+
+    async fn present_credential(&self, ctx: &Context, to: &MultiAddr, oneway: bool) -> miette::Result<()>;
 }
 
 #[async_trait]
@@ -56,13 +58,29 @@ impl Credentials for AuthorityNode {
             .success()
             .into_diagnostic()
     }
+
+    async fn present_credential(&self, ctx: &Context, to: &MultiAddr, oneway: bool) -> miette::Result<()> {
+        let body = PresentCredentialRequest::new(to, oneway);
+        let req = Request::post("/node/credentials/actions/present").body(body);
+        self.0
+            .tell(ctx, "", req)
+            .await
+            .into_diagnostic()?
+            .success()
+            .into_diagnostic()
+    }
 }
 
 #[async_trait]
 impl Credentials for BackgroundNode {
     async fn get_credential(&self, ctx: &Context, overwrite: bool, identity_name: Option<String>) -> miette::Result<CredentialAndPurposeKey> {
-        let b = GetCredentialRequest::new(overwrite, identity_name);
-        self.ask(ctx, Request::post("/node/credentials/actions/get").body(b)).await
+        let body = GetCredentialRequest::new(overwrite, identity_name);
+        self.ask(ctx, Request::post("/node/credentials/actions/get").body(body)).await
+    }
+
+    async fn present_credential(&self, ctx: &Context, to: &MultiAddr, oneway: bool) -> miette::Result<()> {
+        let body = PresentCredentialRequest::new(to, oneway);
+        self.tell(ctx, Request::post("/node/credentials/actions/present").body(body)).await
     }
 }
 
--- implementations/rust/ockam/ockam_command/src/credential/get.rs
+++ implementations/rust/ockam/ockam_command/src/credential/get.rs
@@ -1,10 +1,10 @@
 use clap::Args;
 
 use ockam::Context;
-use ockam_api::nodes::{Credentials, BackgroundNode};
+use ockam_api::nodes::{BackgroundNode, Credentials};
 
 use crate::node::{get_node_name, initialize_node_if_default, NodeOpts};
-use crate::util::{api, node_rpc};
+use crate::util::node_rpc;
 use crate::CommandGlobalOpts;
 
 #[derive(Clone, Debug, Args)]
@@ -33,6 +33,7 @@ async fn rpc(ctx: Context, (opts, cmd): (CommandGlobalOpts, GetCommand)) -> miet
 async fn run_impl(ctx: &Context, opts: CommandGlobalOpts, cmd: GetCommand) -> miette::Result<()> {
     let node_name = get_node_name(&opts.state, &cmd.node_opts.at_node);
     let node = BackgroundNode::create(ctx, &opts.state, &node_name).await?;
-    node.get_credential(ctx, cmd.overwrite, cmd.identity).await?;
+    node.get_credential(ctx, cmd.overwrite, cmd.identity)
+        .await?;
     Ok(())
 }
