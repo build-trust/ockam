a216d4b77cc67a04d9193a1860eb6789174cef07
Pablo Polvorin <pablo@ockam.io>
2023-09-22 14:50:21 -0300

    chore(elixir) more typespec

--- implementations/elixir/ockam/ockly/lib/ockly/native.ex
+++ implementations/elixir/ockam/ockly/lib/ockly/native.ex
@@ -9,9 +9,16 @@ defmodule Ockly.Native do
     load_from: {:ockly, "priv/native/libockly"}
 
   def create_identity, do: create_identity(nil)
+
+  @spec create_identity(binary() | nil) :: {binary(), binary()}
   def create_identity(_), do: error()
+
+  @spec check_identity(binary()) :: binary() | {:error, term()}
   def check_identity(_), do: error()
+
+  @spec attest_secure_channel_key(binary(), binary()) :: binary() | {:error, term()}
   def attest_secure_channel_key(_, _), do: error()
+
   def verify_secure_channel_key_attestation(_, _, _), do: error()
   def verify_credential(_, _, _), do: error()
   def import_signing_secret(_), do: error()
