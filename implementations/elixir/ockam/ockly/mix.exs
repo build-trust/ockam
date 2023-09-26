684d322627f12325b6addd7e52249a35f3d31156
Pablo Polvorin <pablo@ockam.io>
2023-09-25 21:40:08 -0300

    feat(elixir): share elixir build dir to speedup ci testing
    keeps the CI test task time in line while not depending on
    precompiled ffi binaries. It's a middle ground of using
    elixir' umbrella project that I was afraid of switching at
    this point

--- implementations/elixir/ockam/ockly/mix.exs
+++ implementations/elixir/ockam/ockly/mix.exs
@@ -1,9 +1,6 @@
 defmodule Ockly.MixProject do
   use Mix.Project
 
-  @ockam_release_url "https://github.com/metaclips/ockam/releases/download"
-  @version "ockam_v0.86.0"
-
   def project do
     [
       app: :ockly,
@@ -36,93 +33,10 @@ defmodule Ockly.MixProject do
 
   defp aliases do
     [
-      "check.native": &check_native/1,
-      compile: ["check.native", "compile"],
       credo: "credo --strict",
       "lint.format": "format --check-formatted",
       "lint.credo": "credo --strict",
       lint: ["lint.format", "lint.credo"]
     ]
-  end
-
-  defp check_native(args) do
-    case prebuilt_lib_exists?() do
-      true ->
-        :ok
-
-      false ->
-        case System.get_env("OCKLY_PRECOMPILED_LIB", "true") do
-          "true" -> download_native(args)
-          "false" -> :ok
-        end
-    end
-  end
-
-  defp download_native(_args) do
-    with {:ok, lib_name} <- os_specific_lib() do
-      download_url = "#{@ockam_release_url}/#{@version}/#{lib_name}"
-
-      ## To download files we need inets and ssl
-      :inets.start()
-      :ssl.start()
-
-      dest_file = prebuilt_lib_path()
-
-      dest_dir = Path.dirname(dest_file)
-
-      File.mkdir_p!(dest_dir)
-
-      IO.puts("Downloading lib from #{download_url} to #{dest_file}")
-      File.rm_rf(dest_file)
-
-      download_result =
-        :httpc.request(
-          :get,
-          {to_charlist(download_url), []},
-          # We should ensure we test this TLS version when
-          # we update OTP.
-          [{:ssl, [{:versions, [:"tlsv1.2"]}]}],
-          stream: to_charlist(dest_file)
-        )
-
-      case download_result do
-        {:ok, :saved_to_file} -> IO.puts("Download OK")
-        {:ok, other} -> IO.puts("Download error: #{inspect(other)}")
-        other -> IO.puts("Download error: #{inspect(other)}")
-      end
-    end
-  end
-
-  defp prebuilt_lib_exists?() do
-    File.exists?(prebuilt_lib_path())
-  end
-
-  defp prebuilt_lib_path() do
-    Path.join(["priv", "native", "libockly.so"])
-  end
-
-  defp os_specific_lib() do
-    case {:os.type(), to_string(:erlang.system_info(:system_architecture))} do
-      ## Linux libs only built for GNU
-      {{:unix, :linux}, "x86_64" <> type} ->
-        if String.ends_with?(type, "gnu") do
-          {:ok, "linux_x86_libockly.so"}
-        else
-          :error
-        end
-
-      {{:unix, :linux}, "aarch64" <> _type} ->
-        :error
-
-      ## MacOS libs are multi-arch
-      {{:unix, :darwin}, "x86_64" <> _} ->
-        {:ok, "darwin_universal_libockly.so"}
-
-      {{:unix, :darwin}, "aarch64" <> _} ->
-        {:ok, "darwin_universal_libockly.so"}
-
-      _ ->
-        :error
-    end
   end
 end
