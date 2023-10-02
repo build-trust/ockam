d68d60beaa9d6dc6120f4b6e52bac8a4220ffd38
Oleksandr Deundiak <olek@ockam.io>
2023-10-02 18:18:15 +0200

    feat(rust): improve `Identity` `CBOR` structures

--- implementations/rust/ockam/ockam_identity/src/models/versioned_data.rs
+++ implementations/rust/ockam/ockam_identity/src/models/versioned_data.rs
@@ -4,11 +4,10 @@ use ockam_core::compat::vec::Vec;
 /// Binary and a version
 #[derive(Clone, Debug, PartialEq, Eq, Encode, Decode)]
 #[rustfmt::skip]
-#[cbor(map)]
 pub struct VersionedData {
     /// Version
-    #[n(1)] pub version: u8,
+    #[n(0)] pub version: u8,
     /// Binary
     #[cbor(with = "minicbor::bytes")]
-    #[n(2)] pub data: Vec<u8>,
+    #[n(1)] pub data: Vec<u8>,
 }
