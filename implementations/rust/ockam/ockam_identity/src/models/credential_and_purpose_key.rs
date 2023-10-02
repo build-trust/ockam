3ecafc259f9803663a2394f82e2ee0a72594e4f5
Oleksandr Deundiak <olek@ockam.io>
2023-10-02 18:18:15 +0200

    feat(rust): improve `Identity` `CBOR` structures

--- implementations/rust/ockam/ockam_identity/src/models/credential_and_purpose_key.rs
+++ implementations/rust/ockam/ockam_identity/src/models/credential_and_purpose_key.rs
@@ -5,11 +5,10 @@ use minicbor::{Decode, Encode};
 /// [`Credential`] and will be used to verify it
 #[derive(Clone, Debug, PartialEq, Eq, Encode, Decode)]
 #[rustfmt::skip]
-#[cbor(map)]
 pub struct CredentialAndPurposeKey {
     /// [`Credential`]
-    #[n(1)] pub credential: Credential,
+    #[n(0)] pub credential: Credential,
     /// Corresponding [`PurposeKeyAttestation`] that was used to issue that
     /// [`Credential`] and will be used to verify it
-    #[n(2)] pub purpose_key_attestation: PurposeKeyAttestation,
+    #[n(1)] pub purpose_key_attestation: PurposeKeyAttestation,
 }
--- implementations/rust/ockam/ockam_identity/src/models/purpose_key_attestation.rs
+++ implementations/rust/ockam/ockam_identity/src/models/purpose_key_attestation.rs
@@ -12,14 +12,13 @@ use ockam_vault::{
 /// a [`super::super::purpose_key::PurposeKey`] with itself
 #[derive(Clone, Debug, PartialEq, Eq, Encode, Decode)]
 #[rustfmt::skip]
-#[cbor(map)]
 pub struct PurposeKeyAttestation {
     /// CBOR serialized [`super::VersionedData`]
     /// where VersionedData::data is CBOR serialized [`PurposeKeyAttestationData`]
     #[cbor(with = "minicbor::bytes")]
-    #[n(1)] pub data: Vec<u8>,
+    #[n(0)] pub data: Vec<u8>,
     /// Signature over data field using a key from [`super::super::identity::Identity`]
-    #[n(2)] pub signature: PurposeKeyAttestationSignature,
+    #[n(1)] pub signature: PurposeKeyAttestationSignature,
 }
 
 /// Signature over data field using a key from [`super::super::identity::Identity`]
@@ -27,27 +26,26 @@ pub struct PurposeKeyAttestation {
 #[rustfmt::skip]
 pub enum PurposeKeyAttestationSignature {
     /// Signature using EdDSA Ed25519 key from the corresponding [`super::super::identity::Identity`]
-    #[n(1)] EdDSACurve25519(#[n(0)] EdDSACurve25519Signature),
+    #[n(0)] EdDSACurve25519(#[n(0)] EdDSACurve25519Signature),
     /// Signature using ECDSA P256 key from the corresponding [`super::super::identity::Identity`]
-    #[n(2)] ECDSASHA256CurveP256(#[n(0)] ECDSASHA256CurveP256Signature),
+    #[n(1)] ECDSASHA256CurveP256(#[n(0)] ECDSASHA256CurveP256Signature),
 }
 
 /// Data inside a [`PurposeKeyAttestation`]
 #[derive(Clone, Debug, PartialEq, Eq, Encode, Decode)]
 #[rustfmt::skip]
-#[cbor(map)]
 pub struct PurposeKeyAttestationData {
     /// [`Identifier`] of the [`super::super::identity::Identity`] this Purpose Key belongs to
-    #[n(1)] pub subject: Identifier,
+    #[n(0)] pub subject: Identifier,
     /// Latest [`ChangeHash`] (at the moment of issuing) of the [`super::super::identity::Identity`]
     /// this Purpose Key belongs to
-    #[n(2)] pub subject_latest_change_hash: ChangeHash,
+    #[n(1)] pub subject_latest_change_hash: ChangeHash,
     /// Public key of this Purpose Key
-    #[n(3)] pub public_key: PurposePublicKey,
+    #[n(2)] pub public_key: PurposePublicKey,
     /// Creation [`TimestampInSeconds`] (UTC)
-    #[n(4)] pub created_at: TimestampInSeconds,
+    #[n(3)] pub created_at: TimestampInSeconds,
     /// Expiration [`TimestampInSeconds`] (UTC)
-    #[n(5)] pub expires_at: TimestampInSeconds,
+    #[n(4)] pub expires_at: TimestampInSeconds,
 }
 
 /// [`PurposeKeyAttestation`]'s public key
@@ -56,9 +54,9 @@ pub struct PurposeKeyAttestationData {
 pub enum PurposePublicKey {
     /// Key dedicated to creation of Secure Channels
     /// This key is used as a static key in Noise XX handshake
-    #[n(1)] SecureChannelStatic(#[n(0)] X25519PublicKey),
+    #[n(0)] SecureChannelStatic(#[n(0)] X25519PublicKey),
     /// Key dedicated to signing [`super::Credential`]s
-    #[n(2)] CredentialSigning(#[n(0)] CredentialVerifyingKey),
+    #[n(1)] CredentialSigning(#[n(0)] CredentialVerifyingKey),
 }
 
 /// Key dedicated to signing [`super::Credential`]s
@@ -66,7 +64,7 @@ pub enum PurposePublicKey {
 #[rustfmt::skip]
 pub enum CredentialVerifyingKey {
     /// Curve25519 Public Key for verifying EdDSA signatures.
-    #[n(1)] EdDSACurve25519(#[n(0)] EdDSACurve25519PublicKey),
+    #[n(0)] EdDSACurve25519(#[n(0)] EdDSACurve25519PublicKey),
     /// Curve P-256 Public Key for verifying ECDSA SHA256 signatures.
-    #[n(2)] ECDSASHA256CurveP256(#[n(0)] ECDSASHA256CurveP256PublicKey),
+    #[n(1)] ECDSASHA256CurveP256(#[n(0)] ECDSASHA256CurveP256PublicKey),
 }
