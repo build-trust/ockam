79712e23c027179d7c60461526daea5dc30521e9
Hergy Fongue <hergy.fongue@ext.markant.com>
2023-10-05 18:47:21 +0200

    fix(rust) - status --all remove unused lines

--- implementations/rust/ockam/ockam_command/src/version.rs
+++ implementations/rust/ockam/ockam_command/src/version.rs
@@ -14,7 +14,6 @@ impl Version {
 
     pub(crate) fn version() -> &'static str {
         let crate_version = crate_version!();
-        let git_hash = env!("GIT_HASH");
         let message = format!("{crate_version}");
         Box::leak(message.into_boxed_str())
     }
