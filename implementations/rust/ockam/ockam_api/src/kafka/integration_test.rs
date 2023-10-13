eec9a09a5307839ccb1a2f33a124b862aee152a9
sayyedarib <sayyedaribhussain4321@gmail.com>
2023-10-13 23:09:09 +0530

    fix(rust): increase timeout of rust test
    Signed-off-by: sayyedarib <sayyedaribhussain4321@gmail.com>

--- implementations/rust/ockam/ockam_api/src/kafka/integration_test.rs
+++ implementations/rust/ockam/ockam_api/src/kafka/integration_test.rs
@@ -110,7 +110,7 @@ mod test {
     }
 
     #[allow(non_snake_case)]
-    #[ockam_macros::test(timeout = 5_000)]
+    #[ockam_macros::test(timeout = 60_000)]
     async fn producer__flow_with_mock_kafka__content_encryption_and_decryption(
         context: &mut Context,
     ) -> ockam::Result<()> {
