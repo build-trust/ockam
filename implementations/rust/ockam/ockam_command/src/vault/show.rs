1d560dffd0c7a364fc35b62a32aecd608f919d5e
abhiramtilakiiit <abhiram.potula@research.iiit.ac.in>
2023-10-25 22:36:22 +0530

    feat(rust): add interactive tui to vault show

--- implementations/rust/ockam/ockam_command/src/vault/show.rs
+++ implementations/rust/ockam/ockam_command/src/vault/show.rs
@@ -4,8 +4,10 @@ use clap::Args;
 use miette::IntoDiagnostic;
 
 use ockam_api::cli_state::traits::StateDirTrait;
+use ockam_api::cli_state::VaultConfig;
 
 use crate::util::local_cmd;
+use crate::vault::list::VaultListOutput;
 use crate::{docs, CommandGlobalOpts};
 
 const LONG_ABOUT: &str = include_str!("./static/show/long_about.txt");
@@ -31,28 +33,80 @@ impl ShowCommand {
 }
 
 fn run_impl(opts: CommandGlobalOpts, cmd: ShowCommand) -> miette::Result<()> {
-    let name = cmd
-        .name
-        .unwrap_or(opts.state.vaults.default()?.name().to_string());
-    let state = opts.state.vaults.get(name)?;
+    // when interactive prompt is not required
+    if cmd.name.is_some() || !opts.terminal.can_ask_for_user_input() {
+        let name = cmd
+            .name
+            .unwrap_or(opts.state.vaults.default()?.name().to_string());
+        let state = opts.state.vaults.get(name)?;
 
-    let json = serde_json::to_string_pretty(&state).into_diagnostic()?;
+        let json = serde_json::to_string_pretty(&state).into_diagnostic()?;
 
-    let plain = {
-        let mut buf = String::new();
+        let plain = {
+            let mut buf = String::new();
 
-        writeln!(buf, "Vault:").into_diagnostic()?;
-        for line in state.to_string().lines() {
-            writeln!(buf, "{:2}{}", "", line).into_diagnostic()?;
+            writeln!(buf, "Vault:").into_diagnostic()?;
+            for line in state.to_string().lines() {
+                writeln!(buf, "{:2}{}", "", line).into_diagnostic()?;
+            }
+            buf
+        };
+
+        opts.terminal
+            .stdout()
+            .json(json)
+            .plain(plain)
+            .write_line()?;
+    } else {
+        let vault_names: Vec<String> = opts.state.vaults.list_items_names()?;
+
+        if vault_names.is_empty() {
+            opts.terminal
+                .stdout()
+                .plain(
+                    "There are no vaults to show, use `ockam vault create` to create a new vault",
+                )
+                .write_line()?;
+        } else {
+            let selected_names = opts.terminal.select_multiple(
+                "Select the vaults which you want to display".to_string(),
+                vault_names,
+            );
+
+            if selected_names.is_empty() {
+                opts.terminal
+                    .stdout()
+                    .plain("No vaults selected, use <space> to select vaults")
+                    .write_line()?;
+            } else {
+                let mut output: Vec<VaultListOutput> = Vec::new();
+
+                for name in selected_names {
+                    let state = opts.state.vaults.get(&name)?;
+                    let config = VaultConfig::new(state.is_aws())?;
+                    let is_default = opts.state.vaults.is_default(&name)?;
+                    let vault = VaultListOutput::new(name, config, is_default);
+                    output.push(vault);
+                }
+
+                // same output as vault list
+                let plain = opts.terminal.build_list(
+                    &output,
+                    "Vaults",
+                    "No vaults found on this system.",
+                )?;
+
+                let json = serde_json::to_string_pretty(&output).into_diagnostic()?;
+
+                opts.terminal
+                    .clone()
+                    .stdout()
+                    .plain(plain)
+                    .json(json)
+                    .write_line()?;
+            }
         }
-        buf
-    };
-
-    opts.terminal
-        .stdout()
-        .json(json)
-        .plain(plain)
-        .write_line()?;
+    }
 
     Ok(())
 }
