f237e007069ea030daf2504bc462f523b6041b52
Olumide Ogundele <olumideralph@gmail.com>
2023-11-12 10:32:37 +0000

    feat(rust): tuify `space delete` command

--- implementations/rust/ockam/ockam_command/src/space/delete.rs
+++ implementations/rust/ockam/ockam_command/src/space/delete.rs
@@ -1,15 +1,17 @@
 use clap::Args;
 use colorful::Colorful;
+use console::Term;
 
 use ockam::Context;
 use ockam_api::cli_state::{StateDirTrait, StateItemTrait};
 use ockam_api::cloud::space::Spaces;
-
+use ockam_api::cloud::Controller;
 use ockam_api::nodes::InMemoryNode;
 
+use crate::terminal::tui::DeleteCommandTui;
 use crate::util::api::CloudOpts;
 use crate::util::node_rpc;
-use crate::{docs, fmt_ok, CommandGlobalOpts};
+use crate::{docs, fmt_ok, CommandGlobalOpts, Terminal, TerminalStream};
 
 const LONG_ABOUT: &str = include_str!("./static/delete/long_about.txt");
 const AFTER_LONG_HELP: &str = include_str!("./static/delete/after_long_help.txt");
@@ -17,14 +19,13 @@ const AFTER_LONG_HELP: &str = include_str!("./static/delete/after_long_help.txt"
 /// Delete a space
 #[derive(Clone, Debug, Args)]
 #[command(
-arg_required_else_help = true,
 long_about = docs::about(LONG_ABOUT),
 after_long_help = docs::after_help(AFTER_LONG_HELP)
 )]
 pub struct DeleteCommand {
     /// Name of the space.
     #[arg(display_order = 1001)]
-    pub name: String,
+    pub space_name: Option<String>,
 
     #[command(flatten)]
     pub cloud_opts: CloudOpts,
@@ -36,37 +37,95 @@ pub struct DeleteCommand {
 
 impl DeleteCommand {
     pub fn run(self, options: CommandGlobalOpts) {
-        node_rpc(rpc, (options, self));
+        node_rpc(run_impl, (options, self));
     }
-}
-
-async fn rpc(ctx: Context, (opts, cmd): (CommandGlobalOpts, DeleteCommand)) -> miette::Result<()> {
-    run_impl(&ctx, opts, cmd).await
 }
 
 async fn run_impl(
-    ctx: &Context,
-    opts: CommandGlobalOpts,
-    cmd: DeleteCommand,
+    ctx: Context,
+    (opts, cmd): (CommandGlobalOpts, DeleteCommand),
 ) -> miette::Result<()> {
-    if opts
-        .terminal
-        .confirmed_with_flag_or_prompt(cmd.yes, "Are you sure you want to delete this space?")?
-    {
-        let space_id = opts.state.spaces.get(&cmd.name)?.config().id.clone();
-        let node = InMemoryNode::start(ctx, &opts.state).await?;
-        let controller = node.create_controller().await?;
-        controller.delete_space(ctx, space_id).await?;
+    DeleteTui::run(ctx, opts, cmd).await
+}
 
-        let _ = opts.state.spaces.delete(&cmd.name);
-        // TODO: remove projects associated to the space.
-        //  Currently we are not storing that association in the project config file.
-        opts.terminal
-            .stdout()
-            .plain(fmt_ok!("Space with name '{}' has been deleted.", &cmd.name))
-            .machine(&cmd.name)
-            .json(serde_json::json!({ "name": &cmd.name }))
-            .write_line()?;
+pub struct DeleteTui {
+    ctx: Context,
+    opts: CommandGlobalOpts,
+    controller: Controller,
+    cmd: DeleteCommand,
+}
+
+impl DeleteTui {
+    pub async fn run(
+        ctx: Context,
+        opts: CommandGlobalOpts,
+        cmd: DeleteCommand,
+    ) -> miette::Result<()> {
+        let node = InMemoryNode::start(&ctx, &opts.state).await?;
+        let controller = node.create_controller().await?;
+        let tui = Self {
+            ctx,
+            opts,
+            controller,
+            cmd,
+        };
+        tui.delete().await
     }
-    Ok(())
+}
+
+#[ockam_core::async_trait]
+impl DeleteCommandTui for DeleteTui {
+    const ITEM_NAME: &'static str = "space";
+
+    fn cmd_arg_item_name(&self) -> Option<&str> {
+        self.cmd.space_name.as_deref()
+    }
+
+    fn cmd_arg_delete_all(&self) -> bool {
+        false
+    }
+
+    fn cmd_arg_confirm_deletion(&self) -> bool {
+        self.cmd.yes
+    }
+
+    fn terminal(&self) -> Terminal<TerminalStream<Term>> {
+        self.opts.terminal.clone()
+    }
+
+    async fn get_arg_item_name_or_default(&self) -> miette::Result<String> {
+        let space_name = match &self.cmd.space_name {
+            None => self.opts.state.spaces.default()?.name().to_string(),
+            Some(n) => n.to_string(),
+        };
+        Ok(space_name)
+    }
+
+    async fn list_items_names(&self) -> miette::Result<Vec<String>> {
+        Ok(self.opts.state.spaces.list_items_names()?)
+    }
+
+    async fn delete_single(&self, item_name: &str) -> miette::Result<()> {
+        let space_id = self.opts.state.spaces.get(item_name)?.config().id.clone();
+        self.controller.delete_space(&self.ctx, space_id).await?;
+        let _ = self.opts.state.spaces.delete(item_name);
+
+        self.terminal()
+            .stdout()
+            .plain(fmt_ok!(
+                "The space with name {} has been deleted",
+                item_name.light_magenta()
+            ))
+            .machine(item_name)
+            .json(serde_json::json!({ "name": item_name }))
+            .write_line()?;
+        Ok(())
+    }
+
+    async fn delete_multiple(&self, items_names: Vec<String>) -> miette::Result<()> {
+        for item_name in items_names {
+            self.delete_single(&item_name).await?;
+        }
+        Ok(())
+    }
 }
