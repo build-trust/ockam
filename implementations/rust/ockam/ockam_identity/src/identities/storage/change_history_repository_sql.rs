160a77714a69f654d3c26fd595233e1ea610a91e
etorreborre <etorreborre@yahoo.com>
2023-12-01 15:32:01 +0100

    feat(rust): execute an identity update in a transaction

--- implementations/rust/ockam/ockam_identity/src/identities/storage/change_history_repository_sql.rs
+++ implementations/rust/ockam/ockam_identity/src/identities/storage/change_history_repository_sql.rs
@@ -1,5 +1,7 @@
 use core::str::FromStr;
 
+use sqlx::query::Query;
+use sqlx::sqlite::SqliteArguments;
 use sqlx::*;
 use tracing::debug;
 
@@ -9,7 +11,7 @@ use ockam_core::Result;
 use ockam_node::database::{FromSqlxError, SqlxDatabase, SqlxType, ToSqlxType, ToVoid};
 
 use crate::models::{ChangeHistory, Identifier};
-use crate::ChangeHistoryRepository;
+use crate::{ChangeHistoryRepository, Identity, IdentityError, IdentityHistoryComparison, Vault};
 
 /// Implementation of `IdentitiesRepository` trait based on an underlying database
 /// using sqlx as its API, and Sqlite as its driver
@@ -35,15 +37,50 @@ impl ChangeHistorySqlxDatabase {
 
 #[async_trait]
 impl ChangeHistoryRepository for ChangeHistorySqlxDatabase {
+    async fn update_identity(&self, identity: &Identity) -> Result<()> {
+        let mut transaction = self.database.begin().await.into_core()?;
+        let query1 = query_as("SELECT * FROM identity WHERE identifier=$1")
+            .bind(identity.identifier().to_sql());
+        let row: Option<ChangeHistoryRow> =
+            query1.fetch_optional(&mut *transaction).await.into_core()?;
+
+        let do_insert = match row {
+            Some(row) => {
+                let known_identity = Identity::import_from_change_history(
+                    Some(identity.identifier()),
+                    row.change_history()?,
+                    Vault::create_verifying_vault(),
+                )
+                .await?;
+
+                match identity.compare(&known_identity) {
+                    IdentityHistoryComparison::Conflict | IdentityHistoryComparison::Older => {
+                        return Err(IdentityError::ConsistencyError.into());
+                    }
+                    IdentityHistoryComparison::Newer => true,
+                    IdentityHistoryComparison::Equal => false,
+                }
+            }
+            None => true,
+        };
+        if do_insert {
+            Self::insert_query(identity.identifier(), identity.change_history())
+                .execute(&mut *transaction)
+                .await
+                .void()?
+        };
+        transaction.commit().await.void()
+    }
+
     async fn store_change_history(
         &self,
         identifier: &Identifier,
         change_history: ChangeHistory,
     ) -> Result<()> {
-        let query = query("INSERT OR REPLACE INTO identity VALUES (?, ?)")
-            .bind(identifier.to_sql())
-            .bind(change_history.to_sql());
-        query.execute(&self.database.pool).await.void()
+        Self::insert_query(identifier, &change_history)
+            .execute(&self.database.pool)
+            .await
+            .void()
     }
 
     async fn delete_change_history(&self, identifier: &Identifier) -> Result<()> {
@@ -72,6 +109,17 @@ impl ChangeHistoryRepository for ChangeHistorySqlxDatabase {
         let query = query_as("SELECT * FROM identity");
         let row: Vec<ChangeHistoryRow> = query.fetch_all(&self.database.pool).await.into_core()?;
         row.iter().map(|r| r.change_history()).collect()
+    }
+}
+
+impl ChangeHistorySqlxDatabase {
+    fn insert_query<'a>(
+        identifier: &Identifier,
+        change_history: &ChangeHistory,
+    ) -> Query<'a, Sqlite, SqliteArguments<'a>> {
+        query("INSERT OR REPLACE INTO identity VALUES (?, ?)")
+            .bind(identifier.to_sql())
+            .bind(change_history.to_sql())
     }
 }
 
@@ -109,9 +157,8 @@ impl ChangeHistoryRow {
 
 #[cfg(test)]
 mod tests {
-    use crate::{identities, Identity};
-
     use super::*;
+    use crate::{identities, Identity};
 
     #[tokio::test]
     async fn test_identities_repository() -> Result<()> {
@@ -156,6 +203,23 @@ mod tests {
             .get_change_history(identity2.identifier())
             .await?;
         assert_eq!(result, None);
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_update_identity() -> Result<()> {
+        let identities = identities().await?;
+        let identities_creation = identities.identities_creation();
+        let identifier = identities_creation.create_identity().await?;
+
+        // rotating the identity twice
+        identities_creation.rotate_identity(&identifier).await?;
+        let rotated = identities.get_identity(&identifier).await?;
+        identities_creation.rotate_identity(&identifier).await?;
+
+        // try to update the identity with an old rotated version
+        let result = identities_creation.update_identity(&rotated).await;
+        assert!(result.is_err());
         Ok(())
     }
 
