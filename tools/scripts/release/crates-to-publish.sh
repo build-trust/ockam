ac67b5e1a0215ff32238b6db1e78977bb456fb5c
Michael Uti <utimichael9@gmail.com>
2023-12-18 14:34:50 +0100

    ci: always update the ockam crate even if there are no changes

--- tools/scripts/release/crates-to-publish.sh
+++ tools/scripts/release/crates-to-publish.sh
@@ -38,11 +38,21 @@ for crate in implementations/rust/ockam/*; do
     continue
   fi
 
+  # Check if the src file is updated, if it isn't check if the Cargo.toml file is updated.
   if git diff "$last_git_tag" --quiet --name-status -- "$crate"/src; then
     git diff "$last_git_tag" --quiet --name-status -- "$crate"/Cargo.toml || updated_crates="$updated_crates $crate"
   else
     updated_crates="$updated_crates $crate"
   fi
+done
+
+crates_that_must_be_bumped="ockam ockam_app_lib ockam_command"
+for crate in ${crates_that_must_be_bumped[@]}; do
+  if [[ $updated_crates == *"$crate"* ]]; then
+    continue
+  fi
+
+  updated_crates="$updated_crates $crate"
 done
 
 IFS=" " read -r -a updated_crates <<<"${updated_crates[*]}"
