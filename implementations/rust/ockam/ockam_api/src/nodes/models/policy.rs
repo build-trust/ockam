36ec79e156b1f88d9f3704c338ec5a3571a813a1
Shautvast <shautvast@gmail.com>
2023-12-18 17:24:08 +0100

    fix(rust): added cddl validation

--- implementations/rust/ockam/ockam_api/src/nodes/models/policy.rs
+++ implementations/rust/ockam/ockam_api/src/nodes/models/policy.rs
@@ -1,7 +1,7 @@
 use minicbor::{Decode, Encode};
 use ockam_abac::{Action, Expr};
 
-#[derive(Debug, Decode, Encode)]
+#[derive(Debug, Clone, Decode, Encode)]
 #[rustfmt::skip]
 #[cbor(map)]
 pub struct PolicyList {
@@ -18,7 +18,7 @@ impl PolicyList {
     }
 }
 
-#[derive(Debug, Decode, Encode)]
+#[derive(Debug, Clone, Decode, Encode)]
 pub struct Expression {
     #[n(1)]
     action: Action,
@@ -37,5 +37,82 @@ impl Expression {
 
     pub fn expr(&self) -> &Expr {
         &self.expr
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use quickcheck::{quickcheck, Arbitrary, Gen, TestResult};
+
+    use ockam_abac::{Action, Expr};
+
+    use crate::nodes::models::policy::{Expression, PolicyList};
+    use crate::schema::tests::validate_with_schema;
+
+    quickcheck! {
+        fn policy_list(g: PolicyList) -> TestResult {
+            validate_with_schema("policy_list", g)
+        }
+    }
+
+    quickcheck! {
+        fn policy(g: Expression) -> TestResult {
+            validate_with_schema("expression", g)
+        }
+    }
+
+    impl Arbitrary for PolicyList {
+        fn arbitrary(g: &mut Gen) -> Self {
+            PolicyList {
+                expressions: vec![
+                    Expression {
+                        action: Action::new(String::arbitrary(g).as_str()),
+                        expr: Expr::Str(String::arbitrary(g)),
+                    },
+                    Expression {
+                        action: Action::new(String::arbitrary(g).as_str()),
+                        expr: Expr::Int(i64::arbitrary(g)),
+                    },
+                    Expression {
+                        action: Action::new(String::arbitrary(g).as_str()),
+                        expr: Expr::Float(f64::arbitrary(g)),
+                    },
+                    Expression {
+                        action: Action::new(String::arbitrary(g).as_str()),
+                        expr: Expr::Bool(bool::arbitrary(g)),
+                    },
+                    Expression {
+                        action: Action::new(String::arbitrary(g).as_str()),
+                        expr: Expr::Ident(String::arbitrary(g)),
+                    },
+                    Expression {
+                        action: Action::new(String::arbitrary(g).as_str()),
+                        expr: Expr::Seq(vec![
+                            Expr::Str(String::arbitrary(g)),
+                            Expr::Str(String::arbitrary(g)),
+                        ]),
+                    },
+                    Expression {
+                        action: Action::new(String::arbitrary(g).as_str()),
+                        expr: Expr::List(vec![
+                            Expr::Int(i64::arbitrary(g)),
+                            Expr::Float(f64::arbitrary(g)),
+                        ]),
+                    },
+                ],
+            }
+        }
+    }
+
+    impl Arbitrary for Expression {
+        fn arbitrary(g: &mut Gen) -> Self {
+            Expression {
+                action: Action::new(String::arbitrary(g).as_str()),
+                expr: Expr::List(vec![
+                    Expr::Int(i64::arbitrary(g)),
+                    Expr::Float(f64::arbitrary(g)),
+                ]),
+            }
+        }
     }
 }
