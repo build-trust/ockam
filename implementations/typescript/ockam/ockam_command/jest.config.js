6313b1c454d0271273d2ebd88209862ebe474009
Mrinal Wadhwa <mrinal@ockam.io>
2024-01-06 14:46:00 -0800

    chore(typescript): remove typescript wrapper for ockam command

--- implementations/typescript/ockam/ockam_command/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  clearMocks: true,
-  collectCoverage: true,
-  coverageDirectory: "coverage",
-  coverageProvider: "v8",
-  preset: "ts-jest",
-};
-
-// https://jestjs.io/docs/configuration
