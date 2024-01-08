535d47a79022508ead6d58635820ceec69920af6
Oleksandr Deundiak <olek@ockam.io>
2024-01-08 17:56:49 +0400

    feat(rust): improve `RemoteCredentialRestriever`:
     - implement recreating of tcp connections
     - implement shutting down connection

--- implementations/rust/ockam/ockam_identity/src/credentials/credentials_retriever.rs
+++ implementations/rust/ockam/ockam_identity/src/credentials/credentials_retriever.rs
@@ -4,9 +4,11 @@ use tracing::debug;
 use tracing::trace;
 
 use ockam_core::compat::boxed::Box;
+
 use ockam_core::compat::sync::Arc;
 use ockam_core::{async_trait, Address, Result, Route};
 use ockam_node::{Context, DEFAULT_TIMEOUT};
+use ockam_transport_core::Transport;
 
 use crate::models::CredentialAndPurposeKey;
 use crate::{Identifier, SecureChannels, SecureClient};
@@ -50,6 +52,7 @@ impl CredentialsRetriever for CredentialsMemoryRetriever {
 
 /// Credentials retriever for credentials located on a different node
 pub struct RemoteCredentialsRetriever {
+    transport: Arc<dyn Transport>,
     secure_channels: Arc<SecureChannels>,
     issuer: RemoteCredentialsRetrieverInfo,
 }
@@ -57,35 +60,15 @@ pub struct RemoteCredentialsRetriever {
 impl RemoteCredentialsRetriever {
     /// Create a new remote credential retriever
     pub fn new(
+        transport: Arc<dyn Transport>,
         secure_channels: Arc<SecureChannels>,
         issuer: RemoteCredentialsRetrieverInfo,
     ) -> Self {
         Self {
+            transport,
             secure_channels,
             issuer,
         }
-    }
-
-    async fn make_secure_client(
-        &self,
-        ctx: &Context,
-        for_identity: &Identifier,
-    ) -> Result<SecureClient> {
-        let resolved_route = ctx
-            .resolve_transport_route(self.issuer.route.clone())
-            .await?;
-        trace!(
-            "Getting credential from resolved route: {}",
-            resolved_route.clone()
-        );
-
-        Ok(SecureClient::new(
-            self.secure_channels.clone(),
-            resolved_route,
-            &self.issuer.identifier,
-            for_identity,
-            DEFAULT_TIMEOUT,
-        ))
     }
 }
 
@@ -97,12 +80,48 @@ impl CredentialsRetriever for RemoteCredentialsRetriever {
         for_identity: &Identifier,
     ) -> Result<CredentialAndPurposeKey> {
         debug!("Getting credential from: {}", &self.issuer.route);
-        let client = self.make_secure_client(ctx, for_identity).await?;
-        let credential = client
+        let transport_type = self.transport.transport_type();
+        let (resolved_route, transport_address) = Context::resolve_transport_route_static(
+            self.issuer.route.clone(),
+            [(transport_type, self.transport.clone())].into(),
+        )
+        .await?;
+
+        trace!(
+            "Getting credential from resolved route: {}",
+            resolved_route.clone()
+        );
+
+        let client = SecureClient::new(
+            self.secure_channels.clone(),
+            resolved_route,
+            &self.issuer.identifier,
+            for_identity,
+            DEFAULT_TIMEOUT,
+        );
+
+        let credential_result = client
             .ask(ctx, "credential_issuer", Request::post("/"))
             .await?
-            .success()?;
-        Ok(credential)
+            .success();
+
+        if let Some(transport_address) = transport_address {
+            let _ = self.transport.disconnect(transport_address).await;
+        }
+
+        match credential_result {
+            Ok(credential) => {
+                debug!("Getting credential from: {} succeeded", &self.issuer.route);
+                Ok(credential)
+            }
+            Err(err) => {
+                debug!(
+                    "Getting credential from: {} failed with err: {}",
+                    &self.issuer.route, err
+                );
+                Err(err)
+            }
+        }
     }
 }
 
