b93e7de6a447a403c4fd951bb57b54f5278fc64a
Oleksandr Deundiak <olek@ockam.io>
2024-01-08 17:56:49 +0400

    feat(rust): improve `RemoteCredentialRestriever`:
     - implement recreating of tcp connections
     - implement shutting down connection

--- examples/rust/get_started/examples/11-attribute-based-authentication-edge-plane.rs
+++ examples/rust/get_started/examples/11-attribute-based-authentication-edge-plane.rs
@@ -9,7 +9,7 @@ use ockam::node;
 use ockam::{route, Context, Result};
 use ockam_api::authenticator::enrollment_tokens::TokenAcceptor;
 use ockam_api::nodes::NodeManager;
-use ockam_api::{multiaddr_to_route, DefaultAddress};
+use ockam_api::{multiaddr_to_route, multiaddr_to_transport_route, DefaultAddress};
 use ockam_core::compat::sync::Arc;
 use ockam_multiaddr::MultiAddr;
 use ockam_transport_tcp::{TcpInletOptions, TcpTransportExtension};
@@ -72,14 +72,15 @@ async fn start_node(ctx: Context, project_information_path: &str, token: OneTime
     let project = import_project(project_information_path, node.identities()).await?;
 
     // Create a trust context that will be used to authenticate credential exchanges
-    let tcp_project_session = multiaddr_to_route(&project.route(), &tcp).await.unwrap(); // FIXME: Handle error
+    let project_authority_route = multiaddr_to_transport_route(&project.route()).unwrap(); // FIXME: Handle error
 
     // Create a trust context that will be used to authenticate credential exchanges
     let credentials_retriever = Arc::new(RemoteCredentialsRetriever::new(
+        Arc::new(tcp.clone()),
         node.secure_channels(),
         RemoteCredentialsRetrieverInfo::new(
             project.authority_identifier(),
-            tcp_project_session.route,
+            project_authority_route,
             DefaultAddress::CREDENTIAL_ISSUER.into(),
         ),
     ));
