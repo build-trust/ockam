160357bb4a020ed6f0f0c1ddea92d790084da1b2
Michael Uti <utimichael9@gmail.com>
2024-02-21 17:01:44 +0100

    chore: move the ockly crate to the ockam rust workspace

--- implementations/rust/ockam/ockam_rust_elixir_nifs/src/lib.rs
+++ implementations/rust/ockam/ockam_rust_elixir_nifs/src/lib.rs
@@ -1,3 +1,23 @@
+//! This library builds a NIF for our Elixir library
+//! To build the NIF module:
+//!
+//! - Your NIF will now build along with your project.
+//!
+//! ## To load the NIF:
+//!
+//! ```elixir
+//! defmodule Ockly do
+//!   use Rustler, otp_app: :ockly, crate: "ockly"
+//!
+//!   # When your NIF is loaded, it will override this function.
+//!   def add(_a, _b), do: :erlang.nif_error(:nif_not_loaded)
+//! end
+//! ```
+//!
+//! ## Examples
+//!
+//! [This](https://github.com/rusterlium/NifIo) is a complete example of a NIF written in Rust.
+
 use std::{
     future::Future,
     ops::Deref,
