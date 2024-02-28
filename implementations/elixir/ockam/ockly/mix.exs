ea3747db5fa7a544316850298c9396812edb1fde
Michael Uti <utimichael9@gmail.com>
2024-02-28 16:05:20 +0100

    ci: update release workflow to include building of nifs

--- implementations/elixir/ockam/ockly/mix.exs
+++ implementations/elixir/ockam/ockly/mix.exs
@@ -4,7 +4,7 @@ defmodule Ockly.MixProject do
   def project do
     [
       app: :ockly,
-      version: "0.1.0",
+      version: "0.117.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
