76357de780f3a799395f3e0a3aac76166178c9a2
Pablo Polvorin <pablo@ockam.io>
2024-03-04 20:38:42 -0300

    chore(rust): update example

--- examples/rust/get_started/examples/06-credentials-exchange-issuer.rs
+++ examples/rust/get_started/examples/06-credentials-exchange-issuer.rs
@@ -54,7 +54,7 @@ async fn main(ctx: Context) -> Result<()> {
     //
     // For a different application this attested attribute set can be different and
     // distinct for each identifier, but for this example we'll keep things simple.
-    let credential_issuer = CredentialIssuerWorker::new(members.clone(), node.credentials(), &issuer, None, None);
+    let credential_issuer = CredentialIssuerWorker::new(members.clone(), node.credentials(), &issuer, None, None, None);
 
     let mut pre_trusted_identities = BTreeMap::<Identifier, PreTrustedIdentity>::new();
     let attributes = PreTrustedIdentity::new(
