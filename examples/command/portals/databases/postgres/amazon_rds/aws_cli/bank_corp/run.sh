35d29180f99607895944f12e310a37582e76ef0e
Mrinal Wadhwa <mrinal@ockam.io>
2024-03-08 23:34:31 -0800

    docs: add example to show a portal to postgres in amazon rds

---
+++ examples/command/portals/databases/postgres/amazon_rds/aws_cli/bank_corp/run.sh
@@ -0,0 +1,136 @@
+#!/usr/bin/env bash
+set -ex
+
+run() {
+    enrollment_ticket="$1"
+    region=$(aws configure get region)
+    user_id=$(aws sts get-caller-identity  --query 'UserId' |  awk -F'"|:' '{print tolower($1)}')
+    name="ockam-ex-pg-rds-bank-corp-$user_id"
+
+    # ----------------------------------------------------------------------------------------------------------------
+    # CREATE NETWORK
+
+    # Create a new VPC and tag it.
+    vpc_id=$(aws ec2 create-vpc --cidr-block 10.0.0.0/16 --query 'Vpc.VpcId')
+    aws ec2 create-tags --resources "$vpc_id" --tags "Key=Name,Value=${name}-vpc"
+
+    # Create and Internet Gateway and attach it to the VPC.
+    gw_id=$(aws ec2 create-internet-gateway --query 'InternetGateway.InternetGatewayId')
+    aws ec2 attach-internet-gateway --vpc-id "$vpc_id" --internet-gateway-id "$gw_id"
+
+    # Create a route table and a route to the Internet through the Gateway.
+    rtb_id=$(aws ec2 create-route-table --vpc-id "$vpc_id" --query 'RouteTable.RouteTableId')
+    aws ec2 create-route --route-table-id "$rtb_id" --destination-cidr-block 0.0.0.0/0 --gateway-id "$gw_id"
+
+    subnet1_id=$(aws ec2 create-subnet --vpc-id "$vpc_id" --cidr-block 10.0.0.0/25 \
+        --availability-zone "${region}a" --query 'Subnet.SubnetId')
+    aws ec2 modify-subnet-attribute --subnet-id "$subnet1_id" --map-public-ip-on-launch
+    aws ec2 associate-route-table --subnet-id "$subnet1_id" --route-table-id "$rtb_id"
+
+    subnet2_id=$(aws ec2 create-subnet --vpc-id "$vpc_id" --cidr-block 10.0.0.128/25 \
+        --availability-zone "${region}b" --query 'Subnet.SubnetId')
+    aws ec2 modify-subnet-attribute --subnet-id "$subnet2_id" --map-public-ip-on-launch
+    aws ec2 associate-route-table --subnet-id "$subnet2_id" --route-table-id "$rtb_id"
+
+    # Create a security group to allow:
+    #   - TCP egress to the Internet
+    #   - Postgess ingress only from withing our two subnets.
+    sg_id=$(aws ec2 create-security-group --group-name "$name" --vpc-id "$vpc_id" --query 'GroupId' \
+        --description "Allow TCP egress and Postgres ingress")
+    aws ec2 authorize-security-group-egress --group-id "$sg_id" --cidr 0.0.0.0/0 --protocol tcp --port 0-65535
+    aws ec2 authorize-security-group-ingress --group-id "$sg_id" --cidr 10.0.0.0/24 --protocol tcp --port 5432
+
+    # ----------------------------------------------------------------------------------------------------------------
+    # CREATE DATABASE
+
+    aws rds create-db-subnet-group --db-subnet-group-name "${name}-db-subnet-group" \
+        --db-subnet-group-description "DB subnet group" --subnet-ids "$subnet1_id" "$subnet2_id"
+    aws rds create-db-instance --db-instance-identifier "${name}-db-instance" --no-publicly-accessible \
+        --engine postgres --db-instance-class db.t3.micro --allocated-storage 20 \
+        --vpc-security-group-ids "$sg_id" --db-subnet-group-name "${name}-db-subnet-group" \
+        --master-username myadmin --master-user-password mypassword --db-name test
+    aws rds wait db-instance-available --db-instance-identifier "${name}-db-instance"
+    db_endpoint=$(aws rds describe-db-instances --db-instance-identifier "${name}-db-instance" \
+        --query "DBInstances[0].Endpoint.Address")
+
+    # ----------------------------------------------------------------------------------------------------------------
+    # CREATE INSTANCE
+
+    ami_id=$(aws ec2 describe-images --owners 137112412989 --query "Images | sort_by(@, &CreationDate) | [-1].ImageId" \
+        --filters "Name=name,Values=al2023-ami-2023*" "Name=architecture,Values=x86_64" \
+                  "Name=virtualization-type,Values=hvm" "Name=root-device-type,Values=ebs" )
+
+    sed "s/\$ENROLLMENT_TICKET/${enrollment_ticket}/g" run_ockam.sh > user_data1.sh
+    sed "s/\$POSTGRES_ADDRESS/${db_endpoint}/g" user_data1.sh > user_data.sh
+    instance_id=$(aws ec2 run-instances --image-id "$ami_id" --instance-type t2.micro \
+        --subnet-id "$subnet1_id" --security-group-ids "$sg_id" \
+        --user-data file://user_data.sh --query 'Instances[0].InstanceId')
+    aws ec2 create-tags --resources "$instance_id" --tags "Key=Name,Value=${name}-ec2-instance"
+    aws ec2 wait instance-running --instance-ids "$instance_id"
+    rm -f user_data.sh user_data1.sh
+}
+
+cleanup() {
+    user_id=$(aws sts get-caller-identity  --query 'UserId' |  awk -F'"|:' '{print tolower($1)}')
+    name="ockam-ex-pg-rds-bank-corp-$user_id"
+
+    # ----------------------------------------------------------------------------------------------------------------
+    # DELETE INSTANCE
+
+    rm -f user_data.sh user_data1.sh
+    instance_ids=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${name}-ec2-instance" \
+        --query "Reservations[*].Instances[*].InstanceId")
+    for i in $instance_ids; do
+        aws ec2 terminate-instances --instance-ids "$i"
+        aws ec2 wait instance-terminated --instance-ids "$i"
+    done
+
+    # ----------------------------------------------------------------------------------------------------------------
+    # DELETE DATABASE
+
+    if aws rds describe-db-instances --db-instance-identifier "${name}-db-instance" &>/dev/null; then
+        aws rds delete-db-instance --db-instance-identifier "${name}-db-instance" \
+            --skip-final-snapshot --delete-automated-backups
+        aws rds wait db-instance-deleted --db-instance-identifier "${name}-db-instance"
+    fi
+
+    if aws rds describe-db-subnet-groups --db-subnet-group-name "${name}-db-subnet-group" &>/dev/null; then
+        aws rds delete-db-subnet-group --db-subnet-group-name "${name}-db-subnet-group"
+    fi
+
+    # ----------------------------------------------------------------------------------------------------------------
+    # DELETE NETWORK
+
+    vpc_ids=$(aws ec2 describe-vpcs --query 'Vpcs[*].VpcId' --filters "Name=tag:Name,Values=${name}-vpc")
+
+    for vpc_id in $vpc_ids; do
+        internet_gateways=$(aws ec2 describe-internet-gateways --query "InternetGateways[*].InternetGatewayId" \
+            --filters Name=attachment.vpc-id,Values="$vpc_id")
+        for i in $internet_gateways; do
+            aws ec2 detach-internet-gateway --internet-gateway-id "$i" --vpc-id "$vpc_id"
+            aws ec2 delete-internet-gateway --internet-gateway-id "$i"
+        done
+
+        subnet_ids=$(aws ec2 describe-subnets --query "Subnets[*].SubnetId" --filters Name=vpc-id,Values="$vpc_id")
+        for i in $subnet_ids; do aws ec2 delete-subnet --subnet-id "$i"; done
+
+        route_tables=$(aws ec2 describe-route-tables  --filters Name=vpc-id,Values="$vpc_id" \
+            --query 'RouteTables[?length(Associations[?Main!=`true`]) > `0` || length(Associations) == `0`].RouteTableId')
+        for i in $route_tables; do aws ec2 delete-route-table --route-table-id "$i" || true; done
+
+        security_groups=$(aws ec2 describe-security-groups --filters Name=vpc-id,Values="$vpc_id" \
+            --query "SecurityGroups[?!contains(GroupName, 'default')].[GroupId]")
+        for i in $security_groups; do aws ec2 delete-security-group --group-id "$i"; done
+
+        if aws ec2 describe-vpcs --vpc-ids "$vpc_id" &>/dev/null; then
+            aws ec2 delete-vpc --vpc-id "$vpc_id"
+        fi
+    done
+}
+
+export AWS_PAGER="";
+export AWS_DEFAULT_OUTPUT="text";
+
+# Check if the first argument is "cleanup"
+# If it is, call the cleanup function. If not, call the run function.
+if [ "$1" = "cleanup" ]; then cleanup; else run "$1"; fi
