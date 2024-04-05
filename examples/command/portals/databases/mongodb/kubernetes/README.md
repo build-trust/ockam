81b2e12bb55d3ef21a5e6e4f76559742e4e086d8
Michael Uti <utimichael9@gmail.com>
2024-04-05 19:47:42 +0100

    chore: add mongodb kubernetes example

---
+++ examples/command/portals/databases/mongodb/kubernetes/README.md
@@ -0,0 +1,9 @@
+# Kubernetes
+
+This hands-on example uses Ockam to create an end-to-end encrypted portal to mongodb.
+
+We connect a nodejs app in one private kubernetes cluster with a mongodb database in another
+private kubernetes cluster. The example uses docker and kind to create these kubernetes clusters.
+
+You can read a detailed walkthrough of this example at:
+https://docs.ockam.io/portals/databases/mongodb/kubernetes
---
+++ examples/command/portals/databases/mongodb/kubernetes/analysis_corp/run_ockam.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -ex
+
+# This script is used as an entrypoint to a docker container built using ../ockam.dockerfile.
+
+# Run `ockam project enroll ...`
+#
+# The `project enroll` command creates a new vault and generates a cryptographic identity with
+# private keys stored in that vault.
+#
+# The enrollment ticket includes routes and identifiers for the project membership authority
+# and the project’s node that offers the relay service.
+#
+# The enrollment ticket also includes an enrollment token. The project enroll command
+# creates a secure channel with the project membership authority and presents this enrollment token.
+# The authority enrolls presented identity and returns a project membership credential.
+#
+# The command, stores this credential for later use and exits.
+ockam project enroll /etc/ockam/enrollment/ticket
+
+# Create an ockam node.
+#
+# Create an access control policy that only allows project members that possesses a credential with
+# attribute mongodb-outlet="true" to connect to TCP Portal Inlets on this node.
+#
+# Create a TCP Portal Inlet to mongodb.
+# This makes the remote mongodb available on localhost:17017
+ockam node create
+ockam policy create --resource tcp-inlet --expression '(= subject.mongodb-outlet "true")'
+ockam tcp-inlet create --from 0.0.0.0:17017 --via mongodb
+
+# Run the container forever.
+tail -f /dev/null
