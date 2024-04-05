47a8b78fe9048dd4938d987fd9c7b0ac30c7a885
Michael Uti <utimichael9@gmail.com>
2024-04-05 13:41:03 +0100

    chore: add mongodb amazon vpc example

---
+++ examples/command/portals/databases/mongodb/amazon_vpc/README.md
@@ -0,0 +1,9 @@
+# Amazon VPC
+
+This hands-on example uses Ockam to create an end-to-end encrypted portal to MongoDB.
+
+We connect a nodejs app in one Amazon VPC with a Amazon VPC managed Mongo database in another Amazon VPC.
+The example uses AWS CLI to create these VPCs.
+
+You can read a detailed walkthough of this example at:
+https://docs.ockam.io/portals/databases/mongodb/amazon_vpc
---
+++ examples/command/portals/databases/mongodb/amazon_vpc/run.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/env bash
+set -e
+
+# This script, `./run.sh ...` is invoked on a developer’s work machine.
+#
+# This hands-on example uses Ockam to create an end-to-end encrypted portal to MongoDB. We connect a nodejs app in
+# one Amazon VPC with another Amazon VPC with  MongoDB installed.
+#
+# The example uses AWS CLI to create these VPCs.
+#
+# You can read a detailed walkthrough of this example at:
+# https://docs.ockam.io/portals/databases/mongodb/amazon_vpc
+
+run() {
+    # Run `ockam enroll`.
+    #
+    # The enroll command creates a new vault and generates a cryptographic identity with
+    # private keys stored in that vault. It then guides you to sign in to Ockam Orchestrator.
+    #
+    # If this is your first time signing in, the Orchestrator creates a new dedicated project
+    # for you. A project offers two services: a membership authority and a relay service.
+    #
+    # The enroll command then asks this project’s membership authority to sign and issue
+    # a credential that attests that your identifier is a member of this project. Since your
+    # account in Orchestrator is the creator and hence first administrator on this new project,
+    # the membership authority issues this credential. The enroll command stores the
+    # credential for later use and exits.
+    # ockam enroll
+
+    # Create an enrollment ticket to enroll the identity used by an ockam node that will run
+    # adjacent to the MongoDB server in bank_corp's network.
+    #
+    # The identity that enrolls with the generated ticket will be given a project membership
+    # credential in which the project membership authority will cryptographically attest to the
+    # specified attributes - mongodb-outlet=true.
+    #
+    # The identity will also allowed to create a relay in the project at the address `mongodb`.
+    bank_corp_ticket=$(ockam project ticket --usage-count 1 --expires-in 10m \
+        --attribute "mongodb-outlet=true" --relay mongodb)
+
+    # Create an enrollment ticket to enroll the identity used by an ockam node that will run
+    # adjacent to the MongoDB client app in analysis_corp's network.
+    #
+    # The identity that enrolls with the generated ticket will be given a project membership
+    # credential in which the project membership authority will cryptographically attest to the
+    # specified attributes - mongodb-inlet=true.
+    analysis_corp_ticket=$(ockam project ticket --usage-count 1 --expires-in 10m \
+        --attribute "mongodb-inlet=true")
+
+    # Invoke `bank_corp/run.sh` in the directory that has bank_corp's configuration. Pass the above enrollment ticket
+    # as the first argument to run.sh script. Read bank_corp/run.sh to understand the parts that are provisioned in
+    # bank_corp's virtual private cloud.
+    echo; pushd bank_corp; ./run.sh "$bank_corp_ticket"; popd
+
+    # Invoke `analysis_corp/run.sh` in the directory that has analysis_corp's configuration. Pass the above enrollment
+    # ticket as the first argument to run.sh script. Read analysis_corp/run.sh to understand the parts that are
+    # provisioned in analysis_corp's virtual private cloud.
+    echo; pushd analysis_corp; ./run.sh "$analysis_corp_ticket"; popd
+}
+
+# Cleanup after the example - `./run.sh cleanup`
+# Remove all resources that were created in AWS.
+cleanup() {
+    pushd bank_corp; ./run.sh cleanup; popd
+    pushd analysis_corp; ./run.sh cleanup; popd
+}
+
+# Check if Ockam Command is already installed and available in path.
+# If it's not, then install it.
+if ! type ockam &>/dev/null; then
+    curl --proto '=https' --tlsv1.2 -sSfL https://install.command.ockam.io | bash
+    source "$HOME/.ockam/env"
+fi
+
+# Check if the first argument is "cleanup"
+# If it is, call the cleanup function. If not, call the run function.
+if [ "$1" = "cleanup" ]; then cleanup; else run; fi
