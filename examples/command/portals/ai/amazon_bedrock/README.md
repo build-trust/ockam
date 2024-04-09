84d81fcf706a65634d7d67ade1d0a028cb197b39
etorreborre <etorreborre@yahoo.com>
2024-04-09 16:03:52 +0200

    docs(rust): add an example of portal to amazon bedrock

---
+++ examples/command/portals/ai/amazon_bedrock/README.md
@@ -0,0 +1,9 @@
+# Amazon Bedrock
+
+This hands-on example uses Ockam to create an end-to-end encrypted portal to an Amazon Bedrock API.
+
+We connect a nodejs app in one virtual private network with an Amazon Bedrock API in another virtual private network.
+The example uses the AWS CLI to create these virtual networks.
+
+You can read a detailed walkthrough of this example at:
+https://docs.ockam.io/portals/ai/amazon_bedrock
---
+++ examples/command/portals/ai/amazon_bedrock/health_corp/client.js
@@ -0,0 +1,31 @@
+async function run() {
+    try {
+        const statusResponse = await fetch("http://127.0.0.1:3000/status");
+        if (!statusResponse.ok) {
+            console.log("Connection failed, status:", statusResponse.status);
+            return;
+        }
+
+        const query = "What is Ockham's Razor?";
+        console.log("Connected to the model.\n\nApp: ", query);
+        const queryResponse = await fetch("http://127.0.0.1:3000/query", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({query}),
+            keepalive: true,
+        });
+
+        if (!queryResponse.ok) {
+            console.log("Query failed, status:", queryResponse.status);
+            console.log("Error:", queryResponse.error);
+            return;
+        }
+
+        const answer = await queryResponse.json();
+        console.log(answer['answer']);
+    } catch (error) {
+        console.log("Error:", error);
+    }
+}
+
+run();
