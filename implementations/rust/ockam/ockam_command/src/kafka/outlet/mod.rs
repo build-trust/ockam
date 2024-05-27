345a23550ffebbb0efa31ab6cdb71a9a3d39cb2e
Adrian Benavides <adri.benavides312@gmail.com>
2024-05-27 17:51:03 +0200

    feat(rust): add delete and list commands for kafka-outlet

--- implementations/rust/ockam/ockam_command/src/kafka/outlet/mod.rs
+++ implementations/rust/ockam/ockam_command/src/kafka/outlet/mod.rs
@@ -1,6 +1,10 @@
 pub(crate) mod create;
+mod delete;
+mod list;
 
 use self::create::CreateCommand;
+use self::delete::DeleteCommand;
+use self::list::ListCommand;
 use crate::{Command, CommandGlobalOpts};
 use clap::{command, Args, Subcommand};
 
@@ -15,18 +19,24 @@ pub struct KafkaOutletCommand {
 #[derive(Clone, Debug, Subcommand)]
 pub enum KafkaOutletSubcommand {
     Create(CreateCommand),
+    Delete(DeleteCommand),
+    List(ListCommand),
 }
 
 impl KafkaOutletCommand {
     pub fn run(self, opts: CommandGlobalOpts) -> miette::Result<()> {
         match self.subcommand {
             KafkaOutletSubcommand::Create(c) => c.run(opts),
+            KafkaOutletSubcommand::Delete(c) => c.run(opts),
+            KafkaOutletSubcommand::List(c) => c.run(opts),
         }
     }
 
     pub fn name(&self) -> String {
         match &self.subcommand {
             KafkaOutletSubcommand::Create(c) => c.name(),
+            KafkaOutletSubcommand::Delete(c) => c.name(),
+            KafkaOutletSubcommand::List(c) => c.name(),
         }
         .to_string()
     }
