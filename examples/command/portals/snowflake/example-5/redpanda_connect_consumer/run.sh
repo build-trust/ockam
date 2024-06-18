d92d4e419cdd667374f81f000825b32321e6a7b0
Davide Baldo <davide@baldo.me>
2024-06-18 17:47:30 +0200

    docs: added 5th snowflake example

---
+++ examples/command/portals/snowflake/example-5/redpanda_connect_consumer/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+url_encode() {
+  python -c "import urllib.parse; print(urllib.parse.quote('$1', safe=''))"
+}
+
+token=$(cat /snowflake/session/token)
+encoded_token=$(url_encode "$token")
+export SNOWFLAKE_TOKEN="$encoded_token"
+SNOWFLAKE_TOKEN="$encoded_token" redpanda-connect --config /consumer.yaml
