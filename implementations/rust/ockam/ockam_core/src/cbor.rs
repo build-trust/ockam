efc04f378ef3fc2a31355c25d166e0ec297c9faa
Oleksandr Deundiak <olek@ockam.io>
2024-06-26 15:43:04 +0200

    fix(rust): account for `minicbor` length calculation bug

--- implementations/rust/ockam/ockam_core/src/cbor.rs
+++ implementations/rust/ockam/ockam_core/src/cbor.rs
@@ -7,7 +7,7 @@ pub(crate) mod schema;
 
 use crate::compat::vec::Vec;
 use crate::Result;
-use minicbor::{CborLen, Encode, Encoder};
+use minicbor::{CborLen, Encode};
 
 /// Encode a type implementing [`Encode`] and return the encoded byte vector.
 ///
@@ -17,9 +17,10 @@ pub fn cbor_encode_preallocate<T>(x: T) -> Result<Vec<u8>>
 where
     T: Encode<()> + CborLen<()>,
 {
-    let output_len = minicbor::len(&x);
-    let output = Vec::with_capacity(output_len);
-    let mut e = Encoder::new(output);
-    x.encode(&mut e, &mut ())?;
-    Ok(e.into_writer())
+    // Due to minicbor bug this value is bigger than should be for structures having
+    // #[cbor(transparent)] attribute.
+    let max_expected_len = minicbor::len(&x);
+    let mut output = Vec::with_capacity(max_expected_len);
+    minicbor::encode(x, &mut output)?;
+    Ok(output)
 }
