1979a44658cd544d3da6b24bd773c2e243d430ce
Sanjeev Nithyanandam <sanjeev.nithyanandam@ockam.io>
2024-07-03 07:20:22 -0700

    docs: snowflake cdc to kafka example

---
+++ examples/command/portals/snowflake/example-5/snowflake_cdc_publisher/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -ex
+
+cat <<EOF > ./ockam.yaml
+name: kafka_inlet_node
+ticket: ${ENROLLMENT_TICKET}
+
+kafka-inlet:
+  from: 127.0.0.1:9092
+  disable-content-encryption: true
+  allow: snowflake-kafka-outlet
+  to: /project/default/service/forward_to_kafka/secure/api
+EOF
+
+ockam node create --foreground ./ockam.yaml &
+
+sleep 10
+
+python ./snowflake_cdc_publisher.py
