7f674e2ec4ec751ac12d75e3c54f9236d8de10ed
Michael Uti <utimichael9@gmail.com>
2024-08-30 19:06:57 +0100

    ci: remove beam flake utilities from nix

--- tools/nix/parts/elixir.nix
+++ tools/nix/parts/elixir.nix
@@ -1,51 +1,38 @@
 {
   config,
-  inputs,
   lib,
   ...
 }: let
-  inherit (lib) mkEnableOption mkOption types;
-  cfg = config.ockam.elixir;
-in {
-  options = {
-    ockam.elixir = {
-      erlangVersion = mkOption {
-        type = types.nullOr types.str;
-        default = "25.3.2";
-      };
-      languageServer = mkEnableOption "elixir-ls" // {default = true;};
-      shadowAsdf = mkEnableOption "override ASDF to use Nix-provided binaries" // {default = true;};
-      version = mkOption {
-        type = types.nullOr types.str;
-        default = "1.14.4";
+    inherit (lib) mkEnableOption;
+    cfg = config.ockam.elixir;
+  in {
+    options = {
+      ockam.elixir = {
+        languageServer = mkEnableOption "elixir-ls" // {default = true;};
       };
     };
-  };
-  config = {
-    perSystem = {
-      config,
-      pkgs,
-      ...
-    }: let
-      pkgset = inputs.beam-flakes.lib.mkPackageSet {
-        inherit pkgs;
-        inherit (cfg) erlangVersion;
-        elixirVersion = cfg.version;
-        elixirLanguageServer = cfg.languageServer;
-      };
-    in {
-      devShells.elixir = pkgs.mkShell {
-        # ockam_vault_software uses a Rust NIF
-        inputsFrom = with config.devShells; [rust tooling];
-        packages = with pkgset; [elixir erlang] ++ lib.optional cfg.languageServer elixir-ls;
-        # support pkgconfig without duplication of effort
-        inherit (config.devShells.rust) nativeBuildInputs shellHook;
 
-        ASDF_ELIXIR_VERSION = lib.optional cfg.shadowAsdf "system";
-        ASDF_ERLANG_VERSION = lib.optional cfg.shadowAsdf "system";
-        inherit (config.devShells.rust) OCKAM_DISABLE_UPGRADE_CHECK RUSTFLAGS RUST_SRC_PATH LIBCLANG_PATH;
-        inherit (config.devShells.tooling) BATS_LIB;
-      };
+    config = {
+      perSystem = {
+          config,
+          lib,
+          pkgs,
+          ...
+        }: {
+          devShells.elixir = pkgs.mkShell {
+            packages = with pkgs; [
+              erlang_24
+              elixir_1_13
+            ] ++ lib.optional cfg.languageServer elixir-ls;
+
+            # ockam_vault_software uses a Rust NIF
+            inputsFrom = with config.devShells; [rust tooling];
+            # support pkgconfig without duplication of effort
+            inherit (config.devShells.rust) nativeBuildInputs shellHook;
+
+            inherit (config.devShells.rust) OCKAM_DISABLE_UPGRADE_CHECK RUSTFLAGS RUST_SRC_PATH LIBCLANG_PATH;
+            inherit (config.devShells.tooling) BATS_LIB;
+          };
+        };
     };
-  };
 }
