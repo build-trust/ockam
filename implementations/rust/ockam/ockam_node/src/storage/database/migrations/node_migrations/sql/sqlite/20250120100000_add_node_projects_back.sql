8e7995de85287e0eca895aaccd2795c1df465462
etorreborre <etorreborre@yahoo.com>
2025-01-20 11:58:05 +0100

    feat(rust): add the nodes_projects table back

---
+++ implementations/rust/ockam/ockam_node/src/storage/database/migrations/node_migrations/sql/sqlite/20250120100000_add_node_projects_back.sql
@@ -0,0 +1,6 @@
+-- This table is temporarily added back to make it consistent with the current code.
+CREATE TABLE node_project
+(
+    node_name    TEXT PRIMARY KEY, -- Node name
+    project_name TEXT NOT NULL     -- Project name
+);
