c97315c03a1a7db1ddd9cce33eb22df7d2bf0fb3
Adrian Benavides <adri.benavides312@gmail.com>
2025-04-03 11:17:37 +0200

    feat(rust): add ai subcommands for enroll and ticket

---
+++ implementations/rust/ockam/ockam_command/src/ai/ticket.rs
@@ -0,0 +1,31 @@
+use async_trait::async_trait;
+
+use clap::Args;
+use ockam_node::Context;
+
+use crate::{docs, Command, CommandGlobalOpts, Result};
+
+const LONG_ABOUT: &str = include_str!("./static/ticket/long_about.txt");
+const PREVIEW_TAG: &str = include_str!("../static/preview_tag.txt");
+const AFTER_LONG_HELP: &str = include_str!("./static/ticket/after_long_help.txt");
+
+/// Generate an enrollment ticket for an Ockam AI Agent
+#[derive(Clone, Debug, Args)]
+#[command(
+long_about = docs::about(LONG_ABOUT),
+before_help = docs::before_help(PREVIEW_TAG),
+after_long_help = docs::after_help(AFTER_LONG_HELP)
+)]
+pub struct TicketCommand {
+    #[command(flatten)]
+    inner: crate::project::TicketCommand,
+}
+
+#[async_trait]
+impl Command for TicketCommand {
+    const NAME: &'static str = "ai ticket";
+
+    async fn run(self, ctx: &Context, opts: CommandGlobalOpts) -> Result<()> {
+        self.inner.run(ctx, opts).await
+    }
+}
