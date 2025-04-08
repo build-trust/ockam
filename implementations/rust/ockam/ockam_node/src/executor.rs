af517aba9394c9551a92cca2aaaacbeadb046a10
Michael Uti <utimichael9@gmail.com>
2025-04-08 14:22:23 +0100

    chore(rust): make ockam executor clonable

--- implementations/rust/ockam/ockam_node/src/executor.rs
+++ implementations/rust/ockam/ockam_node/src/executor.rs
@@ -28,6 +28,7 @@ use ockam_core::{
 /// This type is a small wrapper around an inner async runtime (`tokio` by
 /// default) and the Ockam router. In most cases it is recommended you use the
 /// `ockam::node` function annotation instead!
+#[derive(Clone)]
 pub struct Executor {
     /// Reference to the runtime needed to spawn tasks
     runtime: Arc<Runtime>,
