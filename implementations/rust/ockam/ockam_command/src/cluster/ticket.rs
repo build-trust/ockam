8c97aa39b3c28d458fa1d672709a61c5713264cf
Pablo Polvorin <pablo@ockam.io>
2025-04-23 12:41:37 -0300

    feat(rust): cluster ticket command going through provisioner
    user won't be able to create enrollment tickets by itself, it must
    create them through the controller/provisioner

--- implementations/rust/ockam/ockam_command/src/cluster/ticket.rs
+++ implementations/rust/ockam/ockam_command/src/cluster/ticket.rs
@@ -1,9 +1,17 @@
+use std::{collections::BTreeMap, sync::Arc};
+
 use async_trait::async_trait;
 
 use clap::Args;
+use miette::{miette, IntoDiagnostic};
+use ockam_api::{
+    nodes::InMemoryNode, orchestrator::ai_platform::node_service_client::AI_API_BASE_URL_ENV,
+};
 use ockam_node::Context;
 
-use crate::{docs, Command, CommandGlobalOpts, Result};
+use crate::{docs, node_command::InMemoryNodeCommand, Command, CommandGlobalOpts, Result};
+
+use super::utils::get_api_client;
 
 const LONG_ABOUT: &str = include_str!("./static/ticket/long_about.txt");
 const PREVIEW_TAG: &str = include_str!("../static/preview_tag.txt");
@@ -17,8 +25,78 @@ before_help = docs::before_help(PREVIEW_TAG),
 after_long_help = docs::after_help(AFTER_LONG_HELP)
 )]
 pub struct AiTicketCommand {
-    #[command(flatten)]
-    inner: crate::project::TicketCommand,
+    // === Specific args for the HTTP API endpoint
+    /// The Cluster that will be used
+    /// If not set, it will be retrieved from the enrolled user data.
+    #[arg(long)]
+    pub cluster: Option<String>,
+
+    /// The name of the Zone
+    #[arg(long)]
+    pub zone_name: String,
+
+    /// Force the command to use the HTTP API.
+    /// By default, the command will use the Orchestrator API.
+    #[arg(long)]
+    pub use_http_api: bool,
+
+    /// The API endpoint of the Ockam AI Platform.
+    /// Defaults to `http://localhost:30080`.
+    #[arg(long)]
+    pub api_endpoint: Option<String>,
+
+    /// Attributes in `key=value` format to be attached to the member. You can specify this option multiple times for multiple attributes
+    #[arg(short, long = "attribute", value_name = "ATTRIBUTE")]
+    attributes: Vec<String>,
+
+    /// Name of the relay that the identity using the ticket will be allowed to create. This name is transformed into attributes to prevent collisions when creating relay names. For example: `--relay foo` is shorthand for `--attribute ockam-relay=foo`
+    #[arg(long = "relay", value_name = "ENROLLEE_ALLOWED_RELAY_NAME")]
+    allowed_relay_name: Option<String>,
+}
+
+#[derive(Clone)]
+struct TicketNodeCommand {
+    opts: CommandGlobalOpts,
+    command: AiTicketCommand,
+}
+
+#[async_trait]
+impl InMemoryNodeCommand for TicketNodeCommand {
+    async fn init(&self) -> miette::Result<()> {
+        if let Some(api_endpoint) = &self.command.api_endpoint {
+            std::env::set_var(AI_API_BASE_URL_ENV, api_endpoint);
+        }
+        Ok(())
+    }
+
+    async fn run(&self, node: Arc<InMemoryNode>) -> miette::Result<()> {
+        let ctx = node.ctx();
+        let use_http_api = self.command.use_http_api || self.command.api_endpoint.is_some();
+        let api_client = get_api_client(&node, use_http_api).await?;
+        let cluster = match &self.command.cluster {
+            None => api_client.get_cluster(ctx).await?.into_inner(),
+            Some(cluster) => cluster.to_string(),
+        };
+        let ticket = api_client
+            .create_enrollment_token(
+                ctx,
+                &cluster,
+                &self.command.zone_name,
+                self.command.attributes()?,
+                self.command.allowed_relay_name.clone(),
+            )
+            .await?;
+
+        self.opts
+            .terminal
+            .clone()
+            .to_stdout()
+            .plain(format!("\n{ticket}"))
+            .machine(&ticket)
+            .json(&serde_json::to_string(&ticket).into_diagnostic()?)
+            .write_line()?;
+        Ok(())
+    }
 }
 
 #[async_trait]
@@ -26,6 +104,26 @@ impl Command for AiTicketCommand {
     const NAME: &'static str = "cluster ticket";
 
     async fn run(self, ctx: &Context, opts: CommandGlobalOpts) -> Result<()> {
-        self.inner.run(ctx, opts).await
+        let command = TicketNodeCommand {
+            opts: opts.clone(),
+            command: self.clone(),
+        };
+        command.execute(ctx, opts.state.clone()).await?;
+        Ok(())
+    }
+}
+
+impl AiTicketCommand {
+    //a bit of copy-pasted from project ticket command. But no tls, enroller, etc.
+    fn attributes(&self) -> Result<BTreeMap<String, String>> {
+        let mut attributes = BTreeMap::new();
+        for attr in &self.attributes {
+            let mut parts = attr.splitn(2, '=');
+            let key = parts.next().ok_or(miette!("key expected"))?;
+            // If no value is provided we assume that the attribute is a boolean attribute set to "true"
+            let value = parts.next().unwrap_or("true");
+            attributes.insert(key.to_string(), value.to_string());
+        }
+        Ok(attributes)
     }
 }
