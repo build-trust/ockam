3885a8b43a900dd5c4596681fe0845957e871ded
Adrian Benavides <adri.benavides312@gmail.com>
2025-05-07 20:01:16 +0200

    feat: add zones to `cluster show` output

--- implementations/rust/ockam/ockam_command/src/cluster/show.rs
+++ implementations/rust/ockam/ockam_command/src/cluster/show.rs
@@ -1,13 +1,15 @@
-use crate::cluster::utils::get_cluster;
+use crate::cluster::common_args::HttpApiArgs;
+use crate::cluster::utils::{get_api_client, get_cluster};
 use crate::node_command::InMemoryNodeCommand;
 use crate::{docs, Command, CommandGlobalOpts, Result};
 use async_trait::async_trait;
 use clap::Args;
 use colorful::Colorful;
 use ockam_api::colors::color_primary;
-use ockam_api::fmt_ok;
 use ockam_api::nodes::InMemoryNode;
+use ockam_api::{fmt_log, fmt_ok};
 use ockam_node::Context;
+use serde::Serialize;
 use std::sync::Arc;
 
 const LONG_ABOUT: &str = include_str!("./static/show/long_about.txt");
@@ -21,25 +23,47 @@ long_about = docs::about(LONG_ABOUT),
 before_help = docs::before_help(PREVIEW_TAG),
 after_long_help = docs::after_help(AFTER_LONG_HELP)
 )]
-pub struct ShowCommand {}
+pub struct ShowCommand {
+    #[command(flatten)]
+    pub http_api: HttpApiArgs,
+}
 
 #[derive(Clone)]
 struct DeployNodeCommand {
     opts: CommandGlobalOpts,
+    command: ShowCommand,
 }
 
 #[async_trait]
 impl InMemoryNodeCommand for DeployNodeCommand {
     async fn run(&self, node: Arc<InMemoryNode>) -> miette::Result<()> {
         let ctx = node.ctx();
+        let use_http_api = self.command.http_api.use_http_api();
+        let api_client = get_api_client(&node, use_http_api).await?;
         let cluster = get_cluster(ctx, &node).await?;
+        let zones = api_client.list_zones(ctx, &cluster).await?;
+        let output = ShowOutput {
+            cluster: cluster.clone(),
+            zones: zones.clone(),
+        };
+        let mut plain = fmt_ok!("Your cluster is {}\n", color_primary(&cluster));
+        if !zones.is_empty() {
+            plain += &fmt_log!(
+                "with zones: {}",
+                zones
+                    .iter()
+                    .map(|z| color_primary(z).to_string())
+                    .collect::<Vec<_>>()
+                    .join(", ")
+            );
+        }
         self.opts
             .terminal
             .clone()
             .to_stdout()
-            .plain(fmt_ok!("Your cluster is {}", color_primary(&cluster)))
+            .plain(plain)
             .machine(&cluster)
-            .json_obj(serde_json::json!({"cluster": cluster}))?
+            .json_obj(output)?
             .write_line()?;
         Ok(())
     }
@@ -50,8 +74,17 @@ impl Command for ShowCommand {
     const NAME: &'static str = "cluster show";
 
     async fn run(self, ctx: &Context, opts: CommandGlobalOpts) -> Result<()> {
-        let command = DeployNodeCommand { opts: opts.clone() };
+        let command = DeployNodeCommand {
+            opts: opts.clone(),
+            command: self.clone(),
+        };
         command.execute(ctx, opts.state.clone()).await?;
         Ok(())
     }
+}
+
+#[derive(Serialize)]
+struct ShowOutput {
+    cluster: String,
+    zones: Vec<String>,
 }
