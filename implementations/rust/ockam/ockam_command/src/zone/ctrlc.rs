4be391c969201e089508e8be045f8416b8ed36a4
Adrian Benavides <adri.benavides312@gmail.com>
2025-05-30 18:13:19 +0200

    refactor: zone ctrlc handler

--- implementations/rust/ockam/ockam_command/src/zone/ctrlc.rs
+++ implementations/rust/ockam/ockam_command/src/zone/ctrlc.rs
@@ -1,20 +1,33 @@
 use once_cell::sync::OnceCell;
+use std::sync::Arc;
 
-static CTRLC_HANDLER: OnceCell<tokio::sync::broadcast::Sender<()>> = OnceCell::new();
+static CTRLC_HANDLER: OnceCell<(
+    tokio::sync::broadcast::Sender<()>,
+    Arc<tokio::sync::broadcast::Receiver<()>>,
+)> = OnceCell::new();
 
 pub struct ZoneCtrlcHandler;
 
 impl ZoneCtrlcHandler {
-    pub fn rx() -> tokio::sync::broadcast::Receiver<()> {
-        let quit_tx = CTRLC_HANDLER.get_or_init(|| {
-            let (quit_tx, _quit_rx) = tokio::sync::broadcast::channel(16);
-            let _quit_tx = quit_tx.clone();
-            ctrlc::set_handler(move || {
-                let _ = _quit_tx.send(());
+    fn get() -> (
+        tokio::sync::broadcast::Sender<()>,
+        Arc<tokio::sync::broadcast::Receiver<()>>,
+    ) {
+        CTRLC_HANDLER
+            .get_or_init(|| {
+                let (quit_tx, quit_rx) = tokio::sync::broadcast::channel(16);
+                let _quit_tx = quit_tx.clone();
+                ctrlc::set_handler(move || {
+                    let _ = _quit_tx.send(());
+                })
+                .expect("Error setting exit signal handler");
+                (quit_tx, Arc::new(quit_rx))
             })
-            .expect("Error setting exit signal handler");
-            quit_tx
-        });
+            .clone()
+    }
+
+    pub fn rx() -> tokio::sync::broadcast::Receiver<()> {
+        let (quit_tx, _) = Self::get();
         quit_tx.subscribe()
     }
 }
