39e82d2afbbb03b00ae750d305334cefb7c223b8
Mrinal Wadhwa <mrinal@ockam.io>
2025-06-01 09:07:48 -0700

    refactor: remove some things

--- tools/nix/parts/all.nix
+++ tools/nix/parts/all.nix
@@ -2,10 +2,8 @@
   imports = [
     ./overlays.nix
     ./elixir.nix
-    ./examples.nix
     ./rust.nix
     ./tooling.nix
-    ./typescript.nix
   ];
 
   perSystem = {
@@ -14,7 +12,7 @@
     ...
   }: {
     devShells.default = pkgs.mkShell {
-      inputsFrom = with config.devShells; [elixir rust tooling typescript];
+      inputsFrom = with config.devShells; [elixir rust tooling];
 
       inherit (config.devShells.rust) nativeBuildInputs CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER DYLD_FALLBACK_LIBRARY_PATH OCKAM_DISABLE_UPGRADE_CHECK RUSTFLAGS RUST_SRC_PATH CARGO_INCREMENTAL LIBCLANG_PATH;
       inherit (config.devShells.tooling) BATS_LIB;
