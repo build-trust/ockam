ede015f59a5648753672d0ad98d41b2a82c5d5c3
Mrinal Wadhwa <mrinal@ockam.io>
2025-06-05 07:38:37 -0700

    docs(python): improve examples/001

--- examples/001/images/main/main.py
+++ examples/001/images/main/main.py
@@ -2,7 +2,7 @@ from ockam import Agent, Node
 
 
 async def main(node):
-    await Agent.start(node, "You are Henry, an expert legal assistant")
+    await Agent.start(node, "You are Henry, an expert legal assistant", "henry")
 
 
 Node.start(main)
