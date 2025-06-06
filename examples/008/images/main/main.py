919c5e1cb14c961dfb6bf159b7885cfefd38a827
Mrinal Wadhwa <mrinal@ockam.io>
2025-06-06 08:03:18 -0700

    docs(python): improve examples/008

--- examples/008/images/main/main.py
+++ examples/008/images/main/main.py
@@ -1,20 +1,14 @@
-from ockam import Agent, Node, ReActPlanner
+from ockam import Agent, Model, Node, ReActPlanner
 
-"""
-Ask a question to this agent via HTTP:
-```
-http --stream -b POST ':8000/agents/henry?timeout=60' message="Estimate how many violins there are in the world"
-```
 
-"""
 async def main(node):
     await Agent.start(
         node=node,
         name="henry",
-        instructions="You are an assistant who solves complex tasks by planning them carefully before solving them.",
-        planner=ReActPlanner(),
+        instructions="You are Henry, an expert legal assistant.",
+        model=Model("llama3.2"),
+        planner=ReActPlanner(Model("deepseek-r1")),
     )
-
 
 
 Node.start(main)
