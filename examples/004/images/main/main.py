b7f3cf04908b314b654acaf932a3caa70ebcf713
Mrinal Wadhwa <mrinal@ockam.io>
2025-06-06 08:05:03 -0700

    docs(python): add examples/004

---
+++ examples/004/images/main/main.py
@@ -0,0 +1,28 @@
+from ockam import Agent, Knowledge, Model, Node
+
+
+async def main(node):
+    docs = {
+        "sec-330": "https://raw.githubusercontent.com/AlextheYounga/us-federal-code/refs/heads/master/usc/title-15-commerce-and-trade/chapter-9a-weather-modification-activities-or-attempts%3B-reporting-requirement/sec-330.md",
+        "sec-330a": "https://raw.githubusercontent.com/AlextheYounga/us-federal-code/refs/heads/master/usc/title-15-commerce-and-trade/chapter-9a-weather-modification-activities-or-attempts%3B-reporting-requirement/sec-330a.md",
+        "sec-330b": "https://raw.githubusercontent.com/AlextheYounga/us-federal-code/refs/heads/master/usc/title-15-commerce-and-trade/chapter-9a-weather-modification-activities-or-attempts%3B-reporting-requirement/sec-330b.md",
+        "sec-330c": "https://raw.githubusercontent.com/AlextheYounga/us-federal-code/refs/heads/master/usc/title-15-commerce-and-trade/chapter-9a-weather-modification-activities-or-attempts%3B-reporting-requirement/sec-330c.md",
+        "sec-330d": "https://raw.githubusercontent.com/AlextheYounga/us-federal-code/refs/heads/master/usc/title-15-commerce-and-trade/chapter-9a-weather-modification-activities-or-attempts%3B-reporting-requirement/sec-330d.md",
+        "sec-330e": "https://raw.githubusercontent.com/AlextheYounga/us-federal-code/refs/heads/master/usc/title-15-commerce-and-trade/chapter-9a-weather-modification-activities-or-attempts%3B-reporting-requirement/sec-330e.md",
+
+    }
+
+    us_fedral_code = Knowledge("us_fedral_code")
+    for name, url in docs.items():
+        await us_fedral_code.add_document(name, url, content_type="text/markdown")
+
+    await Agent.start(
+        node=node,
+        name="henry",
+        instructions="You are Henry, an expert legal assistant",
+        model=Model("nova-micro-v1"),
+        knowledge=us_fedral_code,
+    )
+
+
+Node.start(main)
