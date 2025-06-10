0af32fc3a6cf7d4aae596806c8b6dfc4b929aaa5
Oleksandr Deundiak <olek@ockam.io>
2025-06-10 16:56:09 +0200

    feat(python): extend `ockam.gather`

--- implementations/python/python/ockam/gather.py
+++ implementations/python/python/ockam/gather.py
@@ -1,16 +1,21 @@
 import asyncio
 
 
-def gather(*coros_or_futures, batch_size=None):
-    if not batch_size:
-        return asyncio.gather(*coros_or_futures)
+def gather(*coros_or_futures, batch_size=None, timeout=None, return_exceptions=False):
+    sem = None
+    if batch_size:
+        sem = asyncio.Semaphore(batch_size)
 
-    sem = asyncio.Semaphore(batch_size)
+    async def wrap_task(f):
+        if timeout:
+            f = asyncio.wait_for(f, timeout)
 
-    async def batch_task(f):
-        async with sem:
+        if sem:
+            async with sem:
+                return await f
+        else:
             return await f
 
-    futures = [batch_task(f) for f in coros_or_futures]
+    futures = [wrap_task(f) for f in coros_or_futures]
 
-    return asyncio.gather(*futures)
+    return asyncio.gather(*futures, return_exceptions=return_exceptions)
