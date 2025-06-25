4a0e8f00a90667f9f7a9f0e0824c2aacc99f8c53
Oleksandr Deundiak <olek@ockam.io>
2025-06-25 19:44:09 +0200

    fix(python): logging improvements

--- implementations/python/python/ockam/agents/http.py
+++ implementations/python/python/ockam/agents/http.py
@@ -214,12 +214,12 @@ class HttpServer(InfoContext):
         return self.node
 
     async def serve(self):
-        self.logger.info("Starting the http server")
+        self.logger.info(f"Starting http server at {self.host}:{self.port}")
         config = uvicorn.Config(
             self.app, host=self.host, port=self.port, log_config=get_logging_config(), access_log=True
         )
         server = uvicorn.Server(config)
-        self.logger.info("Started the http server")
+        self.logger.info(f"Started http server at {self.host}:{self.port}")
         await server.serve()
 
     def set_host_and_port(self, listen_address):
